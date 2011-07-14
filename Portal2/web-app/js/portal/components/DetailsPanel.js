ba922055f667d6301b979c14c64559ee768b28b4
pmak <pmak@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2011-07-14 07:54:12 +0000

    separated out details panel

--- Portal2/web-app/js/portal/components/DetailsPanel.js
+++ Portal2/web-app/js/portal/components/DetailsPanel.js
@@ -1,3 +1,37 @@
+var detailsPanel;
+var opacitySlider;
+
 function initDetailsPanel()
 {
+
+// create a separate slider bound to the map but displayed elsewhere
+    opacitySlider = new GeoExt.LayerOpacitySlider({
+        id: "opacitySlider",
+        layer: layer,
+        width: 200,
+        inverse: false,
+        fieldLabel: "opacity",
+        plugins: new GeoExt.LayerOpacitySliderTip({template: '<div>Opacity: {opacity}%</div>'})
+    });
+
+   detailsPanel = new Ext.Panel({
+        title: 'Layer Options',
+        region: 'south',
+        border: false,
+        split: true,
+        height: 100,
+        autoScroll: true,
+        collapsible: true,
+        items: [
+               opacitySlider
+        ]
+    });
+
+
+   function updateDetailsPanel(node)
+   {
+        detailsPanel.text = node.layer.name;
+        detailsPanel.setTitle("Layer Options: " + node.layer.name);
+        opacitySlider.setLayer(node.layer);
+   }
 }
