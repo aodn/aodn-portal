992cd842cccea672aa2d89478fad7b7033281790
pmak <pmak@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2011-07-22 01:54:22 +0000

    can change style on selected layer

--- Portal2/web-app/js/portal/components/detailsPanel.js
+++ Portal2/web-app/js/portal/components/detailsPanel.js
@@ -1,37 +1,66 @@
-var detailsPanel;
-var opacitySlider;
-
 function initDetailsPanel()
-{
+ {
 
-// create a separate slider bound to the map but displayed elsewhere
-    opacitySlider = new GeoExt.LayerOpacitySlider({
-        id: "opacitySlider",
-        layer: layer,
-        width: 200,
-        inverse: false,
-        fieldLabel: "opacity",
-        plugins: new GeoExt.LayerOpacitySliderTip({template: '<div>Opacity: {opacity}%</div>'})
-    });
-
-   detailsPanel = new Ext.Panel({
-        title: 'Layer Options',
-        region: 'south',
-        border: false,
-        split: true,
-        height: 100,
-        autoScroll: true,
-        collapsible: true,
-        items: [
-               opacitySlider
+    styleList = new Ext.data.ArrayStore({
+        autoDestroy: true
+        ,id: 0
+        ,fields: [
+            'blah',
+            {name: 'myId'},
+            {name: 'displayText'}
         ]
     });
 
 
-   function updateDetailsPanel(node)
-   {
-        detailsPanel.text = node.layer.name;
-        detailsPanel.setTitle("Layer Options: " + node.layer.name);
-        opacitySlider.setLayer(node.layer);
-   }
+    // create the combo instance
+    combo = new Ext.form.ComboBox({
+        typeAhead: true
+        ,triggerAction: 'all'
+        ,lazyRender:true
+        ,mode: 'local'
+        ,store: styleList
+        ,valueField: 'myId'
+        ,displayField: 'displayText'
+        ,listeners: {
+            select: function(cbbox, record, index)
+            {
+                detailsPanelLayer.mergeNewParams({
+                    styles: record.get('myId')
+                });
+            }
+        }
+    });
+
+    detailsPanel = new Ext.Panel({
+        title: 'Layer Options'
+        ,region: 'south'
+        ,border: false
+        ,split: true
+        ,height: 100
+        ,autoScroll: true
+        ,collapsible: true
+        ,items: [
+               opacitySlider, combo
+         ]
+    });
 }
+
+function updateDetailsPanel(node)
+{
+
+    var styles = node.layer.metadata.styles;
+    styleList.removeAll();
+    combo.clearValue();
+    var data = new Array();
+
+    for(var i = 0;i < styles.length; i++)
+    {
+        data.push([i, styles[i].name, styles[i].name]);
+    }
+    styleList.loadData(data);
+    detailsPanel.text = node.layer.name;
+    detailsPanel.setTitle("Layer Options: " + node.layer.name);
+    opacitySlider.setLayer(node.layer);
+    detailsPanelLayer = layer;
+}
+
