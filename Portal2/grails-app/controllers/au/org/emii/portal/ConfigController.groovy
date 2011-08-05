f61336bf1999ee09ab16f5051319ee7769140d91
pmak <pmak@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2011-08-05 01:34:50 +0000

    added in links for top menu and adding in config domain for portal

---
+++ Portal2/grails-app/controllers/au/org/emii/portal/ConfigController.groovy
@@ -0,0 +1,102 @@
+package au.org.emii.portal
+
+import grails.converters.JSON
+
+class ConfigController {
+
+    static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
+
+    def index = {
+        redirect(action: "list", params: params)
+    }
+
+	def list = {
+		params.max = Math.min(params.max ? params.int('max') : 10, 100)
+		[configInstanceList: Config.list(params), configInstanceTotal: Config.count()]
+	}
+
+    def create = {
+        def configInstance = new Config()
+        configInstance.properties = params
+        return [configInstance: configInstance]
+    }
+
+    def save = {
+        def configInstance = new Config(params)
+        if (configInstance.save(flush: true)) {
+            flash.message = message(code: 'default.created.message', args: [message(code: 'config.label', default: 'Config'), configInstance.id])
+            redirect(action: "show", id: configInstance.id)
+        }
+        else {
+            render(view: "create", model: [configInstance: configInstance])
+        }
+    }
+
+    def show = {
+        def configInstance = Config.get(params.id)
+        if (!configInstance) {
+            flash.message = message(code: 'default.not.found.message', args: [message(code: 'config.label', default: 'Config'), params.id])
+            redirect(action: "list")
+        }
+        else {
+            [configInstance: configInstance]
+        }
+    }
+
+    def edit = {
+        def configInstance = Config.get(params.id)
+        if (!configInstance) {
+            flash.message = message(code: 'default.not.found.message', args: [message(code: 'config.label', default: 'Config'), params.id])
+            redirect(action: "list")
+        }
+        else {
+            return [configInstance: configInstance]
+        }
+    }
+
+    def update = {
+        def configInstance = Config.get(params.id)
+        if (configInstance) {
+            if (params.version) {
+                def version = params.version.toLong()
+                if (configInstance.version > version) {
+                    
+                    configInstance.errors.rejectValue("version", "default.optimistic.locking.failure", [message(code: 'config.label', default: 'Config')] as Object[], "Another user has updated this Config while you were editing")
+                    render(view: "edit", model: [configInstance: configInstance])
+                    return
+                }
+            }
+            configInstance.properties = params
+            if (!configInstance.hasErrors() && configInstance.save(flush: true)) {
+                flash.message = message(code: 'default.updated.message', args: [message(code: 'config.label', default: 'Config'), configInstance.id])
+                redirect(action: "show", id: configInstance.id)
+            }
+            else {
+                render(view: "edit", model: [configInstance: configInstance])
+            }
+        }
+        else {
+            flash.message = message(code: 'default.not.found.message', args: [message(code: 'config.label', default: 'Config'), params.id])
+            redirect(action: "list")
+        }
+    }
+
+    def delete = {
+        def configInstance = Config.get(params.id)
+        if (configInstance) {
+            try {
+                configInstance.delete(flush: true)
+                flash.message = message(code: 'default.deleted.message', args: [message(code: 'config.label', default: 'Config'), params.id])
+                redirect(action: "list")
+            }
+            catch (org.springframework.dao.DataIntegrityViolationException e) {
+                flash.message = message(code: 'default.not.deleted.message', args: [message(code: 'config.label', default: 'Config'), params.id])
+                redirect(action: "show", id: params.id)
+            }
+        }
+        else {
+            flash.message = message(code: 'default.not.found.message', args: [message(code: 'config.label', default: 'Config'), params.id])
+            redirect(action: "list")
+        }
+    }
+}
---
+++ Portal2/grails-app/views/config/show.gsp
@@ -0,0 +1,45 @@
+
+<%@ page import="au.org.emii.portal.Config" %>
+<!doctype html>
+<html>
+	<head>
+		<meta name="layout" content="main">
+		<g:set var="entityName" value="${message(code: 'config.label', default: 'Config')}" />
+		<title><g:message code="default.show.label" args="[entityName]" /></title>
+	</head>
+	<body>
+		<a href="#show-config" class="skip" tabindex="-1"><g:message code="default.link.skip.label" default="Skip to content&hellip;"/></a>
+		<div class="nav" role="navigation">
+			<ul>
+				<li><a class="home" href="${createLink(uri: '/')}"><g:message code="default.home.label"/></a></li>
+				<li><g:link class="list" action="list"><g:message code="default.list.label" args="[entityName]" /></g:link></li>
+				<li><g:link class="create" action="create"><g:message code="default.new.label" args="[entityName]" /></g:link></li>
+			</ul>
+		</div>
+		<div id="show-config" class="content scaffold-show" role="main">
+			<h1><g:message code="default.show.label" args="[entityName]" /></h1>
+			<g:if test="${flash.message}">
+			<div class="message" role="status">${flash.message}</div>
+			</g:if>
+			<ol class="property-list config">
+			
+				<g:if test="${configInstance?.binding}">
+				<li class="fieldcontain">
+					<span id="binding-label" class="property-label"><g:message code="config.binding.label" default="Binding" /></span>
+					
+						<span class="property-value" aria-labelledby="binding-label"><g:fieldValue bean="${configInstance}" field="binding"/></span>
+					
+				</li>
+				</g:if>
+			
+			</ol>
+			<g:form>
+				<fieldset class="buttons">
+					<g:hiddenField name="id" value="${configInstance?.id}" />
+					<g:link class="edit" action="edit" id="${configInstance?.id}"><g:message code="default.button.edit.label" default="Edit" /></g:link>
+					<g:actionSubmit class="delete" action="delete" value="${message(code: 'default.button.delete.label', default: 'Delete')}" onclick="return confirm('${message(code: 'default.button.delete.confirm.message', default: 'Are you sure?')}');" />
+				</fieldset>
+			</g:form>
+		</div>
+	</body>
+</html>
