0581670ecfdfa9e29598f20c9019b36363d4890c
pmak <pmak@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2011-08-23 07:12:05 +0000

    added in change colour scale

--- Portal2/web-app/js/portal/components/detailsPanel.js
+++ Portal2/web-app/js/portal/components/detailsPanel.js
@@ -6,12 +6,35 @@ var legendImage;
 var dimension;
 var dimensionPanel;
 var ncWMSColourScalePanel;
+var colourScaleMin;
+var colourScaleMax;
 
 
 function initDetailsPanel()
 {
     legendImage = new GeoExt.LegendImage();
     ncWMSColourScalePanel = new Ext.Panel();
+    colourScaleMin = new Ext.form.TextField({
+        fieldLabel: "min",
+        enableKeyEvents: true,
+        listeners: {
+            keydown: function(textfield, event){
+                updateScale(textfield, event);
+            }
+        }
+    });
+
+    colourScaleMax = new Ext.form.TextField({
+        fieldLabel: "max",
+        enableKeyEvents: true,
+        listeners: {
+            keydown: function(textfield, event){
+                updateScale(textfield, event);
+            }
+        }
+    });
+
+    ncWMSColourScalePanel.add(colourScaleMin, colourScaleMax);
 
 // create a separate slider bound to the map but displayed elsewhere
     opacitySlider = new GeoExt.LayerOpacitySlider({
@@ -26,6 +49,7 @@ function initDetailsPanel()
 
     // create the styleCombo instance
     styleCombo = makeCombo("styles");
+    
 
     detailsPanel = new Ext.Panel({
         title: 'Layer Options'
@@ -104,6 +128,7 @@ function updateDimensions(node)
 
                 dimStore.loadData(dimData);
                 detailsPanel.add(dimCombo);
+                detailsPanel.add(ncWMSColourScalePanel);
                 detailsPanel.doLayout();
             }
             
@@ -127,7 +152,30 @@ function updateDetailsPanel(node)
 
 function makeNcWMSColourScale()
 {
-    
+    //http://ncwms.emii.org.au/ncWMS/wms?item=layerDetails&layerName=ACORN_raw_data_SAG%2FSPEED&time=2006-09-19T12%3A00%3A00.000Z&request=GetMetadata
+    /*{"units":"m/sec","bbox":["146.80064392089844","-43.80047607421875","163.8016815185547","-10.000572204589844"],
+     *"scaleRange":["-1.398761","1.249144"],"numColorBands":254,"supportedStyles":["boxfill"],"zaxis":{"units":"meters","positive":false,"values":[-5]},"datesWithData":{"2006":{"8":[1,2,3,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]}},"nearestTimeIso":"2006-09-19T12:00:00Z","timeAxisUnits":"julian","moreInfo":"","copyright":"&lt;div class=&#039;featurewhite&#039;&gt;   &lt;h4 class=&#039;getfeatureTitle&#039;&gt;Informd Storm Bay model output &lt;/h4&gt; &lt;p&gt;This project aims to provide near real-time hydrodynamic modelling of the south-east Tasmania, including the Huon and Derwent Estuaries, DEntrecasreaux Channel and Storm Bay. It is anticipated that input data provided by a sensor network deployed throughout the  region will be used by the hydrodynamic model in a data assimilating capacity. The sensors would provide sea-level, salinity and temperature data in near real-time, while the  hydrodynamic modelling system (SHOC) would then provide near real-time fields of currents, sea-level, water temperature and salinity. &lt;br&gt;For more information see the metadata links  &lt;/p&gt; &lt;p&gt;Contact: &lt;a href=&#039;mailto:Simon.Allen@csiro.au&#039;&gt;Simon.Allen@csiro.au&lt;/a&gt;&lt;br&gt; &lt;/p&gt;      &lt;a title=&#039;http://imosmest.emii.org.au/geonetwork/srv/en/metadata.show?uuid=9dfc847d-ce3b-436a-a15f-02b678829eed&#039; class=&#039;h3&#039; rel=&#039;external&#039; target=&#039;_blank&#039; href=&#039;http://imosmest.emii.org.au/geonetwork/srv/en/metadata.show?uuid=9dfc847d-ce3b-436a-a15f-02b678829eed&#039;&gt; Link to the IMOS metadata record&lt;/a&gt;&lt;br&gt;  &lt;a title=&#039;&#039; class=&#039;h3&#039; rel=&#039;external&#039; target=&#039;_blank&#039; href=&#039;http://ramadda.aodn.org.au/repository/entry/show/output:data.opendap/entryid:synth:6b43cb54-11b5-4af4-a7be-a7b5a76e8476:L3N0b3JtX2NmX25ydF90bXAubmM=/storm_cf_nrt_tmp.nc/dodsC/entry.html&#039;&gt;Data view and download on opendap&lt;/a&gt;&lt;br&gt; &lt;a title=&#039;&#039; class=&#039;h3&#039; rel=&#039;external&#039; target=&#039;_blank&#039; href=&#039;http://ramadda.aodn.org.au/repository/entry/show/Data+Repository/Model+Output/Storm+Bay?entryid=e55bd261-db1c-44ed-9282-06f8e625f4bf&#039;&gt;Data download and Info on ramada&lt;/a&gt;&lt;br&gt; &lt;a title=&#039;&#039; class=&#039;h3&#039; rel=&#039;external&#039; target=&#039;_blank&#039; href=&#039;http://ramadda.aodn.org.au/repository/entry/show/storm_cf_nrt_tmp.zip?entryid=synth%3A6b43cb54-11b5-4af4-a7be-a7b5a76e8476%3AL3N0b3JtX2NmX25ydF90bXAubmM%3D&amp;output=zip.zip&#039;&gt;Data download as netCDF&lt;/a&gt;&lt;br&gt; &lt;!--a href=&#039;http://ramadda.aodn.org.au/repository/entry/show/storm_cf_nrt_tmp.zip?entryid=synth%3A6b43cb54-11b5-4af4-a7be-a7b5a76e8476%3AL3N0b3JtX2NmX25ydF90bXAubmM%3D&amp;output=zip.zip&#039;  target=&#039;_blank&#039;  rel=&#039;external&#039; class=&#039;h3&#039; title=&#039;&#039;&gt;Data download as netCDF&lt;/a&gt;&lt;BR--&gt;    &lt;/div&gt;",
+     *"palettes":["redblue","alg","ncview","greyscale","alg2","occam","rainbow","sst_36","ferret","occam_pastel-30"],"defaultPalette":"rainbow","logScaling":false}*/
+     alert(detailsPanelLayer.url + "item=layerDetails&request=GetMetadata&layerName=" + detailsPanelLayer.params.LAYERS);
+
+     if(detailsPanelLayer.params.colourScale != undefined)
+     {
+         alert("already defined!");
+     }
+     else
+     {
+         alert("grabbing it from the server!")
+         Ext.Ajax.request({
+            url: proxyURL+encodeURIComponent(detailsPanelLayer.url + "?item=layerDetails&request=GetMetadata&layerName=" + detailsPanelLayer.params.LAYERS),
+            success: function(resp){
+                var metadata = Ext.util.JSON.decode(resp.responseText);
+                colourScaleMin.setValue(metadata.scaleRange[0]);
+                colourScaleMax.setValue(metadata.scaleRange[1]);
+            }
+         });
+     }
+
+     
 }
 
 function makeCombo(type)
@@ -176,4 +224,18 @@ function makeCombo(type)
     });
     
     return combo;
+}
+
+function updateScale(textfield, event)
+{
+    //return key
+    if(event.getKey() == 13)
+    {
+        detailsPanelLayer.mergeNewParams({
+            COLORSCALERANGE: colourScaleMin.getValue() + "," + colourScaleMax.getValue()
+        });
+        //COLORSCALERANGE=9.405%2C50&NUMCOLORBANDS=254
+        //alert(colourScaleMin.getValue());
+        //alert(colourScaleMax.getValue());
+    }
 }
