67a1edb95ea54e7b17a6e9aa53b64c727e8b51ca
pblain <pblain@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2011-09-13 01:04:17 +0000

    Remove 1.37 style test

--- test/unit/au/org/emii/portal/ConfigImplTests.groovy
+++ test/unit/au/org/emii/portal/ConfigImplTests.groovy
@@ -4,25 +4,15 @@ import grails.test.*
 
 class ConfigImplTests extends GrailsUnitTestCase {
 	
-	def redirectParams
 	
     protected void setUp() {
         super.setUp()
-		redirectParams = [ : ]
-		ConfigController.metaClass.redirect = { Map args -> redirectParams = args  }
     }
 
     protected void tearDown() {
         super.tearDown()
-		def remove = GroovySystem.metaClassRegistry.&removeMetaClass
-		remove ConfigController
     }
 
-	void testIndexRedirect() {
-		ConfigController cc = new ConfigController()
-		cc.index()
-		assertEquals "edit" , redirectParams.action
-	}
 	
     void testSomething() {
 
