781f616ae2a82ae1c4d842013c5aeec123b4a891
dnahodil <dnahodil@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2011-10-10 04:52:04 +0000

    - unit test for Config.activeInstance

--- Portal2/test/unit/au/org/emii/portal/ConfigTests.groovy
+++ Portal2/test/unit/au/org/emii/portal/ConfigTests.groovy
@@ -11,40 +11,44 @@ class ConfigTests extends GrailsUnitTestCase {
         super.tearDown()
     }
 	
-	void testConstraints() {
-		def config1 = new Config(name : "config1");
-		mockForConstraintsTests(Config, [config1])
-		
-		def testConfig = new Config()
-		assertFalse testConfig.validate()
-		assertEquals "nullable", testConfig.errors["name"]
-		assertEquals "nullable", testConfig.errors["initialBbox"]
-		assertEquals "nullable", testConfig.errors["defaultMenu"]
-		assertEquals "nullable", testConfig.errors["contributorMenu"]
-		assertEquals "nullable", testConfig.errors["regionMenu"]
-		
-		testConfig = new Config(name : "config1")
-		assertFalse testConfig.validate()
-		assertEquals "unique", testConfig.errors["name"]
-		
-		testConfig = new Config(name : "1234")
-		assertFalse testConfig.validate()
-		assertEquals "size", testConfig.errors["name"]
-		
-		testConfig = new Config(name : "12345678901234567890123456")
-		assertFalse testConfig.validate()
-		assertEquals "size", testConfig.errors["name"]
-		
-		testConfig = new Config(initialBbox : "123456789")
-		assertFalse testConfig.validate()
-		assertEquals "size", testConfig.errors["initialBbox"]
-		
-		testConfig = new Config(initialBbox : "123456789012345678901234567890123456789012345678901")
-		assertFalse testConfig.validate()
-		assertEquals "size", testConfig.errors["initialBbox"]
-		
-	}
-	
+    void testConstraints() {
+        def config1 = new Config(name : "config1");
+        mockForConstraintsTests(Config, [config1])
 
+        def testConfig = new Config()
+        assertFalse testConfig.validate()
+        assertEquals "nullable", testConfig.errors["name"]
+        assertEquals "nullable", testConfig.errors["initialBbox"]
+        assertEquals "nullable", testConfig.errors["defaultMenu"]
+        assertEquals "nullable", testConfig.errors["contributorMenu"]
+        assertEquals "nullable", testConfig.errors["regionMenu"]
 
+        testConfig = new Config(name : "config1")
+        assertFalse testConfig.validate()
+        assertEquals "unique", testConfig.errors["name"]
+
+        testConfig = new Config(name : "1234")
+        assertFalse testConfig.validate()
+        assertEquals "size", testConfig.errors["name"]
+
+        testConfig = new Config(name : "12345678901234567890123456")
+        assertFalse testConfig.validate()
+        assertEquals "size", testConfig.errors["name"]
+
+        testConfig = new Config(initialBbox : "123456789")
+        assertFalse testConfig.validate()
+        assertEquals "size", testConfig.errors["initialBbox"]
+
+        testConfig = new Config(initialBbox : "123456789012345678901234567890123456789012345678901")
+        assertFalse testConfig.validate()
+        assertEquals "size", testConfig.errors["initialBbox"]
+    }
+
+    void testActiveInstance() {
+        def config1 = new Config(name: "config1");
+        def config2 = new Config(name: "config2");
+        mockDomain(Config, [config1, config2])
+        
+        assertEquals "Should return first config", config1, Config.activeInstance()
+    }
 }
