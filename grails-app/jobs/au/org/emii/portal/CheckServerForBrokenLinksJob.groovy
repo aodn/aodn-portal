01e2dd932e04f8fe8e5e27154025d808aa56cada
pblain <pblain@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2011-10-18 22:39:04 +0000

    Broken link checker.

---
+++ grails-app/jobs/au/org/emii/portal/CheckServerForBrokenLinksJob.groovy
@@ -0,0 +1,26 @@
+package au.org.emii.portal
+
+
+class CheckServerForBrokenLinksJob {
+	
+		def repeatCount = 0
+
+		def execute(context) {
+			if (context.mergedJobDataMap.get('serverId') == null) {
+				return
+			}
+			   
+			println "Starting CheckServerForBrokenLinksJob with server id = " + context.mergedJobDataMap.get('serverId')
+			
+			def foundServer = Server.get(context.mergedJobDataMap.get('serverId'))
+			
+			if (!foundServer.type.contains("WMS")) {
+				return
+			}
+			
+			println "uri = " + foundServer.uri + " type = " + foundServer.type
+		}
+	  }
+	
+	
+	
