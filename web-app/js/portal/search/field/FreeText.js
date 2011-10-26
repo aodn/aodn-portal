2d1fc6e156a2a44a1faf59f06dda07e5f5714dcb
craigj <craigj@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2011-10-26 02:57:05 +0000

    #782 - Fix issue with autocompletion not working where case is different.   Upgrade combobox to SuperBoxSelect allowing multiple selection.

--- web-app/js/portal/search/field/FreeText.js
+++ web-app/js/portal/search/field/FreeText.js
@@ -1,7 +1,7 @@
 Ext.namespace('Portal.search.field');
 
 Portal.search.field.FreeText = Ext.extend(Ext.form.TextField, {
-   width: 250,
+   width: 350,
    fieldLabel: OpenLayers.i18n('fullTextSearch'),
    name: 'any'   
 });
