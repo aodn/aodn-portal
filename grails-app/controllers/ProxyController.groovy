2b7de1b56edf7f87916e29b11e0b73d901c277a6
dnahodil <dnahodil@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2011-11-01 21:57:54 +0000

    #709 - Updated ProxyController to better handle errors - Changes to how FeatureInfo popup works

--- grails-app/controllers/ProxyController.groovy
+++ grails-app/controllers/ProxyController.groovy
@@ -1,58 +1,42 @@
-
 class ProxyController {
 
-    def DEBUG = true; // todo remove
-
+    def REMOVE_THIS_TO_ENABLE_HOST_LIST_FILTERING = true
+    
     def index = {
 
-        if (params.url) {
+        if ( params.url ) {
 
-           //exclude use to certain hosts
-           def hostList = ['geoserver.emii.org.au','geoserverdev.emii.org.au'] // get values from config
-           def format
-
+            //exclude use to certain hosts
+            def hostList = ['geoserver.emii.org.au','geoserverdev.emii.org.au'] // get values from config
+            
+            // Get the doamin name from the target uri
+            def targetUrl = params.url.toURL()
            
-           // get the doamin name from the supplied uri
-           def hostName =  params.url.toURL().getHost()
-           
-            if (DEBUG || hostList.contains(hostName)) {
+            if ( REMOVE_THIS_TO_ENABLE_HOST_LIST_FILTERING || hostList.contains( targetUrl.getHost() ) ) {
 
-                 def thetext = params.url.toURL()
-                 def conn = thetext.openConnection()
-                 def type = conn.contentType
-                 def incomingFormat
-                 def incomingEncoding = "UTF-8"
-                /*def typeSplit = type.split(';')
-
-                if(type != null)
-                {
-                    incomingFormat = typeSplit[0]
-                 
-                    if(typeSplit.length == 2)
-                        incomingEncoding = typeSplit[1].split('=')[1]
+                def conn = targetUrl.openConnection()
+                
+                def format = params.format == "xml" ? "text/xml" \
+                                                    : "text/html"
+                
+                log.debug "TargetUrl: $targetUrl (expected type: $format)"
+              
+                try {
+                    render( text: targetUrl.text, contentType: format, encoding: "UTF-8" )
                 }
-                 
-                 println("type:" + typeSplit[0])*/
-                 log.info("Proxy: The url to be requested " + thetext)
-                 if (params.format == "xml") {
-                     format = "text/xml"
-                 }
-                 else {
-                     format = "text/html"
-                 }
-                 println(format + '   --matias---    '+thetext)
-                 render(text: thetext.text ,contentType:format,encoding:incomingEncoding)
+                catch (Exception e) {
+                    
+                    log.debug "Exception occurred: $e"
+                    render text: "An error occurred making request", status: 500
+                }
            }
            else {
-               log.error("Proxy: The url " + hostName + "was not allowed")
-               render(text: "Host not allowed",contentType:"text/html",encoding:"UTF-8")
+               log.error "Proxy: The url ${params.url} was not allowed"
+               render text: "Host not allowed", contentType: "text/html", encoding: "UTF-8"
            }
-
         }
         else {
-             render(text: "No URL supplied",contentType:"text/html",encoding:"UTF-8")
+             render( text: "No URL supplied", contentType: "text/html", encoding: "UTF-8" )
         }
-
     }
-
 }
