b9c850e7bf59d128aa3c3769b3f59c9bf61815be
tfotak <tfotak@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2011-11-29 05:17:56 +0000

    #692 #773
    Integration of spatial search into portal
    
    Also added the ability to access grails config options from JS code, see the
    HomeController.config action for info on how to expose more options.

---
+++ web-app/js/portal/common/AppConfigStore.js
@@ -0,0 +1,9 @@
+var appConfigStore = new Ext.data.JsonStore({
+    url: 'home/config',
+    storeId: 'appConfigStore',
+    // reader configs
+    root: 'grailsConfig',
+    idProperty: 'name',
+    fields: ['name', 'value']
+});
+appConfigStore.load()
