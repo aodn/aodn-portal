9ae18e2df333067bb6611d5e4917df4c2dde9a5e
dnahodil <dnahodil@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2012-01-09 03:19:03 +0000

    #646 - WMS Scanner -> Portal API work - Added debugging method to Layer domain, uses 'println' will change to use Log4J later

---
+++ Portal2/test/unit/au/org/emii/portal/LayerDeserializeServiceTests.groovy
@@ -0,0 +1,17 @@
+package au.org.emii.portal
+
+import grails.test.*
+
+class LayerDeserializeServiceTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
