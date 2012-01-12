10d826753fc149b8dafb5f5fecb15d35b4c373ea
pblain <pblain@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2012-01-12 06:02:33 +0000

    A couple of Server Controller tests

--- test/unit/au/org/emii/portal/ServerControllerTests.groovy
+++ test/unit/au/org/emii/portal/ServerControllerTests.groovy
@@ -11,7 +11,23 @@ class ServerControllerTests extends ControllerUnitTestCase {
         super.tearDown()
     }
 
-    void testSomething() {
-
+	void testIndex() {
+		this.controller.index()
+		assertEquals "list", this.controller.redirectArgs["action"]
+	}
+	
+	void testList() {
+		
+		
+		
+	}
+	
+    void testCheckForBrokenLinks() {
+		def checkLinksServiceControl = mockFor(CheckLinksService)
+		checkLinksServiceControl.demand.check(1..1) {String serverId -> "Result string"}
+		this.controller.checkLinksService = checkLinksServiceControl.createMock()
+		this.controller.params.server = "10"
+		this.controller.checkForBrokenLinks()
+		assertTrue this.controller.response.contentAsString.equals("Result string")
     }
 }
