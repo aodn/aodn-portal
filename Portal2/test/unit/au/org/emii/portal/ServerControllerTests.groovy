e580401a3b3eae4fdc7c1c0d707cb726c97c55a4
dnahodil <dnahodil@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2012-01-17 02:12:49 +0000

    - Fixed Server Unit tests which broke with recent changes to constraints

--- Portal2/test/unit/au/org/emii/portal/ServerControllerTests.groovy
+++ Portal2/test/unit/au/org/emii/portal/ServerControllerTests.groovy
@@ -18,12 +18,12 @@ class ServerControllerTests extends ControllerUnitTestCase {
 	
 
 	void testShowServerByItsId() {
-		mockDomain(Server, [new Server(id : 10, uri : "uri1", shortAcron : "A", name : "name1", type : "WMS-1.1.1", parseDate: null, parseFrequency : "", disable : false, allowDiscoveries : true, opacity : 3, imageFormat : "image/png", comments : "" )])
+		mockDomain(Server, [new Server(id : 10, uri : "uri1", shortAcron : "A", name : "name1", type : "WMS-1.1.1", parseDate: null, parseFrequency : 0, disable : false, allowDiscoveries : true, opacity : 3, imageFormat : "image/png", comments : "" )])
 		this.controller.params.serverId = "10_10"
 		this.controller.showServerByItsId()
 		String scum = this.controller.response.contentAsString
 		scum = this.controller.response.contentAsString
-		assertTrue this.controller.response.contentAsString.equals('{"allowDiscoveries":true,"class":"au.org.emii.portal.Server","comments":"","disable":false,"id":10,"imageFormat":"image/png","name":"name1","opacity":3,"parseDate":null,"parseFrequency":"","shortAcron":"A","type":"WMS-1.1.1","uri":"uri1","version":null}')
+		assertTrue this.controller.response.contentAsString.equals('{"allowDiscoveries":true,"class":"au.org.emii.portal.Server","comments":"","disable":false,"id":10,"imageFormat":"image/png","name":"name1","opacity":3,"parseDate":null,"parseFrequency":0,"shortAcron":"A","type":"WMS-1.1.1","uri":"uri1","version":null}')
 	}
 	
 	
