a0deb18c738e72b1766d0c88cc8b197e5208bdfb
pblain <pblain@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2012-01-17 05:07:55 +0000

    Unit tests

--- test/unit/au/org/emii/portal/DepthControllerTests.groovy
+++ test/unit/au/org/emii/portal/DepthControllerTests.groovy
@@ -11,7 +11,35 @@ class DepthControllerTests extends ControllerUnitTestCase {
         super.tearDown()
     }
 
-    void testSomething() {
-
+    void testIndexIncorrectParams() {
+		this.controller.index()
+		assertTrue this.controller.response.contentAsString.equals("incorrect parameters supplied")
     }
+	
+	void testIndexServiceUnavailable() {
+		this.controller.params.lat = 10
+		this.controller.params.lon = 20
+		def configControl = mockFor(Config)
+		def mockConfig = configControl.createMock()
+		configControl.demand.static.activeInstance(1..1) { -> mockConfig}
+		configControl.demand.getUseDepthService(1..1) { -> null}
+		this.controller.index()
+		assertTrue this.controller.response.contentAsString.equals("This service is unavailable")
+	}
+	
+	void testIndex() {
+		this.controller.params.lat = 10
+		this.controller.params.lon = 20
+		def depthServiceConfigControl = mockFor(DepthService)
+		depthServiceConfigControl.demand.getNearestDepth(1..1) {Map params -> "depth service text"}
+		this.controller.depthService = depthServiceConfigControl.createMock()
+		def configControl = mockFor(Config)
+		def mockConfig = new Config()
+		mockConfig.useDepthService = true;
+		configControl.demand.static.activeInstance(1..1) { -> mockConfig}
+		this.controller.index()
+		configControl.verify()
+		assertTrue this.controller.response.contentAsString.equals("depth service text")
+	}
+	
 }
