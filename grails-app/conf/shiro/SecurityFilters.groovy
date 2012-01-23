63ab868caf4390c1539547fb9ea02483f4932850
tfotak <tfotak@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2012-01-23 04:56:23 +0000

    Added calls for server discoveries to publicly accessible actions

--- grails-app/conf/shiro/SecurityFilters.groovy
+++ grails-app/conf/shiro/SecurityFilters.groovy
@@ -47,7 +47,7 @@ class SecurityFilters {
             }
         }
         
-        layerAccess(controller: "layer", action: "list|listBaseLayersAsJson|listNonBaseLayerAsJson|showLayerByItsId|saveOrUpdate") {
+        layerAccess(controller: "layer", action: "list|listBaseLayersAsJson|listNonBaseLayerAsJson|showLayerByItsId|saveOrUpdate|server") {
             before = {
                 
                 logRequest("layerAccess", controllerName, actionName)
