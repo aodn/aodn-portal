91647ef7c01ec303dc21ec10e17861d28d527e28
dnahodil <dnahodil@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2012-02-09 05:43:47 +0000

    - Undoing crazy panic commit. My bad.

--- Portal2/grails-app/conf/DataSource.groovy
+++ Portal2/grails-app/conf/DataSource.groovy
@@ -39,7 +39,7 @@ environments {
 	   dataSource {
 		   dbCreate = "create-drop"
 		   driverClassName = "org.postgresql.Driver"
-		   //url = "jdbc:postgresql://dbdev.emii.org.au:5432/portal2_diff?ssl=true&tcpKeepAlive=true&sslfactory=org.postgresql.ssl.NonValidatingFactory" // Taken out for saftey for now (don't ask). Will re-add after. DO NOT USE!!
+		   url = "jdbc:postgresql://dbdev.emii.org.au:5432/portal2_diff?ssl=true&tcpKeepAlive=true&sslfactory=org.postgresql.ssl.NonValidatingFactory"
 		   username = "portal2user"
 		   password = "wtingzgsfdbat"
 	   }
