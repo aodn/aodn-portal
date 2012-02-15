e1efd7649dd6fd8f5947d3f5580c605aeee84836
tfotak <tfotak@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2012-02-15 00:54:34 +0000

    Fix - Somehow this included changelog got removed by moi I would assume.
    Needed to ensure that menu items with orphaned layers/servers are removed.

--- Portal2/grails-app/migrations/changelog.groovy
+++ Portal2/grails-app/migrations/changelog.groovy
@@ -2484,7 +2484,9 @@ So far this is all exactly as per the Grails docs (which go into much more detai
 	include file: '1328160201244.groovy'
 	
 	include file: '1327970556000.groovy'
-    
+	
+	include file: '1328227661000.groovy'
+	
 	include file: '20120203-DN-AddingNamespaceToLayer.groovy'        
 
 	include file: '20120203-CJ-MakeSnapshotBboxFloat.groovy'
