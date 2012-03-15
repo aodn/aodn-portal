2bed8a8c6cf06fc7ae686b55fe683fcd67466877
dnahodil <dnahodil@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2012-03-15 02:51:16 +0000

    - Removing usage of grails.serverURL as Grails will use it automatically (we only had to specify the base because we were using our own)

--- grails-app/controllers/shiro/AuthController.groovy
+++ grails-app/controllers/shiro/AuthController.groovy
@@ -154,8 +154,8 @@ class AuthController {
             subject message(code: 'mail.request.user.register.subject')
             body message(code: 'mail.request.user.register.body',
                          args: [user.firstName,
-                                createLink(controller:'home', baseUrl: grailsApplication.config.grails.serverURL, absolute: true),
-                                createLink(controller: 'auth', action: 'forgotPassword', baseUrl: grailsApplication.config.grails.serverURL, absolute: true)])
+                                createLink(controller:'home', absolute: true),
+                                createLink(controller: 'auth', action: 'forgotPassword', absolute: true)])
         }
     }
     
@@ -169,7 +169,7 @@ class AuthController {
             body message(code: 'mail.request.user.passwordReset.body',
                          args: [user.firstName,
                                 newPassword,
-                                createLink(controller: 'user', action:'updateAccount', baseUrl: grailsApplication.config.grails.serverURL, absolute: true)])
+                                createLink(controller: 'user', action:'updateAccount', absolute: true)])
         }
     }
 }
