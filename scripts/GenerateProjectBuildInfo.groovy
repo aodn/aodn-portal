793b458dbe4d8dd975921d2f5a92297d9a84519a
dnahodil <dnahodil@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2012-03-26 01:05:42 +0000

    Tweaked output

--- scripts/GenerateProjectBuildInfo.groovy
+++ scripts/GenerateProjectBuildInfo.groovy
@@ -1,28 +1,32 @@
 includeTargets << grailsScript( "Init" )
 
 target( main: "Collects project build info from environment variables and injects it into the Portal" ) {
-    
+
     println "-- Generate Project Build Info script running --"
 
-    println "Reading environment variables"
-    
+    println "Gathering data"
+
     def buildDate = new Date().format( "dd/MM/yyyy HH:mm" )
     def svnRevision = System.getenv( "SVN_REVISION" ) ?: "unk."
     def buildNumber = System.getenv( "BUILD_NUMBER" ) ?: "unk."
 
+    println "Compiling template"
+
     def replacementString = """\
 <%@ page import=\"grails.util.Environment\" %>\
 <b>Portal project build info</b>
-Instance name: \${ grailsApplication.config.instanceName?:"unk." }
+Instance name: \${ grailsApplication.config.instanceName ?: "unk." }
 Environment: \${ Environment.current }
 Build date: $buildDate
 Subversion revision: #$svnRevision
 Build: #$buildNumber
 App version: <g:meta name=\"app.version\"/>\
 """
-    
-    println "replacementString:\n$replacementString"
 
+    println "..... Template ${ "." * 61 }"
+    println replacementString
+    println "." * 76
+    
     def outputFile = new File( "grails-app/views/_projectInfo.gsp" )
 
     if ( outputFile ) {
@@ -30,7 +34,7 @@ App version: <g:meta name=\"app.version\"/>\
         outputFile.text = replacementString.replace( "\n", "<br />\n" )
     }
     else {
-        
+
         println "Could not find output file. No changes made."
     }
 
