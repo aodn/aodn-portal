063ceb1d4e57ad768df62189d73e2640470fcc84
dnahodil <dnahodil@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2012-03-28 04:51:39 +0000

    - Updating build info script

--- scripts/GenerateProjectBuildInfo.groovy
+++ scripts/GenerateProjectBuildInfo.groovy
@@ -1,37 +1,44 @@
 includeTargets << grailsScript( "Init" )
 
 target( main: "Collects project build info from environment variables and injects it into the Portal" ) {
-    
+
     println "-- Generate Project Build Info script running --"
 
-    println "Reading environment variables"
-    
+    println "Gathering data"
+
     def buildDate = new Date().format( "dd/MM/yyyy HH:mm" )
     def svnRevision = System.getenv( "SVN_REVISION" ) ?: "unk."
+    def svnUrl = System.getenv( "SVN_URL" ) ?: "unk."
     def buildNumber = System.getenv( "BUILD_NUMBER" ) ?: "unk."
 
-    def replacementString = """\
-<%@ page import=\"grails.util.Environment\" %>\
-<b>Portal project build info</b>
-Instance name: \${ grailsApplication.config.instanceName?:"unk." }
-Environment: \${ Environment.current }
-Build date: $buildDate
-Subversion revision: #$svnRevision
-Build: #$buildNumber
+    println "Compiling template"
+
+    def templateText = """\
+<%@ page import=\"grails.util.Environment\" %>
+<b>Portal project build info</b><br />
+Instance name: \${ grailsApplication.config.instanceName ?: '<span style="color: red;">None supplied</span>' }<br />
+Environment: \${ Environment.current }<br />
+Build date: $buildDate<br />
+Subversion revision: #$svnRevision<br />
+Subversion url: $svnUrl<br />
+Build: #$buildNumber<br />
 App version: <g:meta name=\"app.version\"/>\
 """
-    
-    println "replacementString:\n$replacementString"
+
+    println "..... Template ${ "." * 61 }"
+    println templateText
+    println "." * 76
 
     def outputFile = new File( "grails-app/views/_projectInfo.gsp" )
 
-    if ( outputFile ) {
+    if ( outputFile.canWrite() ) {
 
-        outputFile.text = replacementString.replace( "\n", "<br />\n" )
+        outputFile.text = templateText
+        println "Template saved to $outputFile"
     }
     else {
-        
-        println "Could not find output file. No changes made."
+
+        println "Could not write to output file. No changes made."
     }
 
     println "-- Generate Project Build Info script complete --"
