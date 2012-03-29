5fa64d3b101fcb87dfa2031c045f562a65d160c7
craigj <craigj@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2012-03-29 05:14:17 +0000

    #1302 - Allow registered users access to snapshot actions

--- Portal2/grails-app/migrations/changelog.groovy
+++ Portal2/grails-app/migrations/changelog.groovy
@@ -6,8 +6,10 @@ databaseChangeLog = {
 	
 	include file: '20120321-DN-TweaksToDefaultConfig.groovy'
 	
-	include file: '20120326-DN-SelfRegisteredUserPermissions.groovy'
-	
+    include file: '20120326-DN-SelfRegisteredUserPermissions.groovy'
+    
+    include file: '20120329-CJ-AddSelfRegisteredUserSnapshotPermissions.groovy'
+    
 	// Changes that apply to all instances must be included here, above the calls to instance-specific change logs
 	
 	// Instance-specific changes
