2c8aaa3b2946e345d739c035cbec3311b432d161
pblain <pblain@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2012-05-03 23:53:54 +0000

    Added ?<version> to the script src link to prevent cached javascript being used after redeploy. Also added version, build, date comment to help in testing/debugging.

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -2,3 +2,25 @@ eventCreateWarStart = { warname, stagingDir ->
     Ant.delete(file: "${stagingDir}/WEB-INF/lib/postgresql-9.0-801.jdbc3.jar")
 }
 
+
+eventCompileStart = { kind ->
+	def buildNumber = metadata.'app.buildNumber'
+ 
+	if (!buildNumber)
+		buildNumber = 1
+	else
+		buildNumber = Integer.valueOf(buildNumber) + 1
+ 
+	metadata.'app.buildNumber' = buildNumber.toString()
+
+	def formatter = new java.text.SimpleDateFormat("MMM dd, yyyy")
+	def buildDate = formatter.format(new Date(System.currentTimeMillis()))
+	metadata.'app.buildDate' = buildDate
+	metadata.'app.buildProfile' = grailsEnv
+		
+	
+	metadata.persist()
+ 
+	println "Compile Starting on Build #${buildNumber}"
+}
+
