ff13bdfa3a76920ac753f48e99782ba990057b08
tfotak <tfotak@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2012-05-16 02:11:17 +0000

    Deleting AuthControllerTest class
    Was not present on OpenID branch
    Updated CheckServerForBrokenLinksJobTests to relieve a compiler error.

--- test/unit/au/org/emii/portal/CheckServerForBrokenLinksJobTests.groovy
+++ test/unit/au/org/emii/portal/CheckServerForBrokenLinksJobTests.groovy
@@ -10,7 +10,6 @@ import org.springframework.context.ApplicationContext
 class CheckServerForBrokenLinksJobTests extends GrailsUnitTestCase {
 	
 	
-	def CheckServerForBrokenLinksJob testJob
 	def grailsApplication
 	
 	protected void setUp() {
