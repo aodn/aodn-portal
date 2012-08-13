a4aa4dfdd368b184dcbd6b6398dca31a7eec56fb
pmbohm <pmbohm@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2012-08-13 22:54:11 +0000

    #1814  layer availability tests

--- test/unit/au/org/emii/portal/CheckLayerAvailabilityServiceTests.groovy
+++ test/unit/au/org/emii/portal/CheckLayerAvailabilityServiceTests.groovy
@@ -10,57 +10,12 @@ class CheckLayerAvailabilityServiceTests extends GrailsUnitTestCase {
 	
     protected void setUp() {
         super.setUp()
-		service = new CheckLayerAvailabilityService()
-		mockDomain(Layer)
-		mockDomain(Server)
     }
 
     protected void tearDown() {
         super.tearDown()
     }
-	void testGetLayerDetailsUnknownId() {
-		
-		
-		assertNull(service._getLayerDetails("67"))
-		
-		
-	}
-	void testGetLayerDetailsKnownId() {
-		
-		Server server = new Server(uri: "something1", shortAcron: "sa1", id: 1, type:"WMS-1.1.1", scanFrequency: 100,
-		name: "something1name", disable: false, opacity: 100, imageFormat: "image/gif",	allowDiscoveries:true)
-		server.save(failOnError: true)
-		assertNotNull(server.id)
-		
-		Layer layer = new Layer(server: server, dataSource: "hello mum")
-		layer.save(failOnError: true)
-		assertNotNull(layer.id)
-		
-		def res = service._getLayerDetails(layer.id as String)
-		
-		assertEquals(layer, res.layer)
-		assertEquals(server, res.server)
-		
-		//assertNull(service._getLayerDetails("67"))
-		
-		/*mockDomain(Layer, [new Layer(id:67)])
-		mockDomain(Server)	
-		
-		
-		def Server server = new Server(uri: "something1", shortAcron: "sa1", id: 1, type:"WMS-1.1.1", scanFrequency: 100,
-		name: "something1name", disable: false, opacity: 100, imageFormat: "image/gif",
-		allowDiscoveries:true)
-		server.save(failOnError: true,flush:true)
-
-		
-		
-
-
-		
-		assertEquals("67", checkLayerAvailabilityService._getLayerDetails("67"))
-		*/
-		
-	}
+	
 
 
 }
