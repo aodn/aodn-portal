a622ea0650adcd3fab1e6441c8a1e397fdd46669
amckeown <amckeown@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2012-09-18 06:45:48 +0000

    #1984 Made Minsize of  details panel equal default size, if people want more room they can minimize.

--- web-app/js/portal/ui/RightDetailsPanel.js
+++ web-app/js/portal/ui/RightDetailsPanel.js
@@ -1,101 +1,99 @@
 Ext.namespace('Portal.ui');
 
-//TODO: add something here to hide/show 'no layer selected' if there are no active layers on the map
+// TODO: add something here to hide/show 'no layer selected' if there are no
+// active layers on the map
 Portal.ui.RightDetailsPanel = Ext.extend(Ext.Panel, {
-	
-	constructor: function(cfg) {
-    	var config = Ext.apply({
-    		id: 'rightDetailsPanel',
-    		region: 'east',
-    		title: OpenLayers.i18n('noActiveLayersSelected'),
-    		//collapsed: true,
-    		stateful: false,
-    		padding:  '10px 10px 5px 20px',
-    		split: true,
-    		width: 350,
-    		minWidth: 250,
-    		layout: 'fit'
-    	}, cfg);
-        
-        Portal.ui.RightDetailsPanel.superclass.constructor.call(this, config);
-    },
 
-	// check whether the panel has been rendered before calling the default expand method
-	expand: function() {
-	  if (this.rendered) {  	
-		Portal.ui.RightDetailsPanel.superclass.expand.call(this, arguments);
-	  }
+	constructor : function(cfg) {
+		var config = Ext.apply({
+					id : 'rightDetailsPanel',
+					region : 'east',
+					title : OpenLayers.i18n('noActiveLayersSelected'),
+					// collapsed: true,
+					stateful : false,
+					padding : '10px 10px 5px 20px',
+					split : true,
+					width : 350,
+					minWidth : 350,
+					maxWidth : 500,
+					layout : 'fit'
+				}, cfg);
+
+		Portal.ui.RightDetailsPanel.superclass.constructor.call(this, config);
 	},
-	expandFinished: function() {
-		
-		// should always be set, but a user could open the panel before adding layers!!		
+
+	// check whether the panel has been rendered before calling the default
+	// expand method
+	expand : function() {
+		if (this.rendered) {
+			Portal.ui.RightDetailsPanel.superclass.expand.call(this, arguments);
+		}
+	},
+	expandFinished : function() {
+
+		// should always be set, but a user could open the panel before adding
+		// layers!!
 		if (this.selectedLayer != null) {
-			this.detailsPanelItems.updateDetailsPanel(this.selectedLayer,true);
-		}		
+			this.detailsPanelItems.updateDetailsPanel(this.selectedLayer, true);
+		}
 	},
 
-	initComponent: function(){
+	initComponent : function() {
 		this.detailsPanelItems = new Portal.details.DetailsPanel();
 		this.selectedLayer = null;
 		this.lockedPanel = false;
-		
-	    this.items = [
-			this.detailsPanelItems
-		];
-		
+
+		this.items = [this.detailsPanelItems];
+
 		this.detailsPanelItems.hideDetailsPanelContents();
-        Portal.ui.RightDetailsPanel.superclass.initComponent.call(this);		
-		this.on("expand",this.expandFinished,this);
-		
+		Portal.ui.RightDetailsPanel.superclass.initComponent.call(this);
+		this.on("expand", this.expandFinished, this);
+
 	},
 
-	getDetailsPanelItems: function(){
+	getDetailsPanelItems : function() {
 		return this.detailsPanelItems;
 	},
 
-	setSelectedLayer: function(openLayer){
+	setSelectedLayer : function(openLayer) {
 		this.selectedLayer = openLayer;
 	},
-	
-	_onChangeLayer: function(evt)
-	{
-		if(evt.layer==this.selectedLayer && evt.property == "name")
-		{
+
+	_onChangeLayer : function(evt) {
+		if (evt.layer == this.selectedLayer && evt.property == "name") {
 			this.update(evt.layer);
 		}
 	},
 	// a new layer has been added or selected
-	update: function(openlayer){
+	update : function(openlayer) {
 		this.selectedLayer = openlayer;
 		openlayer.map.events.on({
-			"changelayer" : this._onChangeLayer,
-			scope : this
-		});
-		
+					"changelayer" : this._onChangeLayer,
+					scope : this
+				});
+
 		this.text = openlayer.name;
 		this.setTitle(openlayer.name);
-		
+
 		// show new layer details unless user requested 'hideLayerOptions' or
-		// || !viewport.isMapVisible() check if the map is still in focus - not the search
-		if (!(Portal.app.config.hideLayerOptions === true  )) {
-			
-			if(this.collapsed ) {
-				// will updateDetailsPanel after expand					
+		// || !viewport.isMapVisible() check if the map is still in focus - not
+		// the search
+		if (!(Portal.app.config.hideLayerOptions === true)) {
+
+			if (this.collapsed) {
+				// will updateDetailsPanel after expand
 				this.expand();
-			}
-			else {
+			} else {
 				this.detailsPanelItems.updateDetailsPanel(openlayer);
 			}
-		}
-		else {
+		} else {
 			this.detailsPanelItems.updateDetailsPanel(openlayer);
-			this.collapse(true);		
-		}		
+			this.collapse(true);
+		}
 	},
 
-
 	// call only when there are no layers in the map
-	collapseAndHide: function(){
+	collapseAndHide : function() {
 		this.setTitle(OpenLayers.i18n('noActiveLayersSelected'));
 		this.collapse(true);
 		this.detailsPanelItems.hideDetailsPanelContents();
