808b9deb3ce473a8b0c0a3efe3fb3a42411a042e
dnahodil <dnahodil@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2012-09-25 05:20:21 +0000

    - Fixed String/GString comparison problem

--- grails-app/services/au/org/emii/portal/LayerService.groovy
+++ grails-app/services/au/org/emii/portal/LayerService.groovy
@@ -190,7 +190,7 @@ class LayerService {
         def titlePart = layer.title ?: "<no title>"
         def parentPart = parent ? _uniquePathIdentifier( parent, parent.parent ) + " // " : ""
 
-        return "$parentPart$namePart -- $titlePart"
+        return "$parentPart$namePart -- $titlePart".toString() // Needs to be a String (ie. not a GString) to be used as a key in a map reliably
     }
 
     // More helpers
