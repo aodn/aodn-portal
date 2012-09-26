93dfd543e102ddaf525eb56c24e8236c67279c58
dnahodil <dnahodil@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2012-09-26 23:05:46 +0000

    Adding fix for when ocean current info is not available

--- grails-app/services/au/org/emii/portal/OceanCurrentService.groovy
+++ grails-app/services/au/org/emii/portal/OceanCurrentService.groovy
@@ -1,5 +1,6 @@
 package au.org.emii.portal
-import java.util.Random 
+
+import org.apache.commons.io.IOUtils
 
 class OceanCurrentService {
 
@@ -9,15 +10,15 @@ class OceanCurrentService {
 	//String acron
 
     def getRandomDetails() {
-		
+
 		def acron = ""
 		def imageURL = "images/OceanCurrent4AODN.png"	// fall back to the local stored image
 		def speil = ""
 		def parentPage = ""
 		def baseURL = "http://oceancurrent.imos.org.au/"
-		def fileURL = baseURL + "sitemap/updating.txt"
+		def fileAddress = baseURL + "sitemap/updating.txt"
 		def cfgInstanceName = grailsApplication.config.instanceName.toLowerCase()
-		
+
 		def waodn = ['NWS/latest.gif',
 						'Ningaloo/latest.gif',
 						'Perth/latest.gif',
@@ -30,41 +31,43 @@ class OceanCurrentService {
 						'uv/latest.gif',
 						'profiles/map/latest.gif'
 					]
-		
+
 		try {
-			
-			def data = new URL(fileURL).getText()	
-			data = data.split("\n").toList()
-			
-			// Special case for West Australian portal		
+
+            def con = fileAddress.toURL().openConnection()
+            con.readTimeout = 20 * 1000 // 20 seconds
+            con.connect()
+
+            def data = IOUtils.toString( con.inputStream, "UTF-8" )
+            data = data.split("\n").toList()
+
+			// Special case for West Australian portal
 			if (cfgInstanceName == 'waodn') {
-				data.retainAll(waodn)				
+				data.retainAll(waodn)
 			}
-			
-				
+
 			if (data.size() > 0) {
 				int lineCount = 0;
-				data.each { lineCount++; } 
-				Random rand = new Random() 
+				data.each { lineCount++; }
+				Random rand = new Random()
 				lineCount = rand.nextInt(lineCount)
 
 				def num = 0
-				data.each { 
+				data.each {
 					if(num == lineCount) {
-						imageURL = baseURL + it 
+						imageURL = baseURL + it
 						acron = it.minus("/latest.gif")
-						speil = "Latest graph for " + acron 
+						speil = "Latest graph for " + acron
 						parentPage = "/latest.html?region=" + cfgInstanceName
 					}
-					num++				
+					num++
 				}
-			}	
+			}
 		}
 		catch (Exception e) {
-            log.info "ERROR: Couldnt open " + fileURL, e           
+            log.info "ERROR: Couldnt open $fileAddress", e
         }
-		
-		
+
 		return [speil: speil, acron: acron, imageURL: imageURL, baseURL: baseURL, parentPage: parentPage]
     }
 }
