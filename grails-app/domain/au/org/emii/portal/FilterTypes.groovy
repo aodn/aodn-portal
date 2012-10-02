cd1993f2ba8bc141ab2df408acc9f0b6fa3a5f5c
pmak <pmak@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2012-10-02 01:45:06 +0000

    daves code review

--- grails-app/domain/au/org/emii/portal/FilterTypes.groovy
+++ grails-app/domain/au/org/emii/portal/FilterTypes.groovy
@@ -10,63 +10,7 @@ package au.org.emii.portal
  * 2) Create an appropriate type in the Javascript, i.e., in web-app/js/portal/filter
  */
 public enum FilterTypes {
-    STRINGTYPE, DATETYPE, NUMTYPE, BOOLTYPE
-
-    @Override
-    String toString(){
-        switch(this){
-            case STRINGTYPE: return "String"
-                break
-            case DATETYPE: return "Date"
-                break
-            case NUMTYPE: return "Number"
-                break
-            case BOOLTYPE: return "Boolean"
-        }
-
-        // Code review - PM: If you decide not to change the implementation (see below)
-        // then I would throw an Exception here to quickly identify an
-        // unhandled Enum value. Currently this returns null if an Enum
-        // is not handled (ie. if someone creates a new one but doesn't update
-        // the switch statement)
-    }
-
-    String getKey() { name() }
-}
-
-// Code review - PM: I think that given how we use this enum it could be made a bit more simple.
-/*
-Currenty there is room for people adding new ones to make a mistake (not update the switch statement) and
-silently introduce a bug. There are 2 ways you could rewrite this enum which would not have this problem.
-
-Option 1: Just rename the enums (my preferred option but it might not fit in with your preferred naming convention)
-
-public enum FilterTypes {
     String, Date, Number, Boolean
 
     String getKey() { name() }
 }
-
-Option 2: Define the enums with a constructor that takes an argument
-
-public enum FilterTypes {
-    STRINGTYPE("String"),
-    DATETYPE("Date"),
-    NUMTYPE("Number"),
-    BOOLTYPE("Boolean")
-
-    def stringRepresentation
-
-    FilterTypes( s ) {
-        stringRepresentation = s
-    }
-
-    @Override
-    String toString(){
-        return stringRepresentation
-    }
-
-    String getKey() { name() }
-}
-
- */
