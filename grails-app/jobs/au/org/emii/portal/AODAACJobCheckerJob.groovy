db0c3275ad62135a43d5e88db43dc204d4bfde25
dnahodil <dnahodil@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2012-10-11 06:03:31 +0000

    - Changed logging options and check time for AODAAC demo

--- grails-app/jobs/au/org/emii/portal/AODAACJobCheckerJob.groovy
+++ grails-app/jobs/au/org/emii/portal/AODAACJobCheckerJob.groovy
@@ -1,9 +1,9 @@
 package au.org.emii.portal
 
 class AODAACJobCheckerJob {
-    def timeout = 300000l // execute job once in 5 MINUTES!  Yeap, 5 * 60 * 1000
+    def timeout = 120000l // execute job once in 5 MINUTES!  Yeap, 5 * 60 * 1000
     AodaacAggregatorService aodaacAggregatorService
-    
+
     def execute() {
         aodaacAggregatorService.checkIncompleteJobs()
     }
