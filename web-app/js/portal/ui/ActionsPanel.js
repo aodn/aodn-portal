4d1b4d2298fa82bb57a5a20c96ba81acbc508866
jburgess <jburgess@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2012-10-15 02:58:19 +0000

    #2049 - merging layer switcher.

--- web-app/js/portal/ui/ActionsPanel.js
+++ web-app/js/portal/ui/ActionsPanel.js
@@ -1,23 +1,60 @@
 Ext.namespace('Portal.ui');
 
 Portal.ui.ActionsPanel = Ext.extend(Ext.Panel, {
-
+    
     constructor: function(cfg) {
         this.mapOptionsPanel = new Portal.ui.MapOptionsPanel(cfg);
         this.activeLayersPanel = new Portal.ui.ActiveLayersPanel(cfg);
         var config = Ext.apply({
             id: 'activeMenuPanel',
-		    padding: '0px 0px 20px 0px',
-		    autoHeight: true,
-		    items:[
-		        this.mapOptionsPanel,
-		        this.activeLayersPanel
-		    ]
-		}, cfg);
-		Portal.ui.ActionsPanel.superclass.constructor.call(this, config);
-	},
+            padding: '0px 0px 20px 0px',                      
+            autoHeight: true,
+            items:[
+                this.mapOptionsPanel,
+                this.activeLayersPanel
+            ]
+        }, cfg);
+        Portal.ui.ActionsPanel.superclass.constructor.call(this, config);
+        
+        this.relayEvents(this.activeLayersPanel, ['removelayer', 'zoomtolayer', 'togglevisibility']);
+        this.relayEvents(this.mapOptionsPanel, ['removealllayers', 'resetmap', 'hidelayeroptionschecked', 'hidelayeroptionsunchecked', 'autozoomchecked', 'autozoomunchecked']);
+        
+        //
+        // This panel (which contains both the MapOptions and the ActiveLayers), needs to
+        // orchestrate event handling between the two child panels, specifically when the
+        // auto zoom check box is toggled and when a different active layer is selected.
+        //
+        this.activeLayersPanel.on('selectedactivelayerchanged', function()
+        {
+            if (this.autoZoomEnabled())
+            {
+                this.activeLayersPanel.zoomToLayer();
+            }
+        }, this);
+        
+        this.mapOptionsPanel.on('autozoomchecked', function()
+        {
+            this.activeLayersPanel.zoomToLayer();
+        }, this);
+    },
+    
+    initBaseLayerCombo: function() {
+        this.mapOptionsPanel.initBaseLayerCombo();
+    },
+    
+    getActiveLayerNodes: function() {
+        return this.activeLayersPanel.getActiveLayerNodes();
+    },
+    
+    layerOptionsVisible: function() {
+        return this.mapOptionsPanel.layerOptionsVisible();
+    },
+    
+    autoZoomEnabled: function() {
+        return this.mapOptionsPanel.autoZoomEnabled();
+    },
 
-	loadSnapshot: function(id) {
-		this.mapOptionsPanel.loadSnapshot(id);
-	}
+    loadSnapshot: function(id) {
+        this.mapOptionsPanel.loadSnapshot(id);
+    }
 });
