b623eb48acbd932a54e10c1e675867299dead940
amckeown <amckeown@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2012-10-19 04:50:08 +0000

    #2072 Revert changes made from usability branch.  Removed display method is neccasary for displaying the items in dropdowns for traditional search and shouldn't interfere with faceted search since its just for display.

--- web-app/js/portal/data/SuggestionStore.js
+++ web-app/js/portal/data/SuggestionStore.js
@@ -1,22 +1,35 @@
 Ext.namespace('Portal.data.SuggestionStore');
 
 Portal.data.SuggestionStore = Ext.extend(Ext.data.XmlStore, {
-    constructor: function(cfg) {
-        var config = Ext.apply({
-            autoDestroy: true,
-            record: 'value',
-            fields: [
-                {name: 'value', mapping: '', sortType: Ext.data.SortTypes.asUCString},
-                {name: 'count', mapping: '@count'}
-            ],
-            sortInfo: {
-                field: "value",
-                direction: 'ASC'
-            }
-        }, cfg);
+	constructor : function(cfg) {
+		var config = Ext.apply({
+			autoDestroy : true,
+			record : 'value',
+			fields : [ {
+				name : 'value',
+				mapping : '',
+				sortType : Ext.data.SortTypes.asUCString
+			}, {
+				name : 'count',
+				mapping : '@count'
+			}, {
+				name : 'display',
+				mapping : '',
+				convert : this.displayName
+			} ],
+			sortInfo : {
+				field : "value",
+				direction : 'ASC'
+			}
+		}, cfg);
 
-        Portal.data.SuggestionStore.superclass.constructor.call(this, config);
+		Portal.data.SuggestionStore.superclass.constructor.call(this, config);
 
-        this.proxy.conn.disableCaching = false;
-    }
+		this.proxy.conn.disableCaching = false;
+	},
+
+	displayName : function(v) {
+		return v.replace(/_/g, ' ');
+	}
+
 });
