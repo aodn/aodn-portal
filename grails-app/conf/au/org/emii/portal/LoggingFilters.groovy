57b4e14cba865abd1f528e0f6a53885331599620
dnahodil <dnahodil@80f082c3-aa3d-4a8a-bc4c-16289f4b763f>
2012-11-01 03:24:45 +0000

    - Added client address (IP) and user-agent to error logging emails

--- grails-app/conf/au/org/emii/portal/LoggingFilters.groovy
+++ grails-app/conf/au/org/emii/portal/LoggingFilters.groovy
@@ -11,17 +11,28 @@ class LoggingFilters {
 
             before = {
 
-                def principal = SecurityUtils?.subject?.principal
+                // Client data
+                MDC.put 'clientAddress', clientAddress( request )
+                MDC.put 'userAgent', request.getHeader( "User-Agent" )
 
+                // User data
+                def principal = SecurityUtils?.subject?.principal
                 MDC.put 'userInfoForFile', "(User: ${ principal ?: "anon." }) "
                 MDC.put 'userInfoForEmail', "User: ${ principal ?: "Not logged-in" }\n"
             }
 
             afterView = {
 
+                MDC.remove 'clientAddress'
+                MDC.remove 'userAgent'
                 MDC.remove 'userInfoForFile'
                 MDC.remove 'userInfoForEmail'
             }
         }
+    }
+
+    String clientAddress( request ) {
+
+        return request.remoteAddr ?: request.getHeader( "X-Forwarded-For" ) ?: request.getHeader( "Client-IP" )
     }
 }
