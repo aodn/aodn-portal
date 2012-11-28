6e26cc1e3467fdda793526058be4143e5131249f
dnahodil <dnahodil+git@gmail.com>
2012-11-28 11:09:09 +1100

    Fixed styling on saved maps disclaimer popup text

--- grails-app/migrations/imos-changelog.groovy
+++ grails-app/migrations/imos-changelog.groovy
@@ -181,5 +181,16 @@ databaseChangeLog = {
 <p>IMOS data is licensed under a <a title="Creative Commons License"  href="http://creativecommons.org/licenses/by/2.5/au/" target="_blank"><span style="white-space: nowrap">Creative Commons Attribution 2.5 Australia License</span></a> <a class="external" title="Creative Commons License"  href="http://creativecommons.org/licenses/by/2.5/au/" target="_blank"><img width="80" src="images/by.png"/></a></p>""")
 			}
 		}
+
+		changeSet(author: "dnahodil (generated)", id: "1353908954037-3", failOnError: true) {
+
+			update(tableName: "config") {
+
+				column(name: "footer_content", value: """\
+<p>You accept all risks and responsibility for losses, damages, costs and other consequences resulting directly or indirectly from using this site and any information or material available from it. If you have any concerns about the veracity of the data, please make enquiries via <a href="mailto:info@imos.org.au">info@imos.org.au</a> to be directed to the data custodian.</p>
+<p>This site is licensed under a <a title="Creative Commons License"  href="http://creativecommons.org/licenses/by/3.0/au/" target="_blank"><span style="white-space: nowrap">Creative Commons Attribution 3.0 Australia License</span></a> <a class="external" title="Creative Commons License"  href="http://creativecommons.org/licenses/by/3.0/au/" target="_blank"><img width="80" src="images/by.png"/></a></p>
+<p>IMOS data is licensed under a <a title="Creative Commons License"  href="http://creativecommons.org/licenses/by/2.5/au/" target="_blank"><span style="white-space: nowrap">Creative Commons Attribution 2.5 Australia License</span></a> <a class="external" title="Creative Commons License"  href="http://creativecommons.org/licenses/by/2.5/au/" target="_blank"><img width="80" src="images/by.png"/></a></p>""")
+			}
+		}
 	}
 }
