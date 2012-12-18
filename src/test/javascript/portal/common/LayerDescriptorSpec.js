3326d1d8ee8e430120cee6935dc3d4b12adedbac
dnahodil <dnahodil@utas.edu.au>
2012-12-19 09:19:41 +1100

    Jasmine tests for #37

--- src/test/javascript/portal/common/LayerDescriptorSpec.js
+++ src/test/javascript/portal/common/LayerDescriptorSpec.js
@@ -7,16 +7,16 @@
  */
 
 describe("Portal.common.LayerDescriptor", function() {
-  
+
     it('from string', function() {
         var layerDescAsString = "{name: 'satellite', server: { uri: 'http://tilecache.emii.org.au/cgi-bin/tilecache.cgi' } }";
-        
+
         var layerDesc = new Portal.common.LayerDescriptor(layerDescAsString);
-        
+
         expect(layerDesc.name).toBe('satellite');
         expect(layerDesc.server.uri).toBe('http://tilecache.emii.org.au/cgi-bin/tilecache.cgi');
     });
-    
+
     it('from javascript object', function() {
         var layerDescAsDecodedJSON = {
             name : 'satellite',
@@ -24,33 +24,33 @@ describe("Portal.common.LayerDescriptor", function() {
                 uri : 'http://tilecache.emii.org.au/cgi-bin/tilecache.cgi'
             }
         };
-        
+
         var layerDesc = new Portal.common.LayerDescriptor(layerDescAsDecodedJSON);
         expect(layerDesc.name).toBe('satellite');
         expect(layerDesc.server.uri).toBe('http://tilecache.emii.org.au/cgi-bin/tilecache.cgi');
     });
-    
+
     it('to openlayer WMS layer', function() {
-        
+
         var layerDesc = new Portal.common.LayerDescriptor({
             "isBaseLayer": true,
             "server": {
-                "opacity": 100,         
-                "type": "WMS-1.1.1",         
-                "uri": "http: //tilecache.emii.org.au/cgi-bin/tilecache.cgi"         
+                "opacity": 100,
+                "type": "WMS-1.1.1",
+                "uri": "http: //tilecache.emii.org.au/cgi-bin/tilecache.cgi"
             }
         });
-        
+
         var openLayer = layerDesc.toOpenLayer();
 
         expect(openLayer.isBaseLayer).toBe(true);
         expect(openLayer.url).toBe("http: //tilecache.emii.org.au/cgi-bin/tilecache.cgi");
         expect(openLayer.opacity).toBe(1);
-        
+
         var openLayerWithOptionOverrides = layerDesc.toOpenLayer({ opacity: 2});
         expect(openLayerWithOptionOverrides.opacity).toBe(2);
     });
-    
+
     it('tests underlying access to parent', function() {
         var layerDescriptor = new Portal.common.LayerDescriptor({
             title : 'test',
@@ -59,11 +59,24 @@ describe("Portal.common.LayerDescriptor", function() {
                 name : 'parent layer'
             }
         });
-        
+
         expect(layerDescriptor._getParentId()).toEqual(100);
         expect(layerDescriptor._getParentName()).toEqual('parent layer');
         layerDescriptor.parent = undefined;
         expect(layerDescriptor._getParentId()).toBeFalsy();
         expect(layerDescriptor._getParentName()).toBeFalsy();
+    });
+
+    it('tests _getAllStyles returns object or empty Array if undefined', function() {
+
+        var testStyles = ['Style 1', 'Style 2'];
+
+        var layerDescriptor = new Portal.common.LayerDescriptor({
+            allStyles: testStyles
+        });
+
+        expect(layerDescriptor._getAllStyles()).toEqual(testStyles);
+        layerDescriptor.allStyles = undefined;
+        expect(layerDescriptor._getAllStyles()).toEqual([]);
     });
 });
