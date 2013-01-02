0a43335c183c2d9373b307560ecc59a76fc538ab
pmbohm <pmbohm@utas.edu.au>
2013-01-02 15:58:51 +1100

    typo in changeset

--- grails-app/migrations/20121217-PB-SOOSFooterText.groovy
+++ grails-app/migrations/20121217-PB-SOOSFooterText.groovy
@@ -3,7 +3,7 @@ databaseChangeLog = {
     if (System.getProperty("INSTANCE_NAME") == 'SOOS') {
         changeSet(author: "pmbohm (generated)", id: "20121217-PB-SOOSFooterText") {
 
-            sql("update config set footer_content='")
+            sql("update config set footer_content=''")
 
             sql("update config set NAME='SOUTHERN OCEAN OBSERVING SYSTEM PORTAL'")
             sql("update config set header_height=100")
