a7c32303227c0435e7a5fb24aeaa0b2c04a7a23e
akashisama <alex.mckeown@utas.edu.au>
2013-01-09 17:12:05 +1100

    Create a basic spec for DateSelectionPanel

---
+++ src/test/javascript/portal/search/DateSelectionPanelSpec.js
@@ -0,0 +1,45 @@
+
+/*
+ * Copyright 2012 IMOS
+ *
+ * The AODN/IMOS Portal is distributed under the terms of the GNU General Public License
+ *
+ */
+
+describe("Portal.search.DateSelectionPanel", function()
+{
+
+    var testWindow;
+
+    var buildMockSelectionPanel = function() {
+        var dateFilter = new Portal.search.DateSelectionPanel({
+            title: "Date Filter",
+            hierarchical: false,
+            fieldGroup: 'organisationNames',
+            fieldName: 'orgName',
+            searcher: new Portal.service.CatalogSearcher()
+        });
+        return dateFilter;
+    };
+
+    beforeEach(function() {
+        testWindow = new Ext.Window();
+    });
+
+    afterEach(function() {
+        testWindow.close();
+    });
+
+    it("creates search form with default options on instantiation", function() {
+        var dateFilter = buildMockSelectionPanel();
+
+        expect(dateFilter.title.contains('<span class="term-selection-panel-header">' + "Date Filter" + '</span>')).toBeTruthy();
+
+        dateFilter.setSelectedSubTitle("sub");
+
+        expect(dateFilter.title.contains('<span class="term-selection-panel-header-selected">' + "Date Filter" + '</span>')).toBeTruthy();
+
+        expect(dateFilter.title.contains('sub')).toBeTruthy();
+
+    });
+});
