e33658782eeee433011d69a60e19787ad85e9d7d
akashisama <alex.mckeown@utas.edu.au>
2013-01-10 11:32:43 +1100

    Add tests for 'removeSelectedSubtitle' and 'onSearch'

--- src/test/javascript/portal/search/DateSelectionPanelSpec.js
+++ src/test/javascript/portal/search/DateSelectionPanelSpec.js
@@ -12,13 +12,22 @@ describe("Portal.search.DateSelectionPanel", function()
     var testWindow;
 
     var buildMockSelectionPanel = function() {
+
+        var searcher = new Portal.service.CatalogSearcher();
+
+        searcher.timesSearchCalled = 0;
+        searcher.search = function(page, summaryOnly) {
+            this.timesSearchCalled++;
+        };
+
         var dateFilter = new Portal.search.DateSelectionPanel({
             title: "Date Filter",
             hierarchical: false,
             fieldGroup: 'organisationNames',
             fieldName: 'orgName',
-            searcher: new Portal.service.CatalogSearcher()
+            searcher: searcher
         });
+
         return dateFilter;
     };
 
@@ -46,6 +55,49 @@ describe("Portal.search.DateSelectionPanel", function()
 
             expect(dateFilter.title.contains('sub')).toBeTruthy();
 
+        });
+    });
+
+    describe("removeSelectedSubTitle", function() {
+        it("puts the title back to normal", function() {
+            var dateFilter = buildMockSelectionPanel();
+
+            dateFilter.removeSelectedSubTitle();
+
+            expect(dateFilter.title.contains('<span class="term-selection-panel-header">' + "Date Filter" + '</span>')).toBeTruthy();
+        });
+    });
+
+    describe("onSearch", function() {
+        it("doesn't do catalog search if start and end are empty", function() {
+            var dateFilter = buildMockSelectionPanel();
+
+            dateFilter.dateRange.setFilterValue({fromDate:"",toDate:""})
+
+            dateFilter.onSearch();
+            expect(dateFilter.searcher.timesSearchCalled).toEqual(0);
+        });
+
+        it("calls catalog search when fromDate and/or toDate are set", function() {
+            var dateFilter = buildMockSelectionPanel();
+
+            dateFilter.dateRange.setFilterValue({fromDate:Date.parseDate("2012-10-20","Y-m-d"),toDate:Date.parseDate("2012-10-27","Y-m-d")})
+
+            dateFilter.onSearch();
+
+            expect(dateFilter.searcher.timesSearchCalled).toEqual(1);
+
+            dateFilter.dateRange.setFilterValue({fromDate:null,toDate:Date.parseDate("2012-10-27","Y-m-d")})
+
+            dateFilter.onSearch();
+
+            expect(dateFilter.searcher.timesSearchCalled).toEqual(2);
+
+            dateFilter.dateRange.setFilterValue({fromDate:Date.parseDate("2012-10-20","Y-m-d"),toDate:null})
+
+            dateFilter.onSearch();
+
+            expect(dateFilter.searcher.timesSearchCalled).toEqual(3);
         });
     });
 });
