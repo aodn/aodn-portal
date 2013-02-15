6a790c4611a3345e6edc6d9d4ad569c8b7f3a342
akashisama <alex.mckeown@utas.edu.au>
2013-02-15 16:26:55 +1100

    #46 Move the bounds before you add gutter to account

---
+++ src/test/javascript/spec_helper.js
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2012 IMOS
+ *
+ * The AODN/IMOS Portal is distributed under the terms of the GNU General Public License
+ *
+ */
+
+Ext.MessageBox.alert = function () {
+    return true;
+};
+
+Ext.MessageBox.confirm = function () {
+    return true;
+};
+
+Ext.MessageBox.show = function () {
+    return true;
+};
