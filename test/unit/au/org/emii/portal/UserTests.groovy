bcbcaa6a169cdb7f6c6a52eb05205ab9480de3f1
dnahodil <dnahodil@utas.edu.au>
2013-04-08 13:58:46 +1000

    Fixed User tests (hopefully for good this time)

--- test/unit/au/org/emii/portal/UserTests.groovy
+++ test/unit/au/org/emii/portal/UserTests.groovy
@@ -37,14 +37,15 @@ class UserTests extends GrailsUnitTestCase {
 
     void testToString() {
 
-//        assertEquals "Joe Bloggs [No Roles] (http://www.example.com/openId)", user.toString()
-//
-//		user.roles = [new UserRole(name: "Role1")]
-//
-//        assertEquals "Joe Bloggs [Roles: Role1] (http://www.example.com/openId)", user.toString()
-//
-//		user.roles = [new UserRole(name: "Role1"), new UserRole(name: "Role2")]
-//
-//		assertEquals "Joe Bloggs [Roles: Role1, Role2] (http://www.example.com/openId)", user.toString()
+		assertEquals "Joe Bloggs [No Roles] (http://www.example.com/openId)", user.toString()
+
+		user.addToRoles( new UserRole(name: "Role1") )
+
+		assertEquals "Joe Bloggs [Roles: Role1] (http://www.example.com/openId)", user.toString()
+
+		user.addToRoles( new UserRole(name: "Role2") )
+
+		// Need to test either match as ordering of Roles is non-deterministic (as it is a Set)
+		assertTrue user.toString() ==~ /Joe Bloggs \[Roles: (Role1, Role2|Role2, Role1)\] \(http:\/\/www\.example\.com\/openId\)/
     }
 }
