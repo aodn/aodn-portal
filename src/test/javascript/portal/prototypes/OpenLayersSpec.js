b75803eb712e74ef8f10722b5bd6af11ef5c2a3f
akashisama <alex.mckeown@utas.edu.au>
2013-04-12 11:22:19 +1000

    #146 Add tests for _is130 method

--- src/test/javascript/portal/prototypes/OpenLayersSpec.js
+++ src/test/javascript/portal/prototypes/OpenLayersSpec.js
@@ -7,8 +7,12 @@
  */
 
 describe("OpenLayers.Layer.WMS", function() {
-    
-    var openLayer = new OpenLayers.Layer.WMS();
+
+    var openLayer;
+
+    beforeEach(function() {
+        openLayer = new OpenLayers.Layer.WMS();
+    });
     
     it("no bounding box", function() {
         expect(openLayer.hasBoundingBox()).toBeFalsy();
@@ -30,7 +34,23 @@ describe("OpenLayers.Layer.WMS", function() {
     });
     
     it("has bounding box", function() {
+        openLayer.bboxMinX = 10;
+        openLayer.bboxMinY = 10;
+        openLayer.bboxMaxX = 10;
         openLayer.bboxMaxY = 10;
         expect(openLayer.hasBoundingBox()).toBeTruthy();
+    });
+
+    describe("_is130", function() {
+        it("returns false for ncwms", function() {
+            openLayer.isNcwms = function() { return true }
+            openLayer.server= {type: "NCWMS 1.3.0"};
+            expect(openLayer._is130()).toBeFalsy();
+        });
+
+        it("returns true for 1.3.0 when not NCWMS", function() {
+            openLayer.server= {type: "WMS 1.3.0"};
+            expect(openLayer._is130()).toBeTruthy();
+        });
     });
 });
