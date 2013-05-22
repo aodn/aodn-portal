44487fd809897b157a4b54eba7f624a08ed71867
Jon Burgess <jkburges@gmail.com>
2013-05-22 14:22:12 +1000

    Reformat tabs 4 spaces.

--- web-app/js/portal/service/CatalogSearcher.js
+++ web-app/js/portal/service/CatalogSearcher.js
@@ -9,148 +9,148 @@
 Ext.namespace('Portal.service');
 
 Portal.service.CatalogSearcher = Ext.extend(Ext.util.Observable, {
-  constructor: function(cfg) {
-    var defaults = {
-        serviceUrl: 'xml.search.summary',
-        baseParams: {
-            fast: 'index'
-        },
-        defaultParams: {},
-        pageSize: 10
-    };
+    constructor: function(cfg) {
+        var defaults = {
+            serviceUrl: 'xml.search.summary',
+            baseParams: {
+                fast: 'index'
+            },
+            defaultParams: {},
+            pageSize: 10
+        };
 
-    var searchFilters = new Ext.data.JsonStore({
-      root: 'filters',
-      fields: ['name', 'value']
-    });
+        var searchFilters = new Ext.data.JsonStore({
+            root: 'filters',
+            fields: ['name', 'value']
+        });
 
-    var config = Ext.apply({
-      searchFilters: searchFilters
-    }, cfg, defaults);
+        var config = Ext.apply({
+            searchFilters: searchFilters
+        }, cfg, defaults);
 
-    Ext.apply(this, config);  // Not done in Observable's constructor for some reason
+        Ext.apply(this, config);  // Not done in Observable's constructor for some reason
 
-    Portal.service.CatalogSearcher.superclass.constructor.call(this, config);
+        Portal.service.CatalogSearcher.superclass.constructor.call(this, config);
 
-    this.addEvents( 'searchcomplete', 'searcherror', 'filteradded', 'filterremoved' );
-  },
+        this.addEvents( 'searchcomplete', 'searcherror', 'filteradded', 'filterremoved' );
+    },
 
-  reset: function() {
-    this.searchFilters.removeAll();
-  },
+    reset: function() {
+        this.searchFilters.removeAll();
+    },
 
-  search: function(summaryOnly) {
+    search: function(summaryOnly) {
 
-    var page = {from: 1, to: this.pageSize};
-    this._search(page, summaryOnly);
-  },
+        var page = {from: 1, to: this.pageSize};
+        this._search(page, summaryOnly);
+    },
 
-  _search: function(page, summaryOnly) {
+    _search: function(page, summaryOnly) {
 
-    var requestUrl = this._getRequestUrl(page, summaryOnly);
+        var requestUrl = this._getRequestUrl(page, summaryOnly);
 
-    Ext.Ajax.request({
-      url: requestUrl,
-      success: summaryOnly ? this._onSuccessfulSummarySearch : this._onSuccessfulSearch,
-      failure: this._logAndReturnErrors,
-      page: page,
-      scope: this,
-      defaultHeaders: {
-        'Content-Type': 'application/xml'
-      }
-    });
-  },
+        Ext.Ajax.request({
+            url: requestUrl,
+            success: summaryOnly ? this._onSuccessfulSummarySearch : this._onSuccessfulSearch,
+            failure: this._logAndReturnErrors,
+            page: page,
+            scope: this,
+            defaultHeaders: {
+                'Content-Type': 'application/xml'
+            }
+        });
+    },
 
-  goToPage: function(start, limit) {
-    var page = {from: start, to: start + limit - 1};
-    this._search(page);
-  },
+    goToPage: function(start, limit) {
+        var page = {from: start, to: start + limit - 1};
+        this._search(page);
+    },
 
-  removeFilters: function(filterPattern) {
+    removeFilters: function(filterPattern) {
 
-    var filters = this.searchFilters.query('name', filterPattern);
+        var filters = this.searchFilters.query('name', filterPattern);
 
-    this.searchFilters.remove(filters.items);
+        this.searchFilters.remove(filters.items);
 
-    this.fireEvent( 'filterremoved' );
-  },
+        this.fireEvent( 'filterremoved' );
+    },
 
-  addFilter: function(name, value) {
-    var rec = new this.searchFilters.recordType();
+    addFilter: function(name, value) {
+        var rec = new this.searchFilters.recordType();
 
-    rec.set('name', name);
-    rec.set('value', value);
+        rec.set('name', name);
+        rec.set('value', value);
 
-    this.searchFilters.add(rec);
+        this.searchFilters.add(rec);
 
-    this.fireEvent( 'filteradded' );
-  },
+        this.fireEvent( 'filteradded' );
+    },
 
-  hasFilters: function() {
+    hasFilters: function() {
 
-      var idx = this.searchFilters.findBy( function( record ) {
+        var idx = this.searchFilters.findBy( function( record ) {
 
-          return record.data.value != '';
-      } );
+            return record.data.value != '';
+        } );
 
-      return idx >= 0;
-  },
+        return idx >= 0;
+    },
 
-  _onSuccessfulSearch: function(response, options) {
+    _onSuccessfulSearch: function(response, options) {
 
-    this.fireEvent('searchcomplete', response.responseXML, options.page);
-  },
+        this.fireEvent('searchcomplete', response.responseXML, options.page);
+    },
 
-  _onSuccessfulSummarySearch: function(response, options) {
+    _onSuccessfulSummarySearch: function(response, options) {
 
-      this.fireEvent('summaryOnlySearchComplete', response.responseXML, options.page);
-  },
+        this.fireEvent('summaryOnlySearchComplete', response.responseXML, options.page);
+    },
 
-  _logAndReturnErrors: function(response, options) {
-    this.fireEvent('searcherror', response);
-  },
+    _logAndReturnErrors: function(response, options) {
+        this.fireEvent('searcherror', response);
+    },
 
-  // Build request url to use from proxyUrl, catalogUrl and filters
-  _getRequestUrl: function(page, summaryOnly) {
-    var params = this._getParams(page, summaryOnly);
+    // Build request url to use from proxyUrl, catalogUrl and filters
+    _getRequestUrl: function(page, summaryOnly) {
+        var params = this._getParams(page, summaryOnly);
 
-    return this.proxyUrl + encodeURIComponent(this.catalogUrl + '/srv/eng/' + this.serviceUrl + '?' + Ext.urlEncode(params));
-  },
+        return this.proxyUrl + encodeURIComponent(this.catalogUrl + '/srv/eng/' + this.serviceUrl + '?' + Ext.urlEncode(params));
+    },
 
-  _getParams: function(page, summaryOnly) {
-    //--- Add search defaults (e.g. map layers only)
-    var params = Ext.apply({}, this.defaultParams);
+    _getParams: function(page, summaryOnly) {
+        //--- Add search defaults (e.g. map layers only)
+        var params = Ext.apply({}, this.defaultParams);
 
-    //--- Add paging params
-    Ext.apply(params, page);
+        //--- Add paging params
+        Ext.apply(params, page);
 
-    //--- Add current search filters
-    this.searchFilters.each(function(rec){
-      name = rec.get('name');
-      param = params[name];
-      value = rec.get('value');
+        //--- Add current search filters
+        this.searchFilters.each(function(rec){
+            name = rec.get('name');
+            param = params[name];
+            value = rec.get('value');
 
-      if(Ext.isDefined(param)){
-          if(Ext.isArray(param)){
-              params[name].push(value);
-          }else{
-              params[name] = [param, value];
-          }
-      }else{
-          params[name] = value;
-      }
-    });
+            if(Ext.isDefined(param)){
+                if(Ext.isArray(param)){
+                    params[name].push(value);
+                }else{
+                    params[name] = [param, value];
+                }
+            }else{
+                params[name] = value;
+            }
+        });
 
-    //--- Add required base parameters (e.g. fast=index)
-    Ext.apply(params, this.baseParams);
+        //--- Add required base parameters (e.g. fast=index)
+        Ext.apply(params, this.baseParams);
 
-    //--- Only return summary info if requested
-    if (summaryOnly) {
-      params[Portal.service.CatalogSearcher.BUILD_SUMMARY_ONLY] = true;
+        //--- Only return summary info if requested
+        if (summaryOnly) {
+            params[Portal.service.CatalogSearcher.BUILD_SUMMARY_ONLY] = true;
+        }
+
+        return params;
     }
-
-    return params;
-  }
 
 });
 
