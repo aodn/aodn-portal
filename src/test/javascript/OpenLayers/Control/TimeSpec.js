d6b438f2edb463506be152df4c37970463cc53aa
Jon Burgess <jkburges@gmail.com>
2013-05-28 11:12:04 +1000

    Time control, Timer and TemoralMap integration.

--- src/test/javascript/OpenLayers/Control/TimeSpec.js
+++ src/test/javascript/OpenLayers/Control/TimeSpec.js
@@ -10,21 +10,81 @@ describe("OpenLayers.Control.Time", function() {
     var timeControl;
 
     beforeEach(function() {
+
         map = new OpenLayers.TemporalMap();
         timeControl = new OpenLayers.Control.Time({
             map: map
         });
-    });
-    
-    it("play", function() {
-        spyOn(map, 'play');
-        timeControl.play();
-        expect(map.play).toHaveBeenCalled();
+
+        spyOn(timeControl.timer, 'start');
+        spyOn(timeControl.timer, 'stop');
+
+        // TODO: refactor custom toBeSame matcher to somewhere common to all specs.
+        this.addMatchers({
+            toBeSame: function(expected) {
+                return this.actual.isSame(expected);   // moment.js#isSame()
+            }
+        });
     });
 
-    it("stop", function() {
-        spyOn(map, 'stop');
-        timeControl.stop();
-        expect(map.stop).toHaveBeenCalled();
+    describe('map', function() {
+        it('initialisation', function() {
+            expect(timeControl.map).not.toBeNull();
+        });
+
+        it('onTick', function() {
+            spyOn(timeControl.map, 'toTime');
+            timeControl.onTick({
+                index: 1,
+                dateTime: moment('2000-12-12T01:01:01Z')
+            });
+
+            expect(map.toTime).toHaveBeenCalledWith(moment('2000-12-12T01:01:01Z'));
+        });
+    });
+
+    describe('timer', function() {
+
+        it('initialisation', function() {
+            expect(timeControl.timer).not.toBeNull();
+        });
+
+        it('start/end date/time specified', function() {
+            timeControl = new OpenLayers.Control.Time({
+                map: map,
+                startDateTime: '2013-03-06T12:34:56',
+                endDateTime: '2013-04-07T02:12:43'
+            });
+
+            expect(timeControl.timer.startDateTime).toBeSame('2013-03-06T12:34:56');
+            expect(timeControl.timer.endDateTime).toBeSame('2013-04-07T02:12:43');
+
+        });
+    });
+
+    describe('play/stop', function() {
+
+        it('on play, timer is started', function() {
+            timeControl.play();
+            expect(timeControl.timer.start).toHaveBeenCalled();
+        });
+
+        it('on play, onTick is registered observer for timer\'s ticks', function() {
+            expect(timeControl.timer.observers['tick']).toBeFalsy();
+            timeControl.play();
+            expect(timeControl.timer.observers['tick']).toBeTruthy();
+            expect(timeControl.timer.observers['tick']).toBe(timeControl.onTick);
+        });
+
+        it('on stop, timer is stopped', function() {
+            timeControl.stop();
+            expect(timeControl.timer.stop).toHaveBeenCalled();
+        });
+
+        it('on stop, onTick is not registered observer for timer\'s ticks', function() {
+            timeControl.play();   // Observer will now be registered.
+            timeControl.stop();
+            expect(timeControl.timer.observers['tick']).toBeFalsy();
+        });
     });
 });
--- web-app/js/portal/ui/openlayers/control/Time.js
+++ web-app/js/portal/ui/openlayers/control/Time.js
@@ -5,16 +5,36 @@
  *
  */
 OpenLayers.Control.Time = OpenLayers.Class(OpenLayers.Control, {
+
+    /**
+     * The timer generates 'tick' events given a date range and a frequency.
+     */
+    timer: null,
+
+    /**
+     * The map that this control is controlling.
+     */
+    map: null,
+
     initialize: function(options) {
-    
+
+        this.timer = new OpenLayers.Timer(options);
+        this.map = options.map;
+
         OpenLayers.Control.prototype.initialize.apply(this, [options]);
     },
 
     play: function() {
-        this.map.play();
+        this.timer.on('tick', this.onTick);
+        this.timer.start()
     },
 
     stop: function() {
-        this.map.stop();
+        this.timer.stop();
+        this.timer.on('tick', undefined);
+    },
+
+    onTick: function(tickEvent) {
+        this.map.toTime(tickEvent.dateTime);
     }
 });
