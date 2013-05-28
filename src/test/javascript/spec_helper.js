32e194f616122f2fcc98bde79a217c02c292d56c
Jon Burgess <jkburges@gmail.com>
2013-05-28 14:51:39 +1000

    Only appending 'TIME' param if time is specified for layer. Constructing NcWMS layer (as opposed to WMS layer) where necessary.

--- src/test/javascript/spec_helper.js
+++ src/test/javascript/spec_helper.js
@@ -15,7 +15,26 @@ Ext.Ajax.request = function(options) {
 beforeEach(function() {
     this.addMatchers({
         toBeSame: function(expected) {
+
+            if (this.actual instanceof Array && expected instanceof Array) {
+                if (this.actual.length != expected.length) {
+                    return false;
+                }
+
+                var equal = true;
+                for (var i = 0; i < expected.length; i++) {
+                    if (this.actual[i] != expected[i]) {
+                        equal = false;
+                    }
+                }
+
+                return equal;
+            }
             return moment(this.actual).isSame(moment(expected));   // moment.js#isSame()
+        },
+
+        toBeInstanceOf: function(expected) {
+            return this.actual instanceof expected;
         }
     });
 });
