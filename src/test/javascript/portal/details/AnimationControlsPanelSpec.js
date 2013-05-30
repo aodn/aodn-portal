ce15763147c322e30aa3c3e2fb43b08f074fb8ca
Jon Burgess <jkburges@gmail.com>
2013-05-30 16:23:56 +1000

    Play/stop on timer called from AnimationControlsPanel.

--- src/test/javascript/portal/details/AnimationControlsPanelSpec.js
+++ src/test/javascript/portal/details/AnimationControlsPanelSpec.js
@@ -14,11 +14,11 @@ describe("Portal.details.AnimationControlsPanel", function() {
     beforeEach(function() {
         animationControlsPanel = new Portal.details.AnimationControlsPanel();
         openLayer = new OpenLayers.Layer.WMS(
-                "the title",
-                "http: //tilecache.emii.org.au/cgi-bin/tilecache.cgi",
-                {},
-                { isBaseLayer: false }
-            );
+            "the title",
+            "http: //tilecache.emii.org.au/cgi-bin/tilecache.cgi",
+            {},
+            { isBaseLayer: false }
+        );
     });
 
     describe("speedUp button", function() {
@@ -234,32 +234,95 @@ describe("Portal.details.AnimationControlsPanel", function() {
 
 
 
-        describe('animatedLayer', function() {
-            it("removes slide after parent layer is removed", function() {
-                var map = new OpenLayers.Map('map');
-                var layerStore = new Portal.data.LayerStore();
-                layerStore.bind(map);
-                map.addLayer(openLayer);
+    describe('animatedLayer', function() {
+        it("removes slide after parent layer is removed", function() {
+            var map = new OpenLayers.Map('map');
+            var layerStore = new Portal.data.LayerStore();
+            layerStore.bind(map);
+            map.addLayer(openLayer);
 
-                animationControlsPanel.setMap(map);
-                animationControlsPanel.setSelectedLayer(openLayer);
+            animationControlsPanel.setMap(map);
+            animationControlsPanel.setSelectedLayer(openLayer);
 
-                animationControlsPanel._convertSelectedLayerToAnimatedLayer();
+            animationControlsPanel._convertSelectedLayerToAnimatedLayer();
 
-                var slide = new OpenLayers.Layer.WMS(
-                    "the title2",
-                    "http: //tilecache.emii.org.au/cgi-bin/tilecache.cgi",
-                    {},
-                    { isBaseLayer: false }
-                );
-                animationControlsPanel.originalLayer.addSlide(slide);
+            var slide = new OpenLayers.Layer.WMS(
+                "the title2",
+                "http: //tilecache.emii.org.au/cgi-bin/tilecache.cgi",
+                {},
+                { isBaseLayer: false }
+            );
+            animationControlsPanel.originalLayer.addSlide(slide);
 
-                spyOn(Ext.MsgBus, 'publish');
+            spyOn(Ext.MsgBus, 'publish');
 
-                openLayer._onLayerRemoved({layer:openLayer});
+            openLayer._onLayerRemoved({layer:openLayer});
 
 
-                expect(Ext.MsgBus.publish).toHaveBeenCalledWith("removeLayer", slide);
-            });
+            expect(Ext.MsgBus.publish).toHaveBeenCalledWith("removeLayer", slide);
         });
+    });
+
+    describe('time control', function() {
+
+        var timeControl;
+        var animatedLayers;
+        
+        beforeEach(function() {
+            timeControl = new OpenLayers.Control.Time();
+            animationControlsPanel = new Portal.details.AnimationControlsPanel({
+                timeControl: timeControl
+            });
+
+            // mock out unrelated functions.
+            animationControlsPanel._getFormDates = function() { return [{}, {}] };
+            animationControlsPanel._waitForOriginalLayer = function() {};
+            animationControlsPanel._updateButtons = function() {};
+
+            // Save the original value (and restore later), otherwise it breaks others tests
+            // (TODO: which it shouldn't).
+            animatedLayers = animationControlsPanel.animatedLayers;
+            animationControlsPanel.animatedLayers = [{
+                params: { "TIME": {} }
+            }];
+        });
+
+        afterEach(function() {
+            animationControlsPanel.animatedLayers = animatedLayers;
+        });
+        
+        it('initialisation', function() {
+            expect(animationControlsPanel.timeControl).toBe(timeControl);
+        });
+        
+        it('on play, time.play is called', function() {
+            spyOn(timeControl, 'play');
+            animationControlsPanel.playButton.fireEvent('click');
+            expect(timeControl.play).toHaveBeenCalled();
+        });
+        
+        it('on play, time.play is called', function() {
+            spyOn(timeControl, 'play');
+            animationControlsPanel.playButton.fireEvent('click');
+            expect(timeControl.play).toHaveBeenCalled();
+        });
+        
+        it('on stop, time.stop is called', function() {
+            animationControlsPanel.currentState = animationControlsPanel.state.PLAYING;
+            animationControlsPanel.counter = 0;
+            
+            spyOn(timeControl, 'stop');
+
+            animationControlsPanel.playButton.fireEvent('click');
+            expect(timeControl.stop).toHaveBeenCalled();
+        });
+    });
+
+    describe('slider', function() {
+        // TODO
+    });
+
+    describe('calendar options', function() {
+        // TODO
+    });
 });
