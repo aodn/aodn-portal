79d9f0f9f024cecc063b1d4eec0c4747e2ebb056
Dan Fruehauf <malkodan@gmail.com>
2013-05-31 11:17:12 +1000

    #162 a bit tidier, now in italic

--- grails-app/controllers/au/org/emii/portal/LayerController.groovy
+++ grails-app/controllers/au/org/emii/portal/LayerController.groovy
@@ -399,7 +399,7 @@ class LayerController {
                             if(linkUrl && linkUrl[0] != "/") { linkExternal = "class=\"external\"" }
                             // Overcome the case where the URL is valid but has no description
                             if (!linkText) {
-                                linkText = "Unnamed resource"
+                                linkText = "<i>Unnamed Resource</i>"
                             }
                             html += """<li><a ${linkExternal} href="${linkUrl}" target="_blank">${linkText}</a></li>\n"""
                         }
