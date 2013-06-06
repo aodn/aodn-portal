849040c5b17a4e6a2a893c2c174f96335acf7f81
Jon Burgess <jkburges@gmail.com>
2013-06-06 14:56:44 +1000

    Deleted a heap of dead code.

--- web-app/js/portal/details/AnimationControlsPanel.js
+++ web-app/js/portal/details/AnimationControlsPanel.js
@@ -29,18 +29,9 @@ Portal.details.AnimationControlsPanel = Ext.extend(Ext.Panel, {
 
 		Portal.details.AnimationControlsPanel.superclass.constructor.call(this, config);
 		
-		Ext.MsgBus.subscribe('removeAllLayers', function() {
-	        this.removeAnimation();
-		}, this);
-        Ext.MsgBus.subscribe('reset', function() {
-            this.removeAnimation();
-        }, this);
-
         Ext.MsgBus.subscribe('removeLayer', function(mesg,openLayer) {
             if (openLayer === this.originalLayer && this.isAnimating()) {
-                this.removeAnimation();
-                // arrives here after selectedLayerChanged listener has completed
-                this.setSelectedLayer(this.activeLayersPanelSelectedLayer);
+
             }
 
         }, this);
@@ -54,15 +45,13 @@ Portal.details.AnimationControlsPanel = Ext.extend(Ext.Panel, {
                     this.stepSlider.setMinValue(0);
                     this.stepSlider.setMaxValue(this.timeControl.getExtent().length - 1);
                 }
-                
-                this.activeLayersPanelSelectedLayer = openLayer;
+/**                
 
                 if (!this.isAnimating()) {
 
                     if (openLayer) {
                         if (openLayer.isAnimatable()) {
                             //show the panel for the first time!
-                            this.setSelectedLayer(openLayer); //set to new
                             this.update();
                         }
                     }
@@ -70,11 +59,12 @@ Portal.details.AnimationControlsPanel = Ext.extend(Ext.Panel, {
                         this.removeAnimation();
                     }
                 }
+*/
             }
-            // openlayer is null so there are no layers
-            else {
-                this.removeAnimation();
-            }
+            // // openlayer is null so there are no layers
+            // else {
+            //     this.removeAnimation();
+            // }
         }, this);
 	},
 
@@ -88,7 +78,6 @@ Portal.details.AnimationControlsPanel = Ext.extend(Ext.Panel, {
 		this.MAX_SPEED_MULTIPLIER = 32;
 
 		this.animatedLayers = new Array();
-        this.activeLayersPanelSelectedLayer = null;
 		var parent = this;
 
 		this.warn = new Ext.form.Label({
@@ -301,7 +290,6 @@ Portal.details.AnimationControlsPanel = Ext.extend(Ext.Panel, {
 		this.items = [this.controlPanel];
 
 		this.speed = this.BASE_SPEED;
-		this._resetForNewAnimation();
 		this.mapPanel = undefined;
 
 		this.pausedTime = "";
@@ -334,14 +322,10 @@ Portal.details.AnimationControlsPanel = Ext.extend(Ext.Panel, {
 	_stopPlaying : function() {
         this.timeControl.stop();
 
-//		clearTimeout(this.timerId);
-//		this.pausedTime = this.animatedLayers[this.counter].params["TIME"];
 		this._updateButtons(this.state.PAUSED);
 	},
 
 	_startPlaying : function() {
-//  		var dates = this._getFormDates();
-//		this._waitForOriginalLayer(dates[0], dates[1]);
         this._updateButtons(this.state.PLAYING);
 
         this.timeControl.play();
@@ -413,16 +397,6 @@ Portal.details.AnimationControlsPanel = Ext.extend(Ext.Panel, {
 
 	},
 
-	_resetForNewAnimation : function() {
-		this.timerId = -1;
-		this.speed = this.BASE_SPEED;
-		this.originalOpacity = -1;
-		this.pausedTime = "";
-
-		// resetting the array
-		this.animatedLayers = new Array();
-	},
-
 	_updateButtons : function(state) {
 		this.currentState = state;
 
@@ -481,39 +455,6 @@ Portal.details.AnimationControlsPanel = Ext.extend(Ext.Panel, {
 		}
 	},
 
-	removeAnimation : function() {
-		if (this.animatedLayers.length > 0) {
-			clearTimeout(this.timerId);
-
-			while (this.animatedLayers.length > 0) {
-				if ((this.animatedLayers[0].map == null)) {
-					this.animatedLayers[0] = null;
-				} 
-
-				this.animatedLayers.shift();
-			}
-
-			// stackoverflow says it's better setting length to zero than to
-			// reinitalise array.,.,.,
-			this.animatedLayers.length = 0;
-			this._setStepLabelText("");
-
-			this._updateButtons(this.state.REMOVED);
-
-			this._resetForNewAnimation();
-			delete this.originalLayer.isAnimated;
-			
-			this.allTimes = null
-			this.originalLayer = null;
-			this.selectedLayer = null;
-		}
-
-	},
-    // this sets the selected animating layer not active layer in Active Layers panel
-	setSelectedLayer : function(layer) {
-		this.selectedLayer = layer;
-	},
-
 	_setSlide : function(index) {
 		if (this.animatedLayers.length > 0) {
 			this.originalLayer.setCurrentSlide(index);
@@ -534,372 +475,6 @@ Portal.details.AnimationControlsPanel = Ext.extend(Ext.Panel, {
 		}
 	},
 
-    /**
-     * This function advances the current "frame"/"slice" for the layer, looping back to the beginning
-     * when necessary.
-     */
-	_cycleAnimation : function(forced) {
-		if (this.originalLayer.getVisibility() && this.originalLayer.map !=null) {
-			if (this.counter < this.animatedLayers.length - 1) {
-				var curLayer = this.animatedLayers[this.counter + 1];
-				if (this.map.getLayer(curLayer.id) == null) {
-					this.map.addLayer(curLayer, false);
-					this.map.setLayerIndex(curLayer, this.map.getLayerIndex(this.originalLayer));
-					curLayer.display(false);
-				} else {
-					if (curLayer.numLoadingTiles == 0) {
-						this.counter++;
-						this._setSlide(this.counter);
-					}
-				}
-			} else {
-				this.counter = 0;
-				this._setSlide(this.counter);
-			}
-		}
-
-	},
-
-	_makeNextSlide : function(timeStamp) {
-		var newLayer = this.originalLayer.template.clone();
-
-		if (this.originalLayer.name.indexOf("animated") > 0) {
-			newLayer.name = this.originalLayer.name.substr(0,
-					this.originalLayer.name.indexOf(" (animated)"))
-					+ " (" + timeStamp + ")";
-		} else {
-			newLayer.name = this.originalLayer.name + " (" + timeStamp + ")";
-		}
-		newLayer.mergeNewParams({
-					TIME : timeStamp
-				});
-
-		newLayer.setVisibility(true);
-		// newLayer.setOpacity(1);
-		// TODO: This assumes the slide we're making should be displayed
-		// straight away
-		newLayer.display(false);
-		newLayer.isAnimatedSlice = true; // NOTE: isAnimatedSlice = a layer
-		// that is part of the animation,
-		// whereas
-		// isAnimated denotes the ORIGINAL layer that is currently animated.
-
-		return newLayer;
-	},
-
-	_getFormDates : function() {
-		if (this.startDatePicker.getValue() === ""
-				|| this.endDatePicker.getValue() === ""
-				|| this.startTimeCombo.getValue() === ""
-				|| this.endTimeCombo.getValue() === "") {
-			// Incomplete start/end time! Do nothing.
-			// Maybe show a message
-			return;
-		}
-
-		var startString = this._getSelectedTimeString(true);
-		var endString = this._getSelectedTimeString(false);
-		return [startString, endString];
-	},
-
-	/*
-	 * This function waits for the original layer to load first before creating
-	 * time slices.
-	 * 
-	 * If the original layer hasn't completely loaded, the time slices (since
-	 * they are cloned!) will try and attempt to load the missing tiles from the
-	 * original layer too. Which means, the time slices never loads and the
-	 * animation doesn't start.
-	 */
-	_waitForOriginalLayer : function(startString, endString) {
-
-		if (this.selectedLayer.numLoadingTiles > 0) {
-
-			this._updateButtons(this.state.LOADING);
-			this.selectedLayer.events.register('loadend', this, function() {
-						this._loadAnimation(startString, endString);
-					});
-		} else {
-			this._loadAnimation(startString, endString)
-		}
-	},
-
-	_loadAnimation : function(startString, endString) {
-		var startDate = this._parseIso8601Date(startString);
-		var endDate = this._parseIso8601Date(endString);
-		
-
-
-		if (startDate == endDate) {
-			alert("The start and end time must not be the same");
-			return false;
-		}
-
-		if (startDate.getTime() > endDate.getTime()) {
-			alert("You must select an end date that is later than the start date");
-			return false;
-		}
-        else {
-            this._convertSelectedLayerToAnimatedLayer();
-
-			var startIndex;
-			var endIndex;
-			var dimSplit = this.getSelectedLayerTimeDimension().extent
-				.split(",");
-
-			for (var i = 0; !(startIndex && endIndex) && i < dimSplit.length; i++) {
-				var date = this._parseIso8601Date(dimSplit[i]);
-				// Use >= because some strings have milliseconds on them meaning
-				// getting exact equality could be impossible
-				if (date.getTime() >= startDate.getTime() && !startIndex) {
-					startIndex = i;
-				} else if (date.getTime() >= endDate.getTime() && !endIndex) {
-					endIndex = i;
-				}
-			}
-
-			this.originalLayer.chosenTimes = dimSplit[startIndex] + "/"
-					+ dimSplit[endIndex];
-
-			var newAnimatedLayers = new Array();
-			
-			// provide instant feedback that we're trying to load stuff
-			this._setStepLabelText("Loading... 0%");
-			
-			for (var j = startIndex; j <= endIndex; j++) {
-				var newLayer = null;
-
-				// find existing layer
-				if (this.originalLayer.slides.length > 0) {
-					for (var i = 0; i < this.originalLayer.slides.length; i++) {
-						if (dimSplit[j] === this.originalLayer.slides[i].params["TIME"]) {
-							newLayer = this.originalLayer.slides[i];
-						}
-					}
-				}
-
-				// or create new layer, since it hasn't been animated before
-				if (newLayer == null) {
-
-					
-					newLayer = this._makeNextSlide(dimSplit[j]);
-					newLayer.parentLayer = this;
-				}
-				newAnimatedLayers.push(newLayer);
-			}
-
-			this.animatedLayers = newAnimatedLayers;
-			
-			this.originalLayer.slides.length = 0;
-			for (var i = 0; i < newAnimatedLayers.length; i++) {
-				this.originalLayer.slides.push(newAnimatedLayers[i]);
-			}
-			
-						
-			// always pre-load the first one
-			this.map.addLayer(this.originalLayer.slides[0], false);
-			this.map.setLayerIndex(this.originalLayer.slides[0], this.map.getLayerIndex(this.originalLayer));
-
-			// this.selectedLayer.setOpacity(1);
-			this.stepSlider.setMinValue(0);
-			this.stepSlider.setMaxValue(this.originalLayer.slides.length - 1);
-
-			if (this.pausedTime !== "") {
-				this.counter = this._getIndexFromTime(this.pausedTime);
-			} else {
-				this.counter = 0;
-			}
-
-			this._resetTimer(this.speed);
-			this._updateButtons(this.state.PLAYING);
-		}
-	},
-
-
-    /* Adds methods to the selectedLayer for supporting animation operations.
-        TODO:We should just extend the OpenLayers.WMS class to allow for animations in the first place,
-     */
-    _convertSelectedLayerToAnimatedLayer : function() {
-        this.originalLayer = this.selectedLayer;
-
-        if (this.originalOpacity == -1)
-            this.originalOpacity = this.selectedLayer.opacity;
-
-        this.originalLayer.isAnimated = true;
-        if (this.originalLayer.name.indexOf("animated") < 0) {
-            this.originalLayer.setName(this.originalLayer.name
-                + " (animated)");
-            // update the name in details panel
-            Ext.MsgBus.publish("selectedLayerChanged", this.originalLayer);
-
-            // setup originalLayer as an animated layer adding and
-            // overriding methods and parameters
-
-            // NOTE: isAnimatedSlice = a layer that is part of the
-            // animation, whereas
-            // isAnimated denotes the ORIGINAL layer that is currently
-            // animated.
-
-            // can't clone later, or the sublayers will pick up the extra
-            // stuff we're about to add
-            this.originalLayer.template = this.originalLayer.clone();
-
-            for (var i = 0, len = this.originalLayer.div.childNodes.length; i < len; ++i) {
-                var element = this.originalLayer.div.childNodes[i].firstChild;
-                OpenLayers.Util.modifyDOMElement(element, null, null, null,
-                    null, null, null, 0);
-            }
-
-            this.originalLayer.slides = new Array();
-
-            this.currentSlide = 0;
-            this.playing = false;
-            this.originalLayer.addSlide = function(openlayer) {
-                this.slides.push(openlayer);
-
-            }
-            // might be better to go other way round, ie sublayers retrieve
-            // opacity from their parent, but think some things access
-            // opacity directly rather than through get method
-            this.originalLayer.setOpacity = function(opacity) {
-                this.opacity = opacity;
-                for (var i = 0; i < this.slides.length; i++) {
-                    this.slides[i].setOpacity(opacity);
-                }
-            }
-
-            this.originalLayer.mergeNewParams = function(newParams) {
-                //TODO:things will go very wrong if fed a TIME param here...
-                for (var i = 0; i < this.slides.length; i++) {
-                    this.slides[i].mergeNewParams(newParams);
-                }
-                return OpenLayers.Layer.WMS.prototype.mergeNewParams.apply(
-                    this, newParams);
-            }
-
-            this.originalLayer.display = function(value) {
-                if (!value) {
-                    for (var i = 0; i < this.slides.length; i++) {
-                        this.slides[i].display(false);
-                    }
-                }
-            }
-            this.originalLayer.setVisibility = function(value) {
-                this.visibility = value;
-                if (!value) {
-                    for (var i = 0; i < this.slides.length; i++) {
-                        this.slides[i].setVisibility(false);
-                    }
-                } else {
-                    // if visibility is off then won't update on zoom
-                    for (var i = 0; i < this.slides.length; i++) {
-                        this.slides[i].setVisibility(true);
-                        this.slides[i].display(false);
-                    }
-                }
-                this.display(false);
-                this.events.triggerEvent("visibilitychanged");
-            }
-
-            this.originalLayer.setCurrentSlide = function(index) {
-                this.currentSlide = index;
-                for (var i = 0; i < this.slides.length; i++) {
-                    this.slides[i].display(i == index);
-                }
-            }
-
-            this.originalLayer._onChangeLayer = function(evt)
-            {
-                // if this layer's order(bottom,second from top, etc) is changed, change the order
-                // of the frames aswell.
-
-                if (evt.property == "order" && this.map != null)
-                {
-                    for (var i = 0; i < this.slides.length; i++)
-                    {
-                        if (this.slides[i]==evt.layer)
-                        {
-                            return;
-                        }
-                    }
-
-                    for (var i = 0; i < this.slides.length; i++)
-                    {
-                        //Weird stuf happens here, but it works.
-                        //just moving the slides doesn't register in the active layers panel
-                        //So remove and add...
-                        this.map.removeLayer(this.slides[i]);
-                        this.map.addLayer(this.slides[i])
-                        this.map.setLayerIndex(this.slides[i], this.map.getLayerIndex(this)+1);
-                    }
-                }
-            }
-
-            //this runs everytime any layer is removed.
-            this.originalLayer._onLayerRemoved = function(evt)
-            {
-                if (evt.layer === this)
-                {
-                    for (var i = 0; i < evt.layer.slides.length; i++)
-                    {
-                        Ext.MsgBus.publish("removeLayer", this.slides[i]);
-
-                        //it seems that sometimes the store fails to remove the layer from the map.
-                        if(this.slides[i].map)
-                        {
-                            this.slides[i].map.removeLayer(this.slides[i]);
-                        }
-                    }
-                }
-            }
-
-            this.originalLayer._onLayerAdded = function(evt)
-            {
-                if (evt.layer == this)
-                {
-                    for (var i = 0; i < this.slides.length; i++)
-                    {
-                        //this seems to also add the slides to the layerStore somehow...
-                        this.map.addLayer(this.slides[i]);
-                    }
-                }
-            }
-
-            this.originalLayer._onMove = function() {
-                // Openlayers.Map seems to reset display of layers after a move
-                // so rereset it back.
-                if(this.getVisibility())
-                {
-                    for (var i = 0; i < this.slides.length; i++) {
-                        var value = i == this.currentSlide
-                        this.slides[i].display(value);
-                        this.slides[i].redraw();
-                    }
-                }
-            }
-
-            this.originalLayer.events.on({
-                "visibilitychanged" : this._onLayerVisibilityChanged,
-                scope : this
-            });
-
-            this.map.events.on({
-                "removelayer" : this.originalLayer._onLayerRemoved,
-                scope : this.originalLayer
-            });
-
-            this.map.events.on({
-                "addlayer" : this.originalLayer._onLayerAdded,
-                scope : this.originalLayer
-            });
-
-            this.map.events.on({
-                "changelayer" : this.originalLayer._onChangeLayer,
-                scope : this.originalLayer
-            });
-        }
-    },
-
 	_onLayerVisibilityChanged : function() {
 		if (!this.originalLayer.getVisibility()) {
 			this._stopPlaying();
@@ -908,34 +483,6 @@ Portal.details.AnimationControlsPanel = Ext.extend(Ext.Panel, {
 		}
 	},
 	
-	_resetTimer : function(speed) {
-		this.speed = speed;
-		var inst = this;
-
-		if (this.animatedLayers.length > 0) {
-			if (this.timerId != -1) {
-				clearTimeout(this.timerId);
-			}
-
-			this.timerId = setInterval(function() {
-						inst._cycleAnimation();
-					}, speed);
-
-		}
-
-		// more milliseconds between each step, so it's slower!
-		if (this.speed > this.BASE_SPEED)
-			this.speedLabel.setText(" (x1/" + (this.speed / this.BASE_SPEED)
-							+ ")", false);
-		else
-			this.speedLabel.setText(" (x " + (this.BASE_SPEED / this.speed)
-							+ ")", false);
-
-		this._updateSpeedButtons();
-		// else no animation is running, so can't change the speed of the
-		// animation
-	},
-
 	update : function() {
 		this.controlPanel.hide();
 
@@ -1100,7 +647,6 @@ Portal.details.AnimationControlsPanel = Ext.extend(Ext.Panel, {
 	loadFromSavedMap : function(layer, stamps) {
 		this.setSelectedLayer(layer);
 		this.update();
-		this._waitForOriginalLayer(stamps[0], stamps[1]);
 	},
 
 	_parseIso8601Date : function(string) {
