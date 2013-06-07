784cfcbf5485f74ed65a3de34392d10ab6106213
Jon Burgess <jkburges@gmail.com>
2013-06-07 16:34:16 +1000

    Caching of img divs per time slice is implemented.

--- web-app/js/portal/ui/openlayers/tile/TemporalImage.js
+++ web-app/js/portal/ui/openlayers/tile/TemporalImage.js
@@ -6,7 +6,35 @@
  */
 OpenLayers.Tile.TemporalImage = OpenLayers.Class(OpenLayers.Tile.Image, {
 
-    initialize: function(name, url, extent, size, options) {
-        OpenLayers.Tile.Image.prototype.initialize.apply(this, arguments);
+    imgCache: {},
+
+    parentDiv: null,
+    
+    toTime: function(dateTime) {
+
+        var key = this.position.toString() + dateTime.format();
+
+        if (!this.imgCache[key]) {
+
+            this.url = this.layer.getURL(this.bounds);
+            
+            var img = new Image();
+            img.src = this.url;
+            $(img).addClass($(this.imgDiv).attr('class'));
+            $(img).attr('style', $(this.imgDiv).attr('style'));
+            
+            this.imgCache[key] = img;
+        }
+
+        if (!this.parentDiv) {
+            this.parentDiv = $(this.imgDiv).parent().get(0);
+        }
+
+        this.imgDiv = this.imgCache[key];
+        
+        $(this.parentDiv).empty();
+        $(this.parentDiv).append(this.imgCache[key]);
+
+        this.show();
     }
 });
