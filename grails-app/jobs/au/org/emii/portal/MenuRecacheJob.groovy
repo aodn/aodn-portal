f0afe7961c951ccb0cd3db7f17fb7e74423bc3c6
dnahodil <dnahodil@utas.edu.au>
2013-06-12 15:43:22 +1000

    Removing Menu recache from after WMS Scan

--- grails-app/jobs/au/org/emii/portal/MenuRecacheJob.groovy
+++ grails-app/jobs/au/org/emii/portal/MenuRecacheJob.groovy
@@ -8,21 +8,14 @@
 
 package au.org.emii.portal
 
-import groovy.time.TimeCategory
-
 class MenuRecacheJob {
 
-	def timeout = 10 * 60 * 1000 // milliseconds
+	def timeout = 30 * 60 * 1000 // milliseconds
 
 	def execute() {
 
-		def startTime = new Date()
+		log.debug "MenuRecacheJob.execute()"
 
 		Config.recacheDefaultMenu()
-
-		use(TimeCategory) {
-
-			log.debug "Menu recache took ${new Date() - startTime}"
-		}
 	}
 }
