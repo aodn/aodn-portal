a758a54acbb45a7e1ecfad356d9b750d21564cbe
Jon Burgess <jkburges@gmail.com>
2013-07-24 10:26:45 +1000

    Added properties to enable saving of ncWms params (#244).

--- grails-app/domain/au/org/emii/portal/SnapshotLayer.groovy
+++ grails-app/domain/au/org/emii/portal/SnapshotLayer.groovy
@@ -9,30 +9,34 @@
 package au.org.emii.portal
 
 class SnapshotLayer {
-    
+
     static belongsTo = [snapshot: Snapshot]
-    
+
     // Configured layer
     Layer layer
-    
+
     // Animation details
     Boolean animated = false
     String chosenTimes
 
-    // Layer added from search     
+    // Layer added from search
     String serviceUrl
     String name
     String title
-    
+
     Boolean isBaseLayer = false
     Boolean hidden = false
     Float opacity
     String styles = ""
     String cql
 
+    // Consider refactoring these properties, as they only apply to some layers (ncWMS).
+    Float ncwmsParamMin
+    Float ncwmsParamMax
+
     static constraints = {
         layer(nullable: true)
-        serviceUrl(url: true, nullable: true, validator: { val, obj -> 
+        serviceUrl(url: true, nullable: true, validator: { val, obj ->
             obj.layer == null && val == null ? 'nullable' : true
         })
         name(blank: false, nullable: true, validator: { val, obj ->
@@ -48,6 +52,19 @@ class SnapshotLayer {
         opacity(nullable:true)
         styles()
         cql(nullable: true)
+        ncwmsParamMin(nullable: true)
+        ncwmsParamMax(
+            nullable: true,
+            validator: {
+                val, obj ->
 
+                if (val < obj.ncwmsParamMin) {
+                    return false
+                }
+
+                // Both null or neither null.
+                (obj.ncwmsParamMin == null && val == null) || (obj.ncwmsParamMin != null && val != null)
+            }
+        )
     }
 }
