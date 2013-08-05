01c41cff6a254401226af3c25647369bffd492b7
dnahodil <dnahodil@utas.edu.au>
2013-08-05 13:50:08 +1000

    Fix remaining whitespace differences

--- grails-app/domain/au/org/emii/portal/Filter.groovy
+++ grails-app/domain/au/org/emii/portal/Filter.groovy
@@ -10,46 +10,46 @@ package au.org.emii.portal
 
 class Filter implements Comparable {
 
-    String label //This is the human readable name
-    FilterType type  //String, time, etc, etc.
-    String name  //note that this is the CQL name
-    static belongsTo = [layer: Layer]
-    static hasMany = [possibleValues: String]
-    List<String> possibleValues
-    boolean enabled
-    boolean downloadOnly
+	String label //This is the human readable name
+	FilterType type  //String, time, etc, etc.
+	String name  //note that this is the CQL name
+	static belongsTo = [layer: Layer]
+	static hasMany = [possibleValues: String]
+	List<String> possibleValues
+	boolean enabled
+	boolean downloadOnly
 
 	static mapping = {
 		sort "name"
 	}
 
-    public Filter(){
-        possibleValues = []
-        downloadOnly = false   //default
-    }
+	public Filter(){
+		possibleValues = []
+		downloadOnly = false   //default
+	}
 
-    static constraints = {
-        name(blank: false)
-        type()
-        layer(nullable: false)
-        label(blank: false)
-        downloadOnly(nullable: false)
-        possibleValues(validator:{ val, obj ->
-            if(obj.type != FilterType.Boolean && obj.type != FilterType.BoundingBox && obj.type != FilterType.Date){
-                if(val.size() > 0)
-                    return true
-            }
-            else
-                return true
-            return ['invalid.possibleValues']
-        })
-    }
+	static constraints = {
+		name(blank: false)
+		type()
+		layer(nullable: false)
+		label(blank: false)
+		downloadOnly(nullable: false)
+		possibleValues(validator:{ val, obj ->
+			if(obj.type != FilterType.Boolean && obj.type != FilterType.BoundingBox && obj.type != FilterType.Date){
+				if(val.size() > 0)
+					return true
+			}
+			else
+				return true
+			return ['invalid.possibleValues']
+		})
+	}
 
-    def beforeDelete(){
-        layer.filters.remove(this);
-    }
+	def beforeDelete(){
+		layer.filters.remove(this);
+	}
 
-    def toLayerData() {
+	def toLayerData() {
 
 		def filterData = [:]
 		filterData["label"] = label
@@ -59,17 +59,17 @@ class Filter implements Comparable {
 		filterData["enabled"] = enabled
 		filterData["possibleValues"] = _uiUsesPossibleValues() ? possibleValues : []
 
-	    return filterData
-    }
+		return filterData
+	}
 
 	def _uiUsesPossibleValues() {
 
 		type == FilterType.String || type == FilterType.Date
 	}
 
-    boolean equals(other){
-        return other.id == this.id && other.name.equals(this.name)
-    }
+	boolean equals(other){
+		return other.id == this.id && other.name.equals(this.name)
+	}
 
 	@Override
 	String toString() {
