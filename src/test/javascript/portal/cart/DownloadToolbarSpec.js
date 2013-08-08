86ac7ce12f85ba4a02591370d1807e91345a3a5e
Jon Burgess <jkburges@gmail.com>
2013-08-08 15:33:54 +1000

    Setting button states appropriately.

--- src/test/javascript/portal/cart/DownloadToolbarSpec.js
+++ src/test/javascript/portal/cart/DownloadToolbarSpec.js
@@ -7,9 +7,18 @@
 describe("Portal.cart.DownloadToolbar", function() {
 
     var toolbar;
+    var store;
+    var myRecord = new Portal.data.GeoNetworkRecord({
+        title: 'my record'
+    });
 
     beforeEach(function() {
         toolbar = new Portal.cart.DownloadToolbar();
+        store = Portal.data.ActiveGeoNetworkRecordStore.instance();
+    });
+
+    afterEach(function() {
+        Portal.data.ActiveGeoNetworkRecordStore.THE_ACTIVE_RECORDS_INSTANCE = undefined;
     });
 
     describe('download all button', function() {
@@ -31,6 +40,68 @@ describe("Portal.cart.DownloadToolbar", function() {
         it('has correct text', function() {
             expect(downloadAllButton.text).toBe(OpenLayers.i18n('okdownload'));
         });
+    });
+
+    describe('button states', function() {
+
+        var clearCartButton;
+        var downloadAllButton;
+
+        beforeEach(function() {
+            downloadAllButton = toolbar.items.get(0);
+            clearCartButton = toolbar.items.get(1);
+        });
+
+        it('disabled when store is initially empty', function() {
+            toolbar = new Portal.cart.DownloadToolbar({
+                store: store
+            });
+
+            expect(downloadAllButton.disabled).toBeTruthy();
+            expect(clearCartButton.disabled).toBeTruthy();
+        });
+
+        it('disabled when store becomes empty', function() {
+            store.add(myRecord);
+            expect(downloadAllButton.disabled).toBeFalsy();
+            expect(clearCartButton.disabled).toBeFalsy();
+
+            store.remove(myRecord);
+            expect(downloadAllButton.disabled).toBeTruthy();
+            expect(clearCartButton.disabled).toBeTruthy();
+        });
+
+        it('disabled when store is cleared', function() {
+            store.add(myRecord);
+            expect(downloadAllButton.disabled).toBeFalsy();
+            expect(clearCartButton.disabled).toBeFalsy();
+
+            store.removeAll();
+            expect(downloadAllButton.disabled).toBeTruthy();
+            expect(clearCartButton.disabled).toBeTruthy();
+        });
+
+        it('enabled when store initially has at least one item', function() {
+            store.add(myRecord);
+            toolbar = new Portal.cart.DownloadToolbar();
+            downloadAllButton = toolbar.items.get(0);
+
+            expect(downloadAllButton.disabled).toBeFalsy();
+            expect(clearCartButton.disabled).toBeFalsy();
+        });
+
+        it('enabled when store becomes non-empty', function() {
+            expect(downloadAllButton.disabled).toBeTruthy();
+            expect(clearCartButton.disabled).toBeTruthy();
+
+            store.add(myRecord);
+            expect(downloadAllButton.disabled).toBeFalsy();
+            expect(clearCartButton.disabled).toBeFalsy();
+        });
+
+        // it('click makes call to server downloadCart/download', function() {
+        //     // TODO:
+        // });
     });
 
     describe('clear cart button', function() {
--- web-app/js/portal/cart/DownloadToolbar.js
+++ web-app/js/portal/cart/DownloadToolbar.js
@@ -10,22 +10,44 @@ Portal.cart.DownloadToolbar = Ext.extend(Ext.Toolbar, {
 
     constructor: function() {
 
+        this.clearCartButton = new Ext.Button({
+            text: OpenLayers.i18n('clearcart'),
+            listeners: {
+                'click': function(button, event) {
+                    Portal.data.ActiveGeoNetworkRecordStore.instance().removeAll();
+                }
+            }
+        });
+
+        this.downloadAllButton = new Ext.Button({
+            text: OpenLayers.i18n('okdownload')
+        });
+
         var config = {
             items: [
-                {
-                    text: OpenLayers.i18n('okdownload')
-                },
-                {
-                    text: OpenLayers.i18n('clearcart'),
-                    listeners: {
-                        'click': function(button, event) {
-                            Portal.data.ActiveGeoNetworkRecordStore.instance().removeAll();
-                        }
-                    }
-                }
-            ]
+                this.downloadAllButton,
+                this.clearCartButton
+            ],
+            store: Portal.data.ActiveGeoNetworkRecordStore.instance()
         };
 
         Portal.cart.DownloadToolbar.superclass.constructor.call(this, config);
+
+        this._updateButtonStates();
+
+        this.store.on('add', this._updateButtonStates, this);
+        this.store.on('remove', this._updateButtonStates, this);
+        this.store.on('clear', this._updateButtonStates, this);
+    },
+
+    _updateButtonStates: function() {
+        if (this.store.getCount() > 0) {
+            this.clearCartButton.enable();
+            this.downloadAllButton.enable();
+        }
+        else {
+            this.clearCartButton.disable();
+            this.downloadAllButton.disable();
+        }
     }
 });
