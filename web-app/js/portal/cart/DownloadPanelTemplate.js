9cdb9cacf8e66457f7f6a22794cd2003255e1c41
dnahodil <dnahodil@utas.edu.au>
2013-09-11 15:32:53 +1000

    Work in laying-out the download view of a data collection. Mainly CSS styles and template changes.

--- web-app/js/portal/cart/DownloadPanelTemplate.js
+++ web-app/js/portal/cart/DownloadPanelTemplate.js
@@ -13,50 +13,122 @@ Portal.cart.DownloadPanelTemplate = Ext.extend(Ext.XTemplate, {
         this.mimeTypes = Portal.app.config.downloadCartMimeTypeToExtensionMapping;
 
         var templateLines = [
-            '<div class="cart-row">',
-            '  <div class="cart-title-row">',
-            '    <span class="cart-title">{title}</span>',
+            '<div class="download-collection">',
+            '  <div class="title-row">',
+            '    {title}',
             '  </div>',
-            '  <tpl if="wmsLayer && wmsLayer.getCqlFilter()"><div class=cart-data-filter>Filter applied: <code>{[this._getLayerFilterDisplayString(values.wmsLayer)]}</code></div></tpl>',
-            '  <div class="cart-files">{[this._getFileListMarkup(values.downloadableLinks)]}</div>',
+            '  <div class="row">',
+            '    <div class="subheading">Metadata</div>',
+            '    {[this._getPointOfTruthLinkEntry(values)]}',
+            '  </div>',
+            '  <div class="row">',
+            '    <div class="subheading">Data</div>',
+            '    {[this._getDataFilterEntry(values.wmsLayer)]}',
+            '    {[this._getDataDownloadEntry(values.wmsLayer)]}',
+            '  </div>',
+            '  <div class="row">',
+            '    <div class="subheading">Attached files</div>',
+            '    {[this._getFileListMarkup(values.downloadableLinks)]}',
+            '  </div>',
             '</div>'
         ];
 
         Portal.cart.DownloadPanelTemplate.superclass.constructor.call(this, templateLines);
     },
 
-    _getLayerFilterDisplayString: function(wmsLayer) {
+    _getPointOfTruthLinkEntry: function(record) {
 
-        return wmsLayer.getCqlFilter();
+        var href = record.pointOfTruthLink.href;
+
+        var html = this._externalLinkMarkup(href, "View metadata record");
+
+        return this._wrapInEntryMarkup(html);
+    },
+
+    _getDataFilterEntry: function(wmsLayer) {
+
+        if (wmsLayer) {
+
+            var html;
+            var cqlText = wmsLayer.getCqlFilter();
+
+            if (cqlText) {
+
+                html = "Filter applied: <code>" + cqlText + "</code>";
+            }
+            else {
+
+                html = "<i>No filters applied.</i>";
+            }
+
+            return this._wrapInEntryMarkup(html);
+        }
+        else {
+
+            return "";
+        }
+    },
+
+    _getDataDownloadEntry: function(wmsLayer) {
+
+        var html;
+
+        if (wmsLayer) {
+
+            html = '<table cellspacing="0" class="x-btn x-btn-noicon" id="ext-comp-999999" style="width: auto;"><tbody class="x-btn-small x-btn-icon-small-left"><tr><td class="x-btn-tl"><i>&nbsp;</i></td><td class="x-btn-tc"></td><td class="x-btn-tr"><i>&nbsp;</i></td></tr><tr><td class="x-btn-ml"><i>&nbsp;</i></td><td class="x-btn-mc"><em unselectable="on" class="x-btn-split" id="ext-gen9999"><button type="button" id="ext-gen99999" class=" x-btn-text">Download as...</button></em></td><td class="x-btn-mr"><i>&nbsp;</i></td></tr><tr><td class="x-btn-bl"><i>&nbsp;</i></td><td class="x-btn-bc"></td><td class="x-btn-br"><i>&nbsp;</i></td></tr></tbody></table>';
+        }
+        else {
+
+            html = "<i>No direct-access to data available currently.</i>"
+        }
+
+        return this._wrapInEntryMarkup(html);
     },
 
     _getFileListMarkup: function(links) {
 
-        var subFilesTemplate = new Ext.XTemplate(
-            '<div class="cart-file-row" >',
-            '{[this._getMarkupForOneFile(values)]}',
-            '</div>',
+        var html = "";
+
+        links = [
+            {
+                title: "cool_report.pdf",
+                href: ""
+            },
+            {
+                title: "presentation.ppt",
+                href: ""
+            }
+        ];
+
+        Ext.each(
+            links,
+            function(link) {
+                html += this._getMarkupForOneFile(link);
+            },
             this
         );
 
-        var html = "";
-
-        Ext.each(links, function(link) {
-            html += subFilesTemplate.apply(link);
-        });
+        if (html == "") html = this._wrapInEntryMarkup("<i>No attached files.</i>");
 
         return html;
     },
 
-    _getMarkupForOneFile: function(values) {
+    _wrapInEntryMarkup: function(text) {
 
-        return "<i>" + values.title + "</i>" + this._fileExtensionInfo(values.type);
+        return '<div class="entry">' + text + '</div>';
     },
 
-    _fileExtensionInfo: function(type) {
+    _externalLinkMarkup: function(href, text) {
 
-        var extension = this.mimeTypes[type];
+        if (!text) text = href;
 
-        return extension ? " (." + extension + ")" : "";
+        return "<a href='" + href + "' target='_blank' class='external'>" + text + "</a>";
+    },
+
+    _getMarkupForOneFile: function(link) {
+
+        var html = this._externalLinkMarkup(link.href, link.title); // Todo - DN: link.title or link.name ?
+
+        return this._wrapInEntryMarkup(html);
     }
 });
