0105db603f46fd6f8c059beb0080e494f763d837
Tommy Fotak <tommy.fotak@gmail.com>
2013-09-16 11:22:52 +1000

    Whitespace changes

--- web-app/js/portal/details/DetailsPanelTab.js
+++ web-app/js/portal/details/DetailsPanelTab.js
@@ -10,26 +10,26 @@ Ext.namespace('Portal.details');
 
 Portal.details.DetailsPanelTab = Ext.extend(Ext.TabPanel, {
 
-    constructor: function(cfg) {
-    	var config = Ext.apply({
-    defaults: {
-        margin: 10
-    },
-    id: 'detailsPanelTabs',
-    ref: 'detailsPanelTabs',
-    border: false,
-    activeTab: 0,
-    enableTabScroll: true,
-    cls: 'floatingDetailsPanelContent',
-    	    flex: 1
-    	}, cfg);
+    constructor: function (cfg) {
+        var config = Ext.apply({
+            defaults: {
+                margin: 10
+            },
+            id: 'detailsPanelTabs',
+            ref: 'detailsPanelTabs',
+            border: false,
+            activeTab: 0,
+            enableTabScroll: true,
+            cls: 'floatingDetailsPanelContent',
+            flex: 1
+        }, cfg);
 
         Portal.details.DetailsPanelTab.superclass.constructor.call(this, config);
     },
 
     initComponent: function() {
 
-    	this.infoPanel = new Portal.details.InfoPanel();
+        this.infoPanel = new Portal.details.InfoPanel();
         this.stylePanel = new Portal.details.StylePanel();
         this.aodaacPanel = new Portal.details.AodaacPanel();
         this.filterPanel = new Portal.filter.FilterPanel();
@@ -45,40 +45,38 @@ Portal.details.DetailsPanelTab = Ext.extend(Ext.TabPanel, {
     },
 
     update: function(layer) {
-
-    	//Update the other tab panels
+        //Update the other tab panels
         this.stylePanel.update( layer, this._showTab, this._hideTab, this );
         this.infoPanel.update( layer, this._showTab, this._hideTab, this );
         this.aodaacPanel.update( layer, this._showTab, this._hideTab, this );
 
-		/**
-			This seems like the neatest way to stop the table layout from keep appending
-			rows (despite removeAll called) to the layout, thus pushing elements further down
-			the panel.
-		**/
+        /**
+         This seems like the neatest way to stop the table layout from keep appending
+         rows (despite removeAll called) to the layout, thus pushing elements further down
+         the panel.
+         **/
         this.remove(this.filterPanel);
         this.filterPanel = new Portal.filter.FilterPanel();
         this.add(this.filterPanel);
         this.filterPanel.update( layer, this._showTab, this._hideTab, this );
-//        this._hideTab(this.filterPanel);
 
         this.show();
     },
 
     _hideTab: function(tab) {
-    	this.hideTabStripItem(tab.id);
-    	for(var i = 0; i < this.items.length; i++) {
-    		var item = this.items.get(i);
-    		if (item) {
-		    	if (Ext.get(this.getTabEl(item)).isVisible()) {
-		    		this.setActiveTab(item);
-		    		i = this.items.length;
-    	        }
+        this.hideTabStripItem(tab.id);
+        for (var i = 0; i < this.items.length; i++) {
+            var item = this.items.get(i);
+            if (item) {
+                if (Ext.get(this.getTabEl(item)).isVisible()) {
+                    this.setActiveTab(item);
+                    i = this.items.length;
+                }
             }
         }
     },
 
     _showTab: function(tab) {
-    	this.unhideTabStripItem(tab.id);
+        this.unhideTabStripItem(tab.id);
     }
 });
