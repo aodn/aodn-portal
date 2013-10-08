c714c6594857e49fd293f9d64613deeafe6603be
Dan Fruehauf <malkodan@gmail.com>
2013-10-08 12:47:15 +1100

    UpdateKnownMimeTypes migration back to having tabs

--- grails-app/migrations/20130806-DN-UpdateKnownMimeTypes.groovy
+++ grails-app/migrations/20130806-DN-UpdateKnownMimeTypes.groovy
@@ -4,24 +4,24 @@ databaseChangeLog = {
 
         def newValue = """\
 {
-    "application/msword":"doc",
-    "application/netcdf":"nc",
-    "application/pdf":"pdf",
-    "application/vnd.google-earth.kml+xml":"kml",
-    "application/vnd.google-earth.kmz":"kmz",
-    "application/xml":"xml",
-    "image/bmp":"bmp",
-    "image/gif":"gif",
-    "image/jpeg":"jpg",
-    "image/png":"png",
-    "image/tiff":"tif",
-    "image/x-tiff":"tif",
-    "image/x-windows-bmp":"bmp",
-    "text/csv":"csv",
-    "text/html":"html",
-    "text/plain":"txt",
-    "text/xhtml":"html",
-    "text/xml":"xml"
+	"application/msword":"doc",
+	"application/netcdf":"nc",
+	"application/pdf":"pdf",
+	"application/vnd.google-earth.kml+xml":"kml",
+	"application/vnd.google-earth.kmz":"kmz",
+	"application/xml":"xml",
+	"image/bmp":"bmp",
+	"image/gif":"gif",
+	"image/jpeg":"jpg",
+	"image/png":"png",
+	"image/tiff":"tif",
+	"image/x-tiff":"tif",
+	"image/x-windows-bmp":"bmp",
+	"text/csv":"csv",
+	"text/html":"html",
+	"text/plain":"txt",
+	"text/xhtml":"html",
+	"text/xml":"xml"
 }"""
 
         sql "UPDATE config SET download_cart_mime_type_to_extension_mapping = '$newValue'"
