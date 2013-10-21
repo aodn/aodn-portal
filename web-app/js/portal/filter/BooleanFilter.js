9b7f69b788a5c188c91d3235f2a24819a634cace
pmbohm <pmbohm@utas.edu.au>
2013-10-22 10:29:44 +1100

    review change

--- web-app/js/portal/filter/BooleanFilter.js
+++ web-app/js/portal/filter/BooleanFilter.js
@@ -1,4 +1,3 @@
-
 /*
  * Copyright 2012 IMOS
  *
@@ -9,26 +8,26 @@
 Ext.namespace('Portal.filter');
 
 Portal.filter.BooleanFilter = Ext.extend(Portal.filter.BaseFilter, {
-    _createField:function() {
+    _createField: function(){
 
         this.checkbox = new Ext.form.Checkbox({
-             name: this.filter.name,
-             value: true,
-             listeners: {
-                 scope: this,
-                 check: this._buttonChecked
-             }
-         });
+            name: this.filter.name,
+            value: true,
+            listeners: {
+                scope: this,
+                check: this._buttonChecked
+            }
+        });
 
-         this.add(this.checkbox);
+        this.add(this.checkbox);
     },
 
-    _buttonChecked: function(button, checked) {
+    _buttonChecked: function(button, checked){
         this._createCQL();
         this._fireAddEvent();
     },
 
-    _createCQL: function() {
+    _createCQL: function(){
         if (this.checkbox.getValue()) {
             this.CQL = this.filter.name + " = true";
         }
@@ -36,22 +35,21 @@ Portal.filter.BooleanFilter = Ext.extend(Portal.filter.BaseFilter, {
             this.CQL = "";
         }
     },
-    
-    handleRemoveFilter: function() {
+
+    handleRemoveFilter: function(){
         this.CQL = "";
         this.checkBox.setValue(false);
     },
 
-    _setExistingFilters: function() {
+    _setExistingFilters: function(){
         this.re = new RegExp(this.filter.name + " = (.*?)( |$)");
 
         var m = this.re.exec(this.layer.getDownloadFilter());
 
-        if (m != null && m.length == 3) {
-            if (m[1] === "true") {
-                this.checkbox.setValue(true);
-                this._createCQL();
-            }
+        if (m && m[1] && m[1] === "true") {
+            this.checkbox.setValue(true);
+            this._createCQL();
         }
     }
+
 });
