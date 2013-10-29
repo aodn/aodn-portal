6e4270981859ca3b5bca8a6ed05ea94fdf5d39cf
dnahodil <dnahodil@utas.edu.au>
2013-10-30 10:43:40 +1100

    Increase width of GFI Popup

---
+++ grails-app/migrations/20131030-DN-GfiPopupWidth.groovy
@@ -0,0 +1,10 @@
+databaseChangeLog = {
+
+    changeSet(author: "dnahodil", id: "1383087986000-1") {
+
+        sql "UPDATE config SET popup_width=490;"
+        update(tableName: "config") {
+            column(name: "popup_width", value: "490")
+        }
+    }
+}
