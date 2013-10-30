5f974aa575c669fd9f192d938aea94a060fe34c6
dnahodil <dnahodil@utas.edu.au>
2013-10-30 11:19:02 +1100

    Removed SQL migration

--- grails-app/migrations/20131030-DN-GfiPopupWidth.groovy
+++ grails-app/migrations/20131030-DN-GfiPopupWidth.groovy
@@ -2,7 +2,6 @@ databaseChangeLog = {
 
     changeSet(author: "dnahodil", id: "1383087986000-1") {
 
-        sql "UPDATE config SET popup_width=490;"
         update(tableName: "config") {
             column(name: "popup_width", value: "550")
         }
