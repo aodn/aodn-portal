90649856b5230517de914a66efc62f328cd9812f
dnahodil <dnahodil@utas.edu.au>
2013-11-13 16:22:11 +1100

    I have brought shame on my family

--- grails-app/migrations/changelog.groovy
+++ grails-app/migrations/changelog.groovy
@@ -63,6 +63,8 @@ databaseChangeLog = {
     include file: '20130806-DN-UpdateKnownMimeTypes.groovy'
     include file: '20130911-PB-SetHeightForAllPortalsVer3.groovy'
     include file: '20131030-DN-GfiPopupWidth.groovy'
+    include file: '20131107-DN-AddUrlDownloadField.groovy'
+    include file: '20131112-DN-AddUrlDownloadReplacementFields.groovy'
 
     // Changes that apply to all instances must be included here, above the calls to instance-specific change logs
 
@@ -70,8 +72,4 @@ databaseChangeLog = {
     include file: 'imos-changelog.groovy'
     include file: 'wa-changelog.groovy'
     include file: 'soos-changelog.groovy'
-
-	include file: '20131107-DN-AddUrlDownloadField.groovy'
-
-	include file: '20131112-DN-AddUrlDownloadReplacementFields.groovy'
 }
