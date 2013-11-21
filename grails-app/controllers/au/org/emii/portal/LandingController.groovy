6bd964e70782b117cd01b0acb4caaaa847be30c4
pmbohm <pmbohm@utas.edu.au>
2013-11-21 17:17:06 +1100

    New landing page content added

--- grails-app/controllers/au/org/emii/portal/LandingController.groovy
+++ grails-app/controllers/au/org/emii/portal/LandingController.groovy
@@ -4,15 +4,23 @@ import grails.util.Environment
 
 class LandingController {
 
+    def oceanCurrentService
+    def portalInstance
 
     def index = {
 
-        if ( Environment.current == Environment.PRODUCTION ) {
-            [ configInstance: Config.activeInstance() ]
-        }
-        else {
+        if (Environment.current == Environment.DEVELOPMENT) {
+
+            def oceanCurrentObj = oceanCurrentService.getRandomDetails()
+            [configInstance: Config.activeInstance(), oceanCurrent: oceanCurrentObj, cfg: Config.activeInstance(), portalBuildInfo: _portalBuildInfo()]
+        } else {
             redirect(controller: "home")
         }
+    }
 
+    def _portalBuildInfo() {
+
+        def md = grailsApplication.metadata
+        return "${ portalInstance.name() } Portal v${ md.'app.version' }, build date: ${md.'app.build.date' ?: '<i>not recorded</i>'}"
     }
 }
---
+++ grails-app/views/landing/_oceanCurrentMini.gsp
@@ -0,0 +1,7 @@
+<a title="Link to the IMOS Ocean Current site" href="${oceanCurrent.baseURL}">
+<h4>Ocean Currents and Temperature Graphs</h4></a>
+
+<div class="minispacer"  >
+  To view the latest state of Australian oceans and coastal seas,
+  go to our <a class="external" title="Ocean Current page"  href="${oceanCurrent.baseURL}" target="_blank"><NOBR>Ocean Current</NOBR></a> page.
+</div>
