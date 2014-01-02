2b565b7af2ba005c6cafad716bf2a4c5347d722a
dnahodil <dnahodil@utas.edu.au>
2014-01-02 14:26:36 +1100

    Created new class ExternalRequest and made ProxiedRequest a subclass

---
+++ src/groovy/au/org/emii/portal/proxying/ExternalRequest.groovy
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2014 IMOS
+ *
+ * The AODN/IMOS Portal is distributed under the terms of the GNU General Public License
+ *
+ */
+
+package au.org.emii.portal.proxying
+
+import au.org.emii.portal.Server
+import org.apache.commons.io.IOUtils
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+import static au.org.emii.portal.UrlUtils.urlWithQueryString
+
+class ExternalRequest {
+
+    static final Logger log = LoggerFactory.getLogger(this.class)
+
+    def outputStream
+    def params
+
+    ExternalRequest(outputStream, params) {
+
+        this.outputStream = outputStream
+        this.params = params
+    }
+
+    def straightThrough = { inputStream, outputStream ->
+        log.debug "Straight-through stream processor"
+        outputStream << inputStream
+    }
+
+    def proxy(streamProcessor = null) {
+
+        def processStream = streamProcessor ?: straightThrough
+
+        def targetUrl = _getTargetUrl()
+        log.debug "Opening connection to target URL: ${targetUrl}"
+
+        def conn = targetUrl.openConnection()
+        _addAuthentication(conn, targetUrl)
+
+        try {
+            processStream conn.inputStream, outputStream
+            outputStream.flush()
+        }
+        catch (Exception e) {
+
+            log.warn "Unable to process response from $targetUrl", e
+        }
+        finally {
+
+            IOUtils.closeQuietly(outputStream)
+        }
+    }
+
+    def _getTargetUrl() {
+
+        def query = params.findAll { key, value ->
+
+            key != "controller" &&
+            key != "action" &&
+            key != "url" &&
+            key != "format" &&
+            key != "_dc"
+        }
+
+        return urlWithQueryString(params.url, query).toURL()
+    }
+
+    def _addAuthentication(connection, url) {
+        def server = _getServer(url)
+        if (server) {
+            server.addAuthentication(connection)
+        }
+    }
+
+    def _getServer(url) {
+        return Server.findByUriLike("%${url.host}%")
+    }
+}
--- src/groovy/au/org/emii/portal/proxying/ProxiedRequest.groovy
+++ src/groovy/au/org/emii/portal/proxying/ProxiedRequest.groovy
@@ -7,92 +7,37 @@
 
 package au.org.emii.portal.proxying
 
-import au.org.emii.portal.Server
-import org.apache.commons.io.IOUtils
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-import static au.org.emii.portal.UrlUtils.urlWithQueryString
-
-class ProxiedRequest {
+class ProxiedRequest extends ExternalRequest {
 
     static final Logger log = LoggerFactory.getLogger(ProxiedRequest.class)
 
     def request
     def response
-    def params
-
-    def straightThrough = { inputStream, outputStream ->
-        log.debug "Straight-through stream processor"
-        outputStream << inputStream
-    }
 
     ProxiedRequest(request, response, params) {
+
+        super(response.outputStream, params)
+
         this.request = request
         this.response = response
-        this.params = params
     }
 
-    def proxy(streamProcessor = null) {
+    @Override
+    def proxy(Object streamProcessor) {
 
-        log.debug "params: $params"
-
-        def processStream = streamProcessor ?: straightThrough
-
-        def targetUrl = _getUrl(params)
-        log.debug "Opening connection to target URL: ${targetUrl}"
-
-        def conn = targetUrl.openConnection()
+        log.debug "ProxiedRequest.proxy() params: $params"
 
         response.contentType = params.format ?: request.contentType
-
-        def outputStream = response.outputStream
-
-        _addAuthentication(conn, targetUrl)
 
         // Force download if filename provided
         if (params.downloadFilename) {
             log.debug "downloadFilename is '${params.downloadFilename}'. Forcing download."
-            response.setHeader("Content-disposition", "attachment; filename=${params.downloadFilename}")
+            response.setHeader "Content-disposition", "attachment; filename=${params.downloadFilename}"
         }
 
-        try {
-            processStream conn.inputStream, outputStream
-            outputStream.flush()
-        }
-        catch (Exception e) {
-
-            log.warn "Unable to process response from $targetUrl", e
-        }
-        finally {
-
-            IOUtils.closeQuietly(outputStream)
-        }
-    }
-
-    def _getUrl(params) {
-
-        def query = params.findAll {
-            key, value ->
-
-            key != "controller" &&
-            key != "action" &&
-            key != "url" &&
-            key != "format" &&
-            key != "_dc"
-        }
-
-        return urlWithQueryString(params.url, query).toURL()
-    }
-
-    def _addAuthentication(connection, url) {
-        def server = _getServer(url)
-        if (server) {
-            server.addAuthentication(connection)
-        }
-    }
-
-    def _getServer(url) {
-        return Server.findByUriLike("%${url.getHost()}%")
+        super.proxy(streamProcessor)
     }
 }
