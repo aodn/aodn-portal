4cf6b5773d7cda978722f4c0a597e09c86f24b8c
pmbohm <pmbohm@utas.edu.au>
2014-01-22 16:23:48 +1100

    fixes #799

--- web-app/js/portal/ui/openlayers/layer/NcWMS.js
+++ web-app/js/portal/ui/openlayers/layer/NcWMS.js
@@ -59,6 +59,7 @@ OpenLayers.Layer.NcWMS = OpenLayers.Class(OpenLayers.Layer.WMS, {
     },
 
     processTemporalExtent: function() {
+
         if (this._destroyed()) {
             return;
         }
@@ -72,6 +73,7 @@ OpenLayers.Layer.NcWMS = OpenLayers.Class(OpenLayers.Layer.WMS, {
         this.temporalExtent = new Portal.visualise.animations.TemporalExtent();
         this.temporalExtent.on('extentparsed', this._processTemporalExtentDone, this);
         this.temporalExtent.parse(this.rawTemporalExtent);
+
     },
 
     _processTemporalExtentDone: function() {
