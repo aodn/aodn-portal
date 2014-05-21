c548677e8e3cc78c8a2ef13980779be23b1baa88
dnahodil <dnahodil@utas.edu.au>
2014-05-21 16:07:23 +1000

    Whitespace

--- web-app/js/portal/details/DataCollectionSelectorPanel.js
+++ web-app/js/portal/details/DataCollectionSelectorPanel.js
@@ -9,7 +9,7 @@ Ext.namespace('Portal.details');
 
 Portal.details.DataCollectionSelectorPanel = Ext.extend(Ext.Panel, {
 
-    constructor : function(cfg) {
+    constructor: function(cfg) {
 
         var config = Ext.apply({}, cfg);
         Portal.ui.ActionsPanel.superclass.constructor.call(this, config);
@@ -21,44 +21,41 @@ Portal.details.DataCollectionSelectorPanel = Ext.extend(Ext.Panel, {
         Ext.MsgBus.subscribe(PORTAL_EVENTS.LAYER_REMOVED, function(eventName, openlayer) {
             this.removeFromlayerComboBox(openlayer);
         }, this);
-
     },
 
-    initComponent : function() {
+    initComponent: function() {
 
         this.spacer = new Ext.Spacer({
-            height : 10
+            height: 10
         });
 
         this.layerComboBox = new Ext.form.ComboBox({
-            width : 235,
-            typeAhead : true,
-            triggerAction : 'all',
-            lazyRender : true,
-            mode : 'local',
-            store : new Ext.data.ArrayStore({
-                id : 0,
-                fields : [ 'id', 'layer', 'layerName' ]
+            width: 235,
+            typeAhead: true,
+            triggerAction: 'all',
+            lazyRender: true,
+            mode: 'local',
+            store: new Ext.data.ArrayStore({
+                id: 0,
+                fields: [ 'id', 'layer', 'layerName' ]
             }),
-            valueField : 'id',
-            editable : false,
-            displayField : 'layerName',
-            listeners : {
-                select : function(combo, record) {
+            valueField: 'id',
+            editable: false,
+            displayField: 'layerName',
+            listeners: {
+                select: function(combo, record) {
                     Ext.MsgBus.publish(PORTAL_EVENTS.SELECTED_LAYER_CHANGED,
-                            record.data.layer);
+                        record.data.layer);
                 }
             }
         });
 
         this.items = [ this.spacer, this.layerComboBox ];
 
-        Portal.details.DataCollectionSelectorPanel.superclass.initComponent
-                .call(this);
-
+        Portal.details.DataCollectionSelectorPanel.superclass.initComponent.call(this);
     },
 
-    updateLayerComboBox : function(layer) {
+    updateLayerComboBox: function(layer) {
         if (layer) {
             if (this.layerComboBox.store.find('id', layer.id) == -1) {
                 this.addTolayerComboBoxStore(layer);
@@ -67,7 +64,7 @@ Portal.details.DataCollectionSelectorPanel = Ext.extend(Ext.Panel, {
         }
     },
 
-    addTolayerComboBoxStore : function(layer) {
+    addTolayerComboBoxStore: function(layer) {
         var layerArray = new Array();
         layerArray['id'] = layer.id;
         layerArray['layerName'] = layer.name;
@@ -75,7 +72,7 @@ Portal.details.DataCollectionSelectorPanel = Ext.extend(Ext.Panel, {
         this.layerComboBox.store.insert(0, new Ext.data.Record(layerArray));
     },
 
-    removeFromlayerComboBox : function(layer) {
+    removeFromlayerComboBox: function(layer) {
         var index = this.layerComboBox.store.find('id', layer.id);
         if (index != -1) {
             this.layerComboBox.setValue('');
