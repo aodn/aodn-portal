98571a2d33251ef0af763ee47204ef6f32143fea
dnahodil <dnahodil@utas.edu.au>
2014-07-04 15:47:00 +1000

    Tests for GogoduckDownloadHandler

--- src/test/javascript/portal/data/GogoduckDownloadHandlerSpec.js
+++ src/test/javascript/portal/data/GogoduckDownloadHandlerSpec.js
@@ -7,4 +7,130 @@
 
 describe('Portal.cart.GogoduckDownloadHandler', function () {
 
+    var handler;
+
+    beforeEach(function() {
+
+        handler = new Portal.cart.GogoduckDownloadHandler({
+            href: 'geoserver_url',
+            name: 'layer_name'
+        });
+    });
+
+    describe('getDownloadOptions', function() {
+
+        it('has one valid option', function() {
+
+            var options = handler.getDownloadOptions();
+
+            expect(options.length).toBe(1);
+
+            var option = options[0];
+
+            expect(option.textKey).not.toBeUndefined();
+            expect(typeof option.handler).toBe('function');
+        });
+
+        it('has no options when missing required href information', function() {
+
+            handler.onlineResource.href = "";
+
+            var options = handler.getDownloadOptions();
+
+            expect(options.length).toBe(0);
+        });
+
+        it('has no options when missing required name information', function() {
+
+            handler.onlineResource.name = "";
+
+            var options = handler.getDownloadOptions();
+
+            expect(options.length).toBe(0);
+        });
+    });
+
+    describe('the click handler', function() {
+
+        var clickHandler;
+        var testCollection;
+        var testHandlerParams;
+        var url;
+        var expectedUrlStart = 'gogoduck/registerJob?jobParameters=';
+        var json;
+
+        beforeEach(function() {
+
+            clickHandler = handler._getClickHandler();
+
+            testCollection = {
+                ncwmsParams: {
+                    dateRangeStart: moment('2000-01-01T01:01:01'),
+                    dateRangeEnd: moment('2014-12-23T23:59:59'),
+                    latitudeRangeStart: -42,
+                    latitudeRangeEnd: -20,
+                    longitudeRangeStart: 160,
+                    longitudeRangeEnd: 170
+                }
+            };
+            testHandlerParams = {
+                emailAddress: 'bob@example.com'
+            };
+
+            url = clickHandler(testCollection, testHandlerParams);
+
+            json = jsonFromUrl(url, expectedUrlStart);
+        });
+
+        it('updates the handlerParams', function() {
+
+            expect(testHandlerParams.asyncDownload).toBe(true);
+            expect(testHandlerParams.collectEmailAddress).toBe(true);
+        });
+
+        it('builds the correct URL', function() {
+
+            expect(url).toStartWith(expectedUrlStart);
+
+            expect(json.layerName).toBe('layer_name');
+            expect(json.emailAddress).toBe('bob@example.com');
+            expect(json.geoserver).toBe('geoserver_url');
+
+            var temporalExtent = json.subsetDescriptor.temporalExtent;
+
+            expect(temporalExtent.start).toBe('2000-01-01T01:01:01.000Z');
+            expect(temporalExtent.end).toBe('2014-12-23T23:59:59.000Z');
+
+            var spatialExtent = json.subsetDescriptor.spatialExtent;
+
+            expect(spatialExtent.north).toBe(-20);
+            expect(spatialExtent.south).toBe(-42);
+            expect(spatialExtent.east).toBe(170);
+            expect(spatialExtent.west).toBe(160);
+        });
+
+        it('builds the correct URL if no area is specified', function() {
+
+            testCollection.ncwmsParams.latitudeRangeStart = undefined;
+            testCollection.ncwmsParams.latitudeRangeEnd = undefined;
+            testCollection.ncwmsParams.longitudeRangeStart = undefined;
+            testCollection.ncwmsParams.longitudeRangeEnd = undefined;
+
+            url = clickHandler(testCollection, testHandlerParams);
+            json = jsonFromUrl(url, expectedUrlStart);
+
+            var spatialExtent = json.subsetDescriptor.spatialExtent;
+
+            expect(spatialExtent.north).toBe(90);
+            expect(spatialExtent.south).toBe(-90);
+            expect(spatialExtent.east).toBe(180);
+            expect(spatialExtent.west).toBe(-180);
+        });
+    });
 });
+
+var jsonFromUrl = function(url, urlStart) {
+
+    var jobParameters = url.substring(urlStart.length);
+    return Ext.util.JSON.decode(decodeURIComponent(jobParameters));
+};
