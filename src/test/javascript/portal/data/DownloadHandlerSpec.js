5419138e7a498211321ed0bf7f30b4d8bfe8b077
dnahodil <dnahodil@utas.edu.au>
2014-07-04 12:28:13 +1000

    Test for DownloadHandler behaviour

--- src/test/javascript/portal/data/DownloadHandlerSpec.js
+++ src/test/javascript/portal/data/DownloadHandlerSpec.js
@@ -7,4 +7,76 @@
 
 describe('Portal.cart.DownloadHandler', function () {
 
+    var handler;
+
+    describe('OnlineResource with only name', function() {
+
+        beforeEach(function() {
+
+            handler = new Portal.cart.DownloadHandler({
+                name: 'some_name'
+            });
+        });
+
+        it('_resourceName returns value', function() {
+
+            expect(handler._resourceName()).toBe('some_name');
+        });
+
+        it('_resourceHref returns undefined', function() {
+
+            expect(handler._resourceHref()).toBeUndefined();
+        });
+
+        it('_resourceHrefNotEmpty returns false', function() {
+
+            expect(handler._resourceHrefNotEmpty()).toBeFalsy();
+        });
+    });
+
+    describe('OnlineResource with only href', function() {
+
+        beforeEach(function() {
+
+            handler = new Portal.cart.DownloadHandler({
+                href: 'some_url'
+            });
+        });
+
+        it('_resourceHref returns value', function() {
+
+            expect(handler._resourceHref()).toBe('some_url');
+        });
+
+        it('_resourceName returns undefined', function() {
+
+            expect(handler._resourceName()).toBeUndefined();
+        });
+
+        it('_resourceNameNotEmpty returns false', function() {
+
+            expect(handler._resourceNameNotEmpty()).toBeFalsy();
+        });
+    });
+
+    describe('OnlineResource with href and name', function() {
+
+        beforeEach(function() {
+
+            handler = new Portal.cart.DownloadHandler({
+                href: 'some_url',
+                name: 'some_name'
+            });
+        });
+
+        it('_resourceHrefNotEmpty returns true', function() {
+
+            expect(handler._resourceHrefNotEmpty()).toBeTruthy();
+        });
+
+        it('_resourceNameNotEmpty returns true', function() {
+
+            expect(handler._resourceNameNotEmpty()).toBeTruthy();
+        });
+    });
 });
