e983637ded40b430ef26f4dbbd24e54ffd299c82
dnahodil <dnahodil@utas.edu.au>
2014-07-04 14:37:18 +1000

    Updated AodaacDownloadHandlerSpec

--- src/test/javascript/portal/data/AodaacDownloadHandlerSpec.js
+++ src/test/javascript/portal/data/AodaacDownloadHandlerSpec.js
@@ -26,7 +26,8 @@ describe('Portal.cart.AodaacDownloadHandler', function () {
 
             var option = options[0];
 
-//            expect(options.textKey).not.toBeUnde // TODO - DN: Continue here
+            expect(option.textKey).not.toBeUndefined();
+            expect(typeof option.handler).toBe('function');
         });
 
         it('has no options when missing required information', function() {
@@ -34,6 +35,72 @@ describe('Portal.cart.AodaacDownloadHandler', function () {
             handler.onlineResource.name = "";
 
             var options = handler.getDownloadOptions();
+
+            expect(options.length).toBe(0);
+        });
+    });
+
+    describe('the click handler', function() {
+
+        var clickHandler;
+        var testCollection;
+        var testHandlerParams;
+        var url;
+
+        beforeEach(function() {
+
+            clickHandler = handler._getClickHandler();
+
+            testCollection = {
+                ncwmsParams: {
+                    dateRangeStart: moment('2000-01-01T01:01:01'),
+                    dateRangeEnd: moment('2014-12-23T23:59:59'),
+                    latitudeRangeStart: -42,
+                    latitudeRangeEnd: -20,
+                    longitudeRangeStart: 160,
+                    longitudeRangeEnd: 170
+                }
+            };
+            testHandlerParams = {
+                emailAddress: 'bob@example.com'
+            };
+
+            url = clickHandler(testCollection, testHandlerParams);
+        });
+
+        it('updated the handlerParams', function() {
+
+            expect(testHandlerParams.asyncDownload).toBe(true);
+            expect(testHandlerParams.collectEmailAddress).toBe(true);
+        });
+
+        it('builds the correct URL', function() {
+
+            expect(url).toStartWith('aodaac/createJob?');
+            expect(url).toHaveParameterWithValue('dateRangeStart', '2000-01-01T01:01:01.000Z');
+            expect(url).toHaveParameterWithValue('dateRangeEnd', '2014-12-23T23:59:59.000Z');
+            expect(url).toHaveParameterWithValue('latitudeRangeStart','-42');
+            expect(url).toHaveParameterWithValue('latitudeRangeEnd', '-20');
+            expect(url).toHaveParameterWithValue('longitudeRangeStart', '160');
+            expect(url).toHaveParameterWithValue('longitudeRangeEnd', '170');
+            expect(url).toHaveParameterWithValue('productId', '131');
+            expect(url).toHaveParameterWithValue('outputFormat', 'nc');
+            expect(url).toHaveParameterWithValue('notificationEmailAddress', 'bob@example.com');
+        });
+
+        it('builds the correct URL if no area is specified', function() {
+
+            testCollection.ncwmsParams.latitudeRangeStart = undefined;
+            testCollection.ncwmsParams.latitudeRangeEnd = undefined;
+            testCollection.ncwmsParams.longitudeRangeStart = undefined;
+            testCollection.ncwmsParams.longitudeRangeEnd = undefined;
+
+            url = clickHandler(testCollection, testHandlerParams);
+
+            expect(url).toHaveParameterWithValue('latitudeRangeStart','-90');
+            expect(url).toHaveParameterWithValue('latitudeRangeEnd', '90');
+            expect(url).toHaveParameterWithValue('longitudeRangeStart', '0');
+            expect(url).toHaveParameterWithValue('longitudeRangeEnd', '180');
         });
     });
 });
