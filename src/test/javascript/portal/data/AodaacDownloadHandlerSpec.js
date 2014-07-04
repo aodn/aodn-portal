bfa2154b5d8f065a760245411627861665655529
dnahodil <dnahodil@utas.edu.au>
2014-07-04 13:49:28 +1000

    Developing AodaacDownloadHandlerSpec

--- src/test/javascript/portal/data/AodaacDownloadHandlerSpec.js
+++ src/test/javascript/portal/data/AodaacDownloadHandlerSpec.js
@@ -7,4 +7,33 @@
 
 describe('Portal.cart.AodaacDownloadHandler', function () {
 
+    var handler;
+
+    beforeEach(function() {
+
+        handler = new Portal.cart.AodaacDownloadHandler({
+            name: 131 // An AODAAC Product ID
+        });
+    });
+
+    describe('getDownloadOptions', function() {
+
+        it('has one valid option', function() {
+
+            var options = handler.getDownloadOptions();
+
+            expect(options.length).toBe(1);
+
+            var option = options[0];
+
+//            expect(options.textKey).not.toBeUnde // TODO - DN: Continue here
+        });
+
+        it('has no options when missing required information', function() {
+
+            handler.onlineResource.name = "";
+
+            var options = handler.getDownloadOptions();
+        });
+    });
 });
