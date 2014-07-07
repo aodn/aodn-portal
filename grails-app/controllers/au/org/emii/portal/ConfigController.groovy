1c3f63dc51f119fe868bf9c57d3be0982663c6c9
dnahodil <dnahodil@utas.edu.au>
2014-07-07 13:13:32 +1000

    Clear config on update

--- grails-app/controllers/au/org/emii/portal/ConfigController.groovy
+++ grails-app/controllers/au/org/emii/portal/ConfigController.groovy
@@ -141,6 +141,7 @@ class ConfigController {
 
             if (!configInstance.hasErrors() && configInstance.save(flush: true) && (flash.message == null)) {
                 flash.message = 'Config updated'
+                Config._configInstance = null
             }
 
             render(view: "edit", model: [configInstance: configInstance])
