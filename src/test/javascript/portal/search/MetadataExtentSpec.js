a7c204e656c6a9a14bd6d08968d7a9ed355dd627
Dan Fruehauf <malkodan@gmail.com>
2014-07-11 18:14:42 +1000

    support for displaying polygons in minimap

--- src/test/javascript/portal/search/MetadataExtentSpec.js
+++ src/test/javascript/portal/search/MetadataExtentSpec.js
@@ -7,59 +7,27 @@
 
 describe("Portal.search.MetadataExtent", function() {
 
-    it("creates a geoBox object from a geonetwork geobox string element", function() {
+    it("creates a geometry out of geoBox string", function() {
         var extent = new Portal.search.MetadataExtent();
-        var geoBox = extent._toGeoBox("140|-60|180|-30");
+        var geometry = extent._geoBoxToGeometry("140|-60|180|-30");
 
-        expect(geoBox.west).toEqual(140);
-        expect(geoBox.south).toEqual(-60);
-        expect(geoBox.east).toEqual(180);
-        expect(geoBox.north).toEqual(-30);
+        expect(geometry.toString()).toEqual("POLYGON((140 -60,180 -60,180 -30,140 -30,140 -60))");
     });
 
-    it("creates an object that acts as an OpenLayers point", function() {
+    it("adds a bbox to the geometries", function() {
         var extent = new Portal.search.MetadataExtent();
-        var point = extent._point(140, -60);
-
-        expect(point.x).toEqual(140);
-        expect(point.y).toEqual(-60);
-    });
-
-    it("creates an array of points that mimic a bounding box going SE NE NW SW", function() {
-        var geoBox = {
-            west: 140,
-            south: -60,
-            east: 180,
-            north: -30
-        };
-
-        var extent = new Portal.search.MetadataExtent();
-        var points = extent._boundingBoxPoints(geoBox);
-
-        expect(points[0].x).toEqual(geoBox.east);
-        expect(points[0].y).toEqual(geoBox.south);
-        expect(points[1].x).toEqual(geoBox.east);
-        expect(points[1].y).toEqual(geoBox.north);
-        expect(points[2].x).toEqual(geoBox.west);
-        expect(points[2].y).toEqual(geoBox.north);
-        expect(points[3].x).toEqual(geoBox.west);
-        expect(points[3].y).toEqual(geoBox.south);
-    });
-
-    it("adds a bbox to the extent", function() {
-        var extent = new Portal.search.MetadataExtent();
-        var before = extent.polygons.length;
+        var before = extent.geometries.length;
         extent.addBBox("140|-60|180|-30");
 
-        expect(extent.polygons.length).toEqual(before + 1);
+        expect(extent.geometries.length).toEqual(before + 1);
     });
 
-    it("creates a feature per bbox", function() {
+    it("creates a feature per geometry", function() {
         var extent = new Portal.search.MetadataExtent();
         extent.addBBox("140|-60|180|-30");
         extent.addBBox("-180|-60|-140|-30");
 
-        expect(extent._vectorFeatures().length).toEqual(extent.polygons.length);
+        expect(extent._vectorFeatures().length).toEqual(extent.geometries.length);
     });
 
     it("creates a layer", function() {
@@ -93,7 +61,7 @@ describe("Portal.search.MetadataExtent", function() {
         expect(boundsArray[3]).toEqual(-30);
     });
 
-    it("creates a bbox from WKT polygon", function() {
+    it("adds a WKT polygon", function() {
         var extent = new Portal.search.MetadataExtent();
         extent.addPolygon("POLYGON((-120 -38,-120 -40,-122 -40,-124 -40,-124 -38,-122 -38,-122 -36,-120 -36,-118 -36,-118 -38,-120 -38))");
 
--- web-app/js/portal/search/MetadataExtent.js
+++ web-app/js/portal/search/MetadataExtent.js
@@ -10,17 +10,20 @@ Ext.namespace('Portal.search');
 Portal.search.MetadataExtent = Ext.extend(Object, {
 
     constructor:function (cfg) {
-        this.polygons = [];
+        this.geometries = [];
     },
 
     addBBox: function(geoBox) {
-        this.polygons.push(this._toGeoBox(geoBox));
+        this.geometries.push(this._geoBoxToGeometry(geoBox));
     },
 
-    addPolygon: function(polygon) {
-        var geoBox = this._wktPolygonToGeoBox(polygon);
-        if (geoBox) {
-            this.polygons.push(geoBox);
+    addPolygon: function(wktPolygon) {
+        var geometry = new OpenLayers.Geometry.fromWKT(wktPolygon);
+        if (geometry) {
+            this.geometries.push(geometry);
+        }
+        else {
+            log.debug("Cannot make polygon out of " + wktPolygon);
         }
     },
 
@@ -33,18 +36,13 @@ Portal.search.MetadataExtent = Ext.extend(Object, {
     },
 
     getBounds: function() {
-        if (!this.bounds && this.polygons.length > 0) {
+        if (!this.bounds && this.geometries.length > 0) {
             this.bounds = new OpenLayers.Bounds();
 
             // Create a Bounds object and extend it to contain all given bounds
-            // defined in this.polygons
-            for (var i = 0; i < this.polygons.length; i++) {
-                this.bounds.extend(new OpenLayers.Bounds(
-                    this.polygons[i].west,
-                    this.polygons[i].south,
-                    this.polygons[i].east,
-                    this.polygons[i].north
-                ));
+            // defined in this.geometries
+            for (var i = 0; i < this.geometries.length; i++) {
+                this.bounds.extend(this.geometries[i].getBounds());
             }
         }
         return this.bounds;
@@ -52,57 +50,23 @@ Portal.search.MetadataExtent = Ext.extend(Object, {
 
     _vectorFeatures: function() {
         var features = [];
-        Ext.each(this.polygons, function(geoBox, index, all) {
-            features.push(new OpenLayers.Feature.Vector(this._boundingBoxPolygon(geoBox)));
+        Ext.each(this.geometries, function(geometry, index, all) {
+            features.push(new OpenLayers.Feature.Vector(geometry));
         }, this);
 
         return features;
     },
 
-    _boundingBoxPolygon: function(geoBox) {
-        return new OpenLayers.Geometry.Polygon(this._boundingBoxLinearRings(geoBox));
-    },
-
-    _boundingBoxLinearRings: function(geoBox) {
-        return new OpenLayers.Geometry.LinearRing(this._boundingBoxPoints(geoBox));
-    },
-
-    _boundingBoxPoints: function(geoBox) {
-        return [
-            this._point(geoBox.east, geoBox.south),
-            this._point(geoBox.east, geoBox.north),
-            this._point(geoBox.west, geoBox.north),
-            this._point(geoBox.west, geoBox.south)
-        ]
-    },
-
-    _point: function(x, y) {
-        return new OpenLayers.Geometry.Point(x, y);
-    },
-
-    _wktPolygonToGeoBox: function(polygon) {
-        var wktPolygon = new OpenLayers.Feature.Vector(OpenLayers.Geometry.fromWKT(polygon));
-        if (wktPolygon.geometry) {
-            var bounds = wktPolygon.geometry.getBounds();
-            return {
-                west: bounds.left,
-                south: bounds.bottom,
-                east: bounds.right,
-                north: bounds.top
-            };
-        }
-        else {
-            return null;
-        }
-    },
-
-    _toGeoBox: function(geoBoxStr) {
+    _geoBoxToGeometry: function(geoBoxStr) {
         var bounds = geoBoxStr.split("|");
-        return {
-            west: parseFloat(bounds[0]),
-            south: parseFloat(bounds[1]),
-            east: parseFloat(bounds[2]),
-            north: parseFloat(bounds[3])
-        };
+
+        var openLayersBounds = new OpenLayers.Bounds(
+            parseFloat(bounds[0]), // left
+            parseFloat(bounds[1]), // bot
+            parseFloat(bounds[2]), // right
+            parseFloat(bounds[3])  // top
+        );
+
+        return openLayersBounds.toGeometry();
     }
 });
