4ecc029b12596dbb4f3ce774b34dd0bd43b2c304
Dan Fruehauf <malkodan@gmail.com>
2014-07-15 14:18:16 +1000

    resetting captcha between requests of same session

--- grails-app/controllers/au/org/emii/portal/DownloadAuthController.groovy
+++ grails-app/controllers/au/org/emii/portal/DownloadAuthController.groovy
@@ -3,14 +3,20 @@ package au.org.emii.portal
 class DownloadAuthController {
 
     def downloadAuthService
+    def simpleCaptchaService
 
     def index = {
+        resetCaptcha(session)
         if (downloadAuthService.needsChallenge(request.getRemoteAddr(), session)) {
             render(view: "challenge")
         }
         else {
             render ""
         }
+    }
+
+    def resetCaptcha(session) {
+        session[simpleCaptchaService.CAPTCHA_IMAGE_ATTR] = null;
     }
 
 }
