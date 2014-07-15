91f7d0d61e5dd30790636eb35dbbcb0456f0dc30
David Nahodil <dnahodil@utas.edu.au>
2014-07-15 10:40:24 +1000

    Update BodaacDownloadHandler.js

--- web-app/js/portal/cart/BodaacDownloadHandler.js
+++ web-app/js/portal/cart/BodaacDownloadHandler.js
@@ -95,7 +95,6 @@ Portal.cart.BodaacDownloadHandler = Ext.extend(Portal.cart.DownloadHandler, {
     },
 
     _valueFromNameField: function(index) {
-
         return this.onlineResource.name.split(this.NAME_FIELD_DELIMETER)[index];
     }
 });
