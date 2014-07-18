8837dc7fbdcefc25844e1309615aa642bf5423a6
Dan Fruehauf <malkodan@gmail.com>
2014-07-18 10:18:49 +1000

    tests for DownloadAuthController

--- test/unit/au/org/emii/portal/DownloadAuthControllerTests.groovy
+++ test/unit/au/org/emii/portal/DownloadAuthControllerTests.groovy
@@ -3,15 +3,49 @@ package au.org.emii.portal
 import grails.test.*
 
 class DownloadAuthControllerTests extends ControllerUnitTestCase {
+    def downloadAuthService
+
     protected void setUp() {
         super.setUp()
+
+        DownloadAuthService.metaClass.resetChallenge = {}
+        downloadAuthService = new DownloadAuthService()
+        downloadAuthService.metaClass.needsChallenge = { ipAddress, session -> return true }
+
+        controller.downloadAuthService = downloadAuthService
     }
 
     protected void tearDown() {
         super.tearDown()
     }
 
-    void testSomething() {
+    void testRenderChallengeViewIfUserNotAuthenticated() {
+        controller.index()
+        def response = controller.response.contentAsString
 
+        assertEquals "challenge", controller.renderArgs.view
+    }
+
+    void testRenderEmptyHtmlIfUserAuthenticated() {
+        downloadAuthService.metaClass.needsChallenge = { ipAddress, session -> return false }
+
+        controller.index()
+        def response = controller.response.contentAsString
+
+        assertEquals "", response
+    }
+
+    void testChallengeCaptchaReset() {
+        boolean resetCalled = false
+
+        downloadAuthService.metaClass.resetChallenge = {
+            session ->
+
+            resetCalled = true
+        }
+
+        controller.index()
+
+        assertTrue resetCalled
     }
 }
