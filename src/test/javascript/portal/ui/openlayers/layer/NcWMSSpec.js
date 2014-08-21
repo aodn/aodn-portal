c311a4b345c3b9360c3b60839fcf9dde1f39de81
David Nahodil <dnahodil@utas.edu.au>
2014-08-21 11:25:24 +1000

    Fix for #1343
    Changes for BODAAC:
    Only store start and end dates if they are valid (which won't happen until the temporal extents are loaded from ncWMS).
    Only use dates if they are stored (if no dates are stored then no temporal extent is uded).

--- src/test/javascript/portal/ui/openlayers/layer/NcWMSSpec.js
+++ src/test/javascript/portal/ui/openlayers/layer/NcWMSSpec.js
@@ -18,7 +18,25 @@ describe('Portal.ui.openlayers.layer.NcWMS', function() {
             };
 
             expect(ncwmsLayer.getCqlForTemporalExtent()).toEqual(
-                'time%20%3E%3D%202000-01-01T01%3A01%3A01.000Z%20and%20time%20%3C%3D%202001-01-01T01%3A01%3A01.000Z'
+                'time >= 2000-01-01T01:01:01.000Z and time <= 2001-01-01T01:01:01.000Z'
+            );
+
+            ncwmsLayer.bodaacFilterParams = {
+                dateRangeStart: startTime,
+                dateRangeEnd: null
+            };
+
+            expect(ncwmsLayer.getCqlForTemporalExtent()).toEqual(
+                'time >= 2000-01-01T01:01:01.000Z'
+            );
+
+            ncwmsLayer.bodaacFilterParams = {
+                dateRangeStart: null,
+                dateRangeEnd: endTime
+            };
+
+            expect(ncwmsLayer.getCqlForTemporalExtent()).toEqual(
+                'time <= 2001-01-01T01:01:01.000Z'
             );
         });
     });
