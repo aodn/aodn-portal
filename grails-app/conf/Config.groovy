b1ed216ef5b3f9c8daf631d0a8ee46709ed2febb
Jon Burgess <jkburges@gmail.com>
2014-09-05 11:56:58 +1000

    Load external config *after* everything else.  This means that chef can override *anything* (including log configuration, which is specifically why this change is being done). Consistency in custom log fields (c.f. aatams) - this is to make log collection and parsing easier.

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -248,43 +248,6 @@ portal {
     }
 }
 
-/**
- * Instance specific customisation, clearly stolen from:
- * http://phatness.com/2010/03/how-to-externalize-your-grails-configuration/
- *
- * To use set for a specific instance, either set the environment variable "INSTANCE_NAME", or add this in the grails
- * commandline like so:
- *
- * grails -DINSTANCE_NAME=WA run-app
- *
- * Instance specific config files are located in $project_home/instances/
- *
- * Any configuration found in these instance specific file will OVERRIDE values set in Config.groovy and
- * application.properties.
- *
- * NOTE: app.name and version is ignored in external application.properties
- */
-if (!grails.config.locations || !(grails.config.locations instanceof List)) {
-    grails.config.locations = []
-}
-
-try {
-    configurationPath = new InitialContext().lookup('java:comp/env/aodn.configuration')
-    grails.config.locations << "file:${configurationPath}"
-
-    println "Loading external config from '$configurationPath'..."
-
-    def startupConfig = new ConfigSlurper(grailsSettings.grailsEnv).parse(new File(configurationPath).toURI().toURL())
-    System.setProperty("INSTANCE_NAME", startupConfig.portal.instance.name ?: defaultInstanceName)
-}
-catch (e) {
-
-    println "Not loading external config"
-
-    portal.instance.name = defaultInstanceName
-    System.setProperty "INSTANCE_NAME", portal.instance.name
-}
-
 // MARVL project integration
 marvl {
     urlList {
@@ -296,12 +259,16 @@ marvl {
 // WFS indexed file info
 indexedFile.fileSizeColumnName = "size"
 
+// Something else is adding an appender (what?!) - get rid of it, else we end up
+// with duplicate logs in production.
+org.apache.log4j.Logger.rootLogger.removeAllAppenders()
+
 // log4j configuration
-log4jConversionPattern = env['LOG4J_CONVERSION_PATTERN'] ?: "%-5p %d [%-12t] %c %X{userInfoForFile}- %m%n"
+def log4jConversionPattern = env['LOG4J_CONVERSION_PATTERN'] ?: "%-5p %d [%-12t] %c %X{username}- %m%n"
 
 log4j = {
-    appenders {
 
+    appenders {
         console name:'stdout', layout: pattern(conversionPattern: log4jConversionPattern)
     }
 
@@ -339,4 +306,41 @@ log4j = {
         'grails.app.controller.au.org.emii.portal.SystemController',
         'grails.app.domain',
         'grails.app.realms'
+}
+
+/**
+ * Instance specific customisation, clearly stolen from:
+ * http://phatness.com/2010/03/how-to-externalize-your-grails-configuration/
+ *
+ * To use set for a specific instance, either set the environment variable "INSTANCE_NAME", or add this in the grails
+ * commandline like so:
+ *
+ * grails -DINSTANCE_NAME=WA run-app
+ *
+ * Instance specific config files are located in $project_home/instances/
+ *
+ * Any configuration found in these instance specific file will OVERRIDE values set in Config.groovy and
+ * application.properties.
+ *
+ * NOTE: app.name and version is ignored in external application.properties
+ */
+if (!grails.config.locations || !(grails.config.locations instanceof List)) {
+    grails.config.locations = []
+}
+
+try {
+    configurationPath = new InitialContext().lookup('java:comp/env/aodn.configuration')
+    grails.config.locations << "file:${configurationPath}"
+
+    println "Loading external config from '$configurationPath'..."
+
+    def startupConfig = new ConfigSlurper(grailsSettings.grailsEnv).parse(new File(configurationPath).toURI().toURL())
+    System.setProperty("INSTANCE_NAME", startupConfig.portal.instance.name ?: defaultInstanceName)
+}
+catch (e) {
+
+    println "Not loading external config"
+
+    portal.instance.name = defaultInstanceName
+    System.setProperty "INSTANCE_NAME", portal.instance.name
 }
