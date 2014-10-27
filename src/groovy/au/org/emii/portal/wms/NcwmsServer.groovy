62db5536e11e11137d455e16d7803d0ec57dfd8c
Dan Fruehauf <malkodan@gmail.com>
2014-10-27 18:32:26 +1100

    NcwmsServer class is parsing ncwms dates

--- src/groovy/au/org/emii/portal/wms/NcwmsServer.groovy
+++ src/groovy/au/org/emii/portal/wms/NcwmsServer.groovy
@@ -26,9 +26,35 @@ class NcwmsServer extends WmsServer {
     }
 
     def getFilters(server, layer) {
+        def json = JSON.parse(getUrlContent(getMetadataUrl(server, layer)))
+
+        def filters = [
+            [
+                label: "Time",
+                type: "TimeSeries",
+                name: "timesteps",
+                possibleValues: parseDatesWithData(json.datesWithData)
+            ]
+        ]
+
+        return filters
     }
 
     def getFilterValues(server, layer, filter) {
+    }
+
+    private parseDatesWithData(datesWithData) {
+        def datesWithDataFormatted = []
+        datesWithData.each() { year, months ->
+            months.each() { month, days ->
+                days.each() { day ->
+                    // Month is zero based, so lets handle this
+                    def formattedDate = String.format("%02d-%02d-%02dT00:00:00Z", year.toInteger(), month.toInteger() + 1, day.toInteger())
+                    datesWithDataFormatted.push(formattedDate)
+                };
+            };
+        };
+        return datesWithDataFormatted
     }
 
     private String getUrlContent(url) {
