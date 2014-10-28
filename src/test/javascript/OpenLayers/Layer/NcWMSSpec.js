f4a26321a02f4619a07129ad719e323b8902fd85
Dan Fruehauf <malkodan@gmail.com>
2014-10-28 12:34:08 +1100

    NcWMS: async date loading
    on slower machines dates loading can jam the browser as it was done
    inline. the solution is to interleave it and load every year in an async
    manner. this lets the browser regain control and the experience is more
    responsive

--- src/test/javascript/OpenLayers/Layer/NcWMSSpec.js
+++ src/test/javascript/OpenLayers/Layer/NcWMSSpec.js
@@ -145,15 +145,9 @@ describe("OpenLayers.Layer.NcWMS", function() {
         var sampleJson = '{ test: 1, supportedStyles: [], palettes: [] }';
 
         beforeEach(function() {
-            spyOn(cachedLayer, '_parseDatesWithData').andCallFake(function() {});
             spyOn(cachedLayer.temporalExtent, 'getFirstDay').andCallFake(function() {});
             spyOn(cachedLayer.temporalExtent, 'getLastDay').andCallFake(function() {});
             spyOn(cachedLayer, 'loadTimeSeriesForDay').andCallFake(function() {});
-        });
-
-        it('calls _parseDatesWithData', function() {
-            cachedLayer._timeSeriesDatesLoaded(sampleJson);
-            expect(cachedLayer._parseDatesWithData).toHaveBeenCalled();
         });
 
         it('calls addDays', function() {
@@ -202,21 +196,38 @@ describe("OpenLayers.Layer.NcWMS", function() {
         });
     });
 
-    it('parses dates from NcWMS GetMetadata JSON', function() {
+    it('async parses dates from NcWMS GetMetadata JSON', function() {
         var ncwmsFiltersJson = '[{"label":"Time","type":"TimeSeries","name":"timesteps","possibleValues":["2010-02-23T00:00:00Z","2010-03-10T00:00:00Z","2010-03-11T00:00:00Z","2010-03-12T00:00:00Z","2010-03-13T00:00:00Z"]}]';
         var ncwmsFilters = Ext.util.JSON.decode(ncwmsFiltersJson);
 
-        var datesWithData = cachedLayer._parseDatesWithData(ncwmsFilters);
+        var datesWithData = null;
 
-        expect(datesWithData.length).toEqual(5);
-        expect(searchForDate(moment.utc('2010-02-23'), datesWithData)).toBeGreaterThan(-1);
-        expect(searchForDate(moment.utc('2010-03-10'), datesWithData)).toBeGreaterThan(-1);
-        expect(searchForDate(moment.utc('2010-03-11'), datesWithData)).toBeGreaterThan(-1);
-        expect(searchForDate(moment.utc('2010-03-12'), datesWithData)).toBeGreaterThan(-1);
-        expect(searchForDate(moment.utc('2010-03-13'), datesWithData)).toBeGreaterThan(-1);
+        spyOn(cachedLayer, '_timeSeriesDatesLoaded').andCallFake(
+            function(_datesWithData) {
+                datesWithData = _datesWithData;
+            }
+        );
 
-        // Search for something that shouldn't be there
-        expect(searchForDate(moment.utc('2200-02-02'), datesWithData)).toEqual(-1);
+        cachedLayer._parseDatesWithDataAsync(ncwmsFilters);
+
+        // Wait for function to return
+        waitsFor(function() {
+            return datesWithData;
+        }, "Temporal extent not processed", 1000);
+
+        runs(function() {
+            expect(cachedLayer._timeSeriesDatesLoaded).toHaveBeenCalled();
+
+            expect(datesWithData.length).toEqual(5);
+            expect(searchForDate(moment.utc('2010-02-23'), datesWithData)).toBeGreaterThan(-1);
+            expect(searchForDate(moment.utc('2010-03-10'), datesWithData)).toBeGreaterThan(-1);
+            expect(searchForDate(moment.utc('2010-03-11'), datesWithData)).toBeGreaterThan(-1);
+            expect(searchForDate(moment.utc('2010-03-12'), datesWithData)).toBeGreaterThan(-1);
+            expect(searchForDate(moment.utc('2010-03-13'), datesWithData)).toBeGreaterThan(-1);
+
+            // Search for something that shouldn't be there
+            expect(searchForDate(moment.utc('2200-02-02'), datesWithData)).toEqual(-1);
+        });
     });
 
     describe('get next time', function() {
