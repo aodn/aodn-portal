510d5c939aa7f4a2701fc8ee7befdc6bfbdf1b79
Dan Fruehauf <malkodan@gmail.com>
2014-11-18 17:48:47 +1100

    script to fetch and convert filters to xml
    parsing of xml filters from local directory
    
    Conflicts:
    	test/unit/au/org/emii/portal/LayerControllerTests.groovy

--- src/groovy/au/org/emii/portal/wms/WmsServer.groovy
+++ src/groovy/au/org/emii/portal/wms/WmsServer.groovy
@@ -7,7 +7,12 @@
 
 package au.org.emii.portal.wms
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
 abstract class WmsServer {
+    static final Logger log = LoggerFactory.getLogger(this)
+
     abstract getStyles(server, layer)
 
     abstract getFilters(server, layer)
