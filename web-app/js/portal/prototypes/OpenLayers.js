85e40b3dc1f4f72591bfc89c4546fb431a46e5dc
Dan Fruehauf <malkodan@gmail.com>
2014-11-18 17:48:47 +1100

    script to fetch and convert filters to xml
    parsing of xml filters from local directory
    
    Conflicts:
    	test/unit/au/org/emii/portal/LayerControllerTests.groovy

--- web-app/js/portal/prototypes/OpenLayers.js
+++ web-app/js/portal/prototypes/OpenLayers.js
@@ -133,10 +133,6 @@ OpenLayers.Layer.WMS.prototype.isNcwms = function() {
     return false;
 };
 
-OpenLayers.Layer.WMS.prototype.isKnownToThePortal = function() {
-    return (this.grailsLayerId) ? true : false;
-};
-
 OpenLayers.Layer.WMS.prototype.getCqlFilter = function() {
     if (this.params["CQL_FILTER"]) {
         return this.params["CQL_FILTER"];
