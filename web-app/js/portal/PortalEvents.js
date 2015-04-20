e8b7a2ea4282bd5dd320a651c6676df746b1c48f
David Nahodil <dnahodil@utas.edu.au>
2015-04-20 12:42:39 +1000

    Whitespace

--- web-app/js/portal/PortalEvents.js
+++ web-app/js/portal/PortalEvents.js
@@ -1,15 +1,15 @@
 var PORTAL_EVENTS = {
-    BEFORE_SELECTED_LAYER_CHANGED:      "beforeSelectedLayerChanged",
-    SELECTED_LAYER_CHANGED:             "selectedLayerChanged",
-    BASE_LAYER_CHANGED:                 "baseLayerChanged",
-    LAYER_REMOVED:                      "layerRemoved",
-    LAYER_LOADING_START:                "layerLoadingStart",
-    LAYER_LOADING_END:                  "layerLoadingEnd",
-    BASE_LAYER_LOADED_FROM_SERVER:      "baseLayersLoadedFromServer",
-    DATA_COLLECTION_ADDED:              "dataCollectionAdded",
-    DATA_COLLECTION_MODIFIED:           "dataCollectionModified",
-    DATA_COLLECTION_REMOVED:            "dataCollectionRemoved",
-    VIEW_DATA_COLLECTION:               "viewDataCollection",
-    RESET:                              "reset",
-    LOAD_SNAPSHOT:                      "loadSnapshot"
+    BEFORE_SELECTED_LAYER_CHANGED: "beforeSelectedLayerChanged",
+    SELECTED_LAYER_CHANGED:        "selectedLayerChanged",
+    BASE_LAYER_CHANGED:            "baseLayerChanged",
+    LAYER_REMOVED:                 "layerRemoved",
+    LAYER_LOADING_START:           "layerLoadingStart",
+    LAYER_LOADING_END:             "layerLoadingEnd",
+    BASE_LAYER_LOADED_FROM_SERVER: "baseLayersLoadedFromServer",
+    DATA_COLLECTION_ADDED:         "dataCollectionAdded",
+    DATA_COLLECTION_MODIFIED:      "dataCollectionModified",
+    DATA_COLLECTION_REMOVED:       "dataCollectionRemoved",
+    VIEW_DATA_COLLECTION:          "viewDataCollection",
+    RESET:                         "reset",
+    LOAD_SNAPSHOT:                 "loadSnapshot"
 };
