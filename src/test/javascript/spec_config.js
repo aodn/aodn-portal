317e77e84b22750a066655df879578d3af081fd9
pmbohm <pmbohm@utas.edu.au>
2015-04-30 15:07:31 +1000

    New Jasmine  JS file that will run first in tests. Contains JS global BASE_URL

---
+++ src/test/javascript/spec_config.js
@@ -0,0 +1,8 @@
+/*
+ * Copyright 2012 IMOS
+ *
+ * The AODN/IMOS Portal is distributed under the terms of the GNU General Public License
+ *
+ */
+
+var BASE_URL = "${grailsApplication.config.grails.serverURL}";
