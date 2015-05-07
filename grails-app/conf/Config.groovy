6b8d0c8d852132d5e9354e09b7a83a753f0ecbd5
Dan Fruehauf <malkodan@gmail.com>
2015-05-07 16:09:39 +1000

    blue marble base layer

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -124,8 +124,13 @@ environments {
 geonetwork.searchPath = 'xml.search.imos'
 
 // Server configuration
-baselayerServer = [
+imosStaticGeoserver = [
     uri: "http://geoserver-static.aodn.org.au/geoserver/baselayers/wms",
+    wmsVersion: '1.1.1'
+]
+
+osgeoGeoserver = [
+    uri: "http://maps.opengeo.org/geowebcache/service/wms",
     wmsVersion: '1.1.1'
 ]
 
@@ -179,7 +184,7 @@ knownServers = [
 minimap {
     baselayer {
         name = "baselayer"
-        url = baselayerServer.uri
+        url = imosStaticGeoserver.uri
         params = [layers: 'default_bathy']
     }
 }
@@ -188,12 +193,17 @@ baselayers = [
     [
         name: "default_bathy",
         title: "Bathymetry Baselayer",
-        server: baselayerServer
+        server: imosStaticGeoserver
     ],
     [
         name: "default_basemap_simple",
         title: "Simple Baselayer",
-        server: baselayerServer
+        server: imosStaticGeoserver
+    ],
+    [
+        name: "bluemarble",
+        title: "Blue Marble Baselayer",
+        server: osgeoGeoserver
     ]
 ]
 
