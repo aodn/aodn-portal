ed73bdd26c90c0d9615e9765eecc73bda7f4f55d
Dan Fruehauf <malkodan@gmail.com>
2015-05-20 11:01:59 +1000

    use correct encoding for XmlSlurper

--- grails-app/controllers/au/org/emii/portal/LayerController.groovy
+++ grails-app/controllers/au/org/emii/portal/LayerController.groovy
@@ -10,7 +10,6 @@ package au.org.emii.portal
 import au.org.emii.portal.wms.NcwmsServer
 import au.org.emii.portal.wms.GeoserverServer
 import org.springframework.web.util.HtmlUtils
-import org.xml.sax.SAXException
 
 import grails.converters.JSON
 
@@ -31,27 +30,13 @@ class LayerController {
         def status = HTTP_500_INTERNAL_SERVER_ERROR
 
         if (params.uuid != null) {
-
             try {
-                def con = new URL(_getMetadataUrl(params.uuid)).openConnection()
-                def metadataText = con.content.text
-
-                if (_isXmlContent(con.contentType)) {
-
-                    def xml = new XmlSlurper().parseText(metadataText)
-                    //TODO: Validate schema before proceeding
-
-                    def abstractText = HtmlUtils.htmlEscape(xml.identificationInfo.MD_DataIdentification.abstract.CharacterString.text())
-
-                    response = abstractText
-                    status = HTTP_200_OK
-                }
+                def xml = new XmlSlurper().parse(_getMetadataUrl(params.uuid))
+                response = HtmlUtils.htmlEscape(xml.identificationInfo.MD_DataIdentification.abstract.CharacterString.text())
+                status = HTTP_200_OK
             }
-            catch (SAXException e) {
+            catch (Exception e) {
                 status = HTTP_500_INTERNAL_SERVER_ERROR
-            }
-            catch (FileNotFoundException e) {
-                status = HTTP_404_NOT_FOUND
             }
         }
 
