ee40c7c4122918e37fc50ea6990bf9ee930dc9b5
David Nahodil <dnahodil@utas.edu.au>
2015-06-19 12:19:48 +1000

    New SpatialConstraintValidator class

---
+++ web-app/js/portal/filter/validation/SpatialConstraintValidator.js
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2015 IMOS
+ *
+ * The AODN/IMOS Portal is distributed under the terms of the GNU General Public License
+ *
+ */
+
+Ext.namespace('Portal.filter.validation');
+
+Portal.filter.validation.SpatialConstraintValidator = Ext.extend(Object, {
+
+});
