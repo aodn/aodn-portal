e98125e92b67f77894e73c84b97a6e903ddc4d69
Jon Burgess <jkburges@gmail.com>
2015-07-31 11:57:58 +1000

    Fix bug with add more than one layer

--- web-app/js/portal/details/SubsetPanelAccordion.js
+++ web-app/js/portal/details/SubsetPanelAccordion.js
@@ -25,11 +25,17 @@ Portal.details.SubsetPanelAccordion = Ext.extend(Ext.Panel, {
 
         Ext.MsgBus.subscribe(PORTAL_EVENTS.DATA_COLLECTION_ADDED, function(eventName, dataCollection) {
             this.add(this._newSubsetItemsWrapperPanel(dataCollection));
+            this.doLayout();  // This seems to be required in order to first collapse all folders.
+            this.setActiveItem(dataCollection.getUuid());
         }, this);
 
         Ext.MsgBus.subscribe(PORTAL_EVENTS.DATA_COLLECTION_REMOVED, function(eventName, dataCollection) {
             this.remove(dataCollection.getUuid());
         }, this);
+    },
+
+    setActiveItem: function(itemId) {
+        this.layout.setActiveItem(itemId);
     },
 
     _newSubsetItemsWrapperPanel: function(dataCollection) {
