2327757c57c495e922929224d65f6fd4cd4d008e
David Nahodil <dnahodil@utas.edu.au>
2015-08-13 09:43:32 +1000

    Navigation buttons respond to correct event

--- web-app/js/portal/ui/MainToolbar.js
+++ web-app/js/portal/ui/MainToolbar.js
@@ -72,7 +72,7 @@ Portal.ui.MainToolbar = Ext.extend(Ext.Toolbar, {
             success: function(resp) {
                 $("#footerContent").html(resp.responseText);
             },
-            failure: function(resp) {
+            failure: function() {
                 log.debug("Failed to obtain external links from server");
             }
         });
@@ -81,7 +81,7 @@ Portal.ui.MainToolbar = Ext.extend(Ext.Toolbar, {
     _registerEvents: function() {
         this.mainPanel.on('tabchange', this._renderNavigationButtons, this);
 
-        Ext.MsgBus.subscribe(PORTAL_EVENTS.LAYER_REMOVED, function(subject, openLayer) {
+        Ext.MsgBus.subscribe(PORTAL_EVENTS.DATA_COLLECTION_REMOVED, function() {
             this._renderNavigationButtons(this.mainPanel);
         }, this);
     },
