7836fca67806f70a66e8a6d91946afef3bc68ec6
David Nahodil <dnahodil@utas.edu.au>
2015-10-09 11:18:27 +1100

    Remove compyright notice from recently-added files

--- web-app/js/portal/ui/openlayers/layer/MiniMapBaseLayer.js
+++ web-app/js/portal/ui/openlayers/layer/MiniMapBaseLayer.js
@@ -1,9 +1,4 @@
-/*
- * Copyright 2015 IMOS
- *
- * The AODN/IMOS Portal is distributed under the terms of the GNU General Public License
- *
- */
+
 OpenLayers.Layer.MiniMapBaseLayer = OpenLayers.Class(OpenLayers.Layer.WMS, {
     initialize: function() {
         OpenLayers.Layer.WMS.prototype.initialize.apply(this, [
