4db81752cdbccbfaefe2c07b14dbb2e49539b451
Craig Jones <Craig.Jones@utas.edu.au>
2016-03-09 18:33:40 +1100

    Format dates selected from date picker for vector data as ISO8601 utc dates. Don't convert them from local time to utc as this gives unexpected results.

--- web-app/js/portal/utils/TimeUtil.js
+++ web-app/js/portal/utils/TimeUtil.js
@@ -1,46 +1,27 @@
 Ext.namespace('Portal.utils');
 
 Portal.utils.TimeUtil = Ext.extend(Object, {
-    constructor:function(config) {
-        this.DATE_FORMAT = 'Y-m-d';
-        this.TIME_FORMAT = 'H:i:s (P)';
-        this.DATE_TIME_FORMAT = this.DATE_FORMAT + ' ' + this.TIME_FORMAT;
-    },
-
     _parseIso8601Date : function(string) {
         return Date.parseDate(string, "c");
     },
 
-    _toDateString : function(date) {
-        return date.format(this.DATE_FORMAT);
+    _toIso8601DateString : function(date) {
+        return date.getFullYear() + "-"
+                + this._pad((date.getMonth() + 1)) + "-"
+                + this._pad(date.getDate());
     },
 
-    _toUtcDateString : function(date) {
-        return date.getUTCFullYear() + "-"
-                + this._pad((date.getUTCMonth() + 1)) + "-"
-                + this._pad(date.getUTCDate());
-    },
-
-    _toTimeString : function(date) {
-        return date.format(this.TIME_FORMAT);
-    },
-
-    _toUtcTimeString : function(date) {
-        return this._pad(date.getUTCHours()) + ":"
-                + this._pad(date.getUTCMinutes()) + ":"
-                + this._pad(date.getUTCSeconds()) + 'Z';
+    _toIso8601TimeString : function(date) {
+        return this._pad(date.getHours()) + ":"
+                + this._pad(date.getMinutes()) + ":"
+                + this._pad(date.getSeconds());
     },
 
     _pad : function(val) {
         return val < 10 ? '0' + val : val.toString();
     },
 
-    _toUtcIso8601DateString : function(date, timeString) {
-        if (timeString) {
-            return this._toUtcIso8601DateString(Date.parseDate(date
-                            .format(this.DATE_FORMAT)
-                            + ' ' + timeString, this.DATE_TIME_FORMAT));
-        }
-        return this._toUtcDateString(date) + 'T' + this._toUtcTimeString(date);
+    toUtcIso8601DateString : function(date) {
+        return this._toIso8601DateString(date) + 'T' + this._toIso8601TimeString(date) + 'Z';
     }
 });
