ac5d245e9e7d1f65dd34bbb1bd059baa35b8b1a5
bhasin85 <bhasin85@gmail.com>
2016-07-07 14:25:50 +1000

    Selenium Setup Files

---
+++ selenium/src/test/java/au/org/emii/portal/tests/BrowserStackTest.java
@@ -0,0 +1,19 @@
+package au.org.emii.portal.tests;
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class BrowserStackTest extends BaseTest {
+
+    @Test(invocationCount = invocationCount, threadPoolSize = threadPoolSize)
+    public void BrowserStackTest() {
+        getDriver().get("http://www.google.com");
+        WebElement element = getDriver().findElement(By.name("q"));
+
+        element.sendKeys("BrowserStackTest");
+        element.submit();
+        wait(5);
+        System.out.println(getDriver().getTitle());
+    }
+}
