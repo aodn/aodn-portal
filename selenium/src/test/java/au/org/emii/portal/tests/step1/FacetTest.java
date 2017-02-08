2afae7cc24d28150376d71a54103eb36b8b3363f
akashisama <a.mckeown@utas.edu.au>
2017-02-09 10:57:25 +1100

    Test the organisation facet

--- selenium/src/test/java/au/org/emii/portal/tests/step1/FacetTest.java
+++ selenium/src/test/java/au/org/emii/portal/tests/step1/FacetTest.java
@@ -6,12 +6,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
+import java.util.ArrayList;
 import java.util.List;
 
 public class FacetTest extends BaseTest {
 
-    private static Logger log = Logger.getLogger(PaginateInResults.class.getName());
+    private static Logger log = Logger.getLogger(FacetTest.class.getName());
 
     @Test
     public void parameterPhysicalWaterTest() throws InterruptedException {
@@ -59,6 +59,54 @@ public class FacetTest extends BaseTest {
         verifyFacetResults(firstLevelFacet, "Precipitation and evaporation");
         verifyFacetResults(firstLevelFacet, "Air-Sea Fluxes");
         verifyFacetResults(firstLevelFacet, "UV radiation");
+    }
+
+    @Test
+    public void organisationTest() throws InterruptedException {
+        getDriver().get(AODN_PORTAL_SEARCH_PAGE);
+
+
+        List<WebElement> filterPanels = webElementUtil.findElements(By.className("search-filter-panel"));
+        WebElement panel = filterPanels.get(1);
+
+        //open up the organisations panel, since it starts closed
+        panel.click();
+
+        List<WebElement> orgs = panel.findElements(By.tagName("a"));
+
+        //The orgs WebElements will go stale, so we keep a list of their names to use throughout the test
+        List<String> orgNames = new ArrayList<String>();
+
+        log.info("Getting Organisation names");
+        for(WebElement org: orgs) {
+            orgNames.add(org.getText());
+        }
+
+        for(String orgName: orgNames) {
+           verifyOrganisation(orgName);
+        }
+    }
+
+    private void verifyOrganisation(String orgName) {
+        log.info("Loading search page - Step 1");
+        getDriver().get(AODN_PORTAL_SEARCH_PAGE);
+
+        List<WebElement> filterPanels = webElementUtil.findElements(By.className("search-filter-panel"));
+        WebElement panel = filterPanels.get(1);
+
+        //open up the organisations panel, since it starts closed
+        panel.click();
+
+        portalUtil.selectFacet(orgName);
+
+        //now convert to just the org name, without the number of results
+        orgName = orgName.split(" \\(\\d{1,}\\)")[0];
+        log.info("Checking Organisation:" + orgName);
+        List<WebElement> results = webElementUtil.findElements(By.className("resultsTextBody"));
+        for (WebElement result : results) {
+            WebElement facetResult = result.findElement(By.xpath("div[2]/span[2]"));
+            Assert.assertTrue(facetResult.getText().contains(orgName));
+        }
     }
 
     private void verifyFacetResults(String firstLevelFacet, String secondLevelFacet) {
