8e37857eb8c27fbf60393df4917cb1c4615ee308
Alex McKeown <a.mckeown@utas.edu.au>
2017-02-27 16:17:12 +1100

    Altered the selenium test for the date go button to also test that the results have been filtered correctly (#2434)

---
+++ selenium/src/test/java/au/org/emii/portal/tests/step1/FacetDateTest.java
@@ -0,0 +1,62 @@
+package au.org.emii.portal.tests.step1;
+
+import au.org.emii.portal.tests.BaseTest;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class FacetDateTest extends BaseTest {
+
+    private static Logger log = Logger.getLogger(FacetDateTest.class.getName());
+
+    @Test
+    public void facetDateTest() {
+        log.info("Loading search page - Step 1");
+        WebDriver driver = getDriver();
+        driver.get(AODN_PORTAL_SEARCH_PAGE);
+
+        log.info("Selecting Date Facet");
+        portalUtil.selectFacetHeading("Parameter");
+        portalUtil.selectFacetHeading("Platform");
+        portalUtil.selectFacetHeading("Date (UTC)");
+
+        WebElement goButton = portalUtil.getFacetHeadingElement("Date (UTC)").findElement(By.xpath("//button[contains(.,'Go')]"));
+        WebElement parentElement = goButton.findElement(By.xpath("./../../../../.."));
+        Assert.assertTrue(parentElement.getAttribute("class").contains("x-item-disabled"), "Go button should not be enabled");
+
+        webElementUtil.findElement(By.xpath("//input[@name='extFrom']")).sendKeys("1991-01-01");
+        webElementUtil.findElement(By.xpath("//input[@name='extTo']")).sendKeys("1991-12-31");
+        webElementUtil.clickButtonWithText("Go");
+        portalUtil.validateFacetHeading("1991");
+
+        List<WebElement> oldResults = webElementUtil.findElements(By.className("resultsTextBody"));
+
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+        wait.until(ExpectedConditions.stalenessOf(oldResults.get(0)));
+
+        List<WebElement> results = webElementUtil.findElements(By.className("resultsTextBody"));
+        for (WebElement result : results) {
+            //sometimes the orgs section is too long, so we need to expand to see the years
+            result.click();
+            List<WebElement> facetResults = result.findElements(By.xpath("div/span[2]"));
+
+            //the last div has the years
+            String yearsString = facetResults.get(facetResults.size()-1).getText();
+            String[] years = yearsString.split(" - ");
+
+            Assert.assertTrue(Integer.parseInt(years[0]) <= 1991, "Result not within given date range");
+            Assert.assertTrue(Integer.parseInt(years[1]) >= 1991, "Result not within given date range");
+        }
+
+        log.info("Validation Complete");
+    }
+}
