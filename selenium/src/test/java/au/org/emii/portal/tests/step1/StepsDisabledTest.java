edafa6962f3550dccf363bbef2abf3c86b6e5f2b
Alex McKeown <a.mckeown@utas.edu.au>
2017-03-30 11:40:16 +1100

    Review change: use button text to click rather than list order.

--- selenium/src/test/java/au/org/emii/portal/tests/step1/StepsDisabledTest.java
+++ selenium/src/test/java/au/org/emii/portal/tests/step1/StepsDisabledTest.java
@@ -1,7 +1,6 @@
 package au.org.emii.portal.tests.step1;
 
 import au.org.emii.portal.tests.BaseTest;
-import au.org.emii.portal.tests.step3.ShapeFileTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -12,29 +11,47 @@ import org.apache.log4j.Logger;
 import java.util.List;
 
 public class StepsDisabledTest extends BaseTest {
-    private static Logger log = Logger.getLogger(ShapeFileTest.class.getName());
+    private static Logger log = Logger.getLogger(StepsDisabledTest.class.getName());
 
     @Test
     public void stepsDisabledTest() throws InterruptedException{
         WebDriver driver = getDriver();
         driver.get(AODN_PORTAL_SEARCH_PAGE);
 
+        //test step 2 button
         List<WebElement> buttons = webElementUtil.findElements(By.className("viewPortButton"));
 
-        buttons.get(1).click();
+        WebElement step2Button = null;
+        for(WebElement button:buttons) {
+            if (button.getText().contains("Create a Subset")) {
+                step2Button = button;
+            }
+        }
+
+        Assert.assertNotNull(step2Button,"No Step 2 ('Create a Subset') button found");
+        step2Button.click();
 
         WebElement results = webElementUtil.findElement(By.className("faceted-search-results"));
 
-        Assert.assertTrue(results.isDisplayed(), "Step 2 button works before a collection is added");
+        Assert.assertTrue(results.isDisplayed(), "Step 2 ('Create a Subset') button works before a collection is added");
 
+        //now we test step 3 button
         driver.navigate().refresh();
         buttons = webElementUtil.findElements(By.className("viewPortButton"));
 
-        buttons.get(2).click();
+        WebElement step3Button = null;
+        for(WebElement button:buttons) {
+            if (button.getText().contains("Download")) {
+                step3Button = button;
+            }
+        }
+
+        Assert.assertNotNull(step3Button,"No Step 3 ('Download') button found");
+        step3Button.click();
 
         results = webElementUtil.findElement(By.className("faceted-search-results"));
 
-        Assert.assertTrue(results.isDisplayed(), "Step 3 button works before a collection is added");
+        Assert.assertTrue(results.isDisplayed(), "Step 3 ('download') button works before a collection is added");
 
     }
 }
