4d3c9b8017d58bfea0dcaf55f3e74caf76751461
Ankit Bhasin <ankit.bhasin@utas.edu.au>
2017-07-10 12:37:16 +1000

    Clear a Facet - Start New Search

--- selenium/src/test/java/au/org/emii/portal/utils/PortalUtil.java
+++ selenium/src/test/java/au/org/emii/portal/utils/PortalUtil.java
@@ -3,10 +3,15 @@ package au.org.emii.portal.utils;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+
+import java.util.List;
+
+import static au.org.emii.portal.tests.BaseTest.AODN_PORTAL_SEARCH_PAGE;
 
 public class PortalUtil {
     private static Logger log = Logger.getLogger(WebElementUtil.class.getName());
@@ -20,6 +25,20 @@ public class PortalUtil {
     public void selectFacet(String facetName) {
         webElementUtil.clickLinkContainingText(facetName);
         webElementUtil.waitForElement(By.xpath(String.format("//a[contains(., '%s')]/preceding-sibling::input[@checked]", facetName)));
+    }
+
+    public void verifyFacetResults(WebDriver driver, String firstLevelFacet, String secondLevelFacet) {
+        log.info("Loading search page - Step 1");
+        driver.get(AODN_PORTAL_SEARCH_PAGE);
+
+        selectFacet(firstLevelFacet);
+        selectFacet(secondLevelFacet);
+
+        List<WebElement> results = webElementUtil.findElements(By.className("resultsTextBody"));
+        for (WebElement result : results) {
+            WebElement facetResult = result.findElement(By.xpath("div[1]/span[2]"));
+            Assert.assertTrue(facetResult.getText().contains(secondLevelFacet));
+        }
     }
 
     private String getFacetHeadingXpath(String facetHeading) {
