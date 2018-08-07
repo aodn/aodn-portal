af756a689549b27496bd2a67aef09676187f0a83
anguss00 <Angus.Scheibner@utas.edu.au>
2018-08-08 09:12:02 +1000

    Revert "Check for null map in SpatialConstraint and null div in SpatialConstraintMap to pass unit tests"
    This reverts commit bcca366ee0d22efddf4147fce1f0298feb849491.

--- web-app/js/portal/ui/openlayers/SpatialConstraintMap.js
+++ web-app/js/portal/ui/openlayers/SpatialConstraintMap.js
@@ -32,14 +32,10 @@ OpenLayers.SpatialConstraintMap = OpenLayers.Class(OpenLayers.Map, {
         );
 
         Ext.MsgBus.subscribe(PORTAL_EVENTS.STARTED_LOADING_FILTERS, function () {
-            if (Ext.get(this.viewPortDiv.id) != null) {
-                Ext.get(this.viewPortDiv.id).setStyle('cursor', 'wait');
-            }
+            Ext.get(this.viewPortDiv.id).setStyle('cursor', 'wait');
         } , this);
         Ext.MsgBus.subscribe(PORTAL_EVENTS.DATA_COLLECTION_MODIFIED, function () {
-            if (Ext.get(this.viewPortDiv.id) != null) {
-                Ext.get(this.viewPortDiv.id).setStyle('cursor', null);
-            }
+            Ext.get(this.viewPortDiv.id).setStyle('cursor', null);
         }, this);
 
     },
