6d5437c84fe07575492474ec5176408b5eec21cf
pmbohm <pmbohm@utas.edu.au>
2019-08-22 13:03:10 +1000

    Clean out unused Proxy.js

--- web-app/js/portal/utils/Proxy.js
+++
@@ -1,8 +0,0 @@
-Ext.namespace('Portal.utils');
-Ext.namespace('Portal.utils.Proxy');
-
-Portal.utils.Proxy.proxyURL = "proxy?url=";
-
-Portal.utils.Proxy.proxy = function(url) {
-    return this.proxyURL + encodeURIComponent(url);
-};
