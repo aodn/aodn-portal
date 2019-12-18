a78397b90397f7a5e648edebd5cc94ae9935396b
Craig Rose <cm.rose@utas.edu.au>
2019-12-18 15:49:49 +1100

    remove changes to Config.groovy

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -511,9 +511,7 @@ log4j.main = {
     debug   'grails.app.job',
         'grails.app.tagLib',
         'grails.app.controller.au.org.emii.portal.SystemController',
-        'grails.plugin.mail',
-        'grails.app.services.au.org.emii.portal.BulkDownloadService',
-        'grails.app.controllers.au.org.emii.portal.DownloadController'
+        'grails.plugin.mail'
 }
 
 /**
