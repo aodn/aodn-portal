1146f51d84dfb04460360d136b28678c381e61ec
Craig Rose <cm.rose@utas.edu.au>
2021-07-30 11:38:50 +1000

    always return current year when no date given

--- web-app/js/portal/search/FacetedSearchResultsDataView.js
+++ web-app/js/portal/search/FacetedSearchResultsDataView.js
@@ -337,11 +337,11 @@ initComponent: function() {
     },
 
     _formatTemporalExtentDateString: function(dateString) {
+        var dateFormat = OpenLayers.i18n('temporalExtentDateFormat');
         if (dateString) {
-            var dateFormat = OpenLayers.i18n('temporalExtentDateFormat');
             return this._parseTemporalExtentDateString(dateString).format(dateFormat);
         } else {
-            return "";
+            return moment().format("YYYY");
         }
     },
 
