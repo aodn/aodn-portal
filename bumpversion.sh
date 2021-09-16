19dd06f5c8311a3ab7750c59a48d57619b543f34
Alex McKeown <a.mckeown@utas.edu.au>
2021-09-16 14:13:33 +1000

    Use bump2version to bump versions, and bump via git tags

--- bumpversion.sh
+++ bumpversion.sh
@@ -1,66 +1,14 @@
 #!/usr/bin/env bash
 
-set -eux
+set -euxo pipefail
 
 RELEASE_BRANCH=master
 
-_get_maven_version() {
-  # extract version from pom.xml
-  version=$(xmllint --xpath '/*[local-name()="project"]/*[local-name()="version"]/text()' pom.xml)
-  echo "${version}"
-}
-
-_set_maven_version() {
-  local suffix="$1"; shift
-
-  # use Maven versions plugin to bump version
-  mvn build-helper:parse-version versions:set \
-    -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion}${suffix} \
-    versions:commit
-}
-
-_set_grails_version() {
-  local version=$1; shift
-  grails set-version "${version}"
-}
-
-_update_git() {
-  local version=$1; shift
-  git fetch --prune origin "+refs/tags/*:refs/tags/*"
-  git add pom.xml application.properties
-  git commit -m "Bump version to ${version}"
-  git tag -a -f -m "Bump version to ${version}" ${version}
-  git push --atomic origin "HEAD:${RELEASE_BRANCH}" tag "${version}"
-}
-
-_bumpversion() {
-  local suffix=$1; shift
-  _set_maven_version "$suffix" &>/dev/null
-  local new_version=$(_get_maven_version)
-  _set_grails_version "${new_version}"
-  echo "${new_version}"
-}
-
-build() {
-  local suffix="-dev"
-  _bumpversion "$suffix"
-}
-
-release() {
-  local suffix=" "
-  _bumpversion "$suffix"
-  local new_version=$(_get_maven_version)
-  _update_git "${new_version}"
-}
-
 main() {
-  local mode=$1; shift
-
-  if [ "x${mode}" == "xrelease" ]; then
-    release
-  elif [ "x${mode}" == "xbuild" ]; then
-    build
-  fi
+  git fetch --prune origin "+refs/tags/*:refs/tags/*"
+  OLD_VERSION=$(git tag -l '*.*.*' --sort=-version:refname | head -n 1)
+  NEW_VERSION=$(bump2version --current-version $OLD_VERSION --list --tag --commit  --allow-dirty patch | grep -oP '^new_version=\K.*$')
+  git push tag $NEW_VERSION
 
   exit 0
 }
