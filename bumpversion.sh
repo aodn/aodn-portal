90ccc6e2d6e1492435e71c82b97e3696268611f3
Alex McKeown <a.mckeown@utas.edu.au>
2021-09-30 09:54:30 +1000

    Fix git push command

--- bumpversion.sh
+++ bumpversion.sh
@@ -8,7 +8,7 @@ main() {
   git fetch --prune origin "+refs/tags/*:refs/tags/*"
   OLD_VERSION=$(git tag -l '*.*.*' --sort=-version:refname | head -n 1)
   NEW_VERSION=$(bump2version --current-version $OLD_VERSION --list --tag --commit  --allow-dirty patch | grep -oP '^new_version=\K.*$')
-  git push tag $NEW_VERSION
+  git push origin tag $NEW_VERSION
 
   exit 0
 }
