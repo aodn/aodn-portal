e6e5b0ea296f25ce3de9a2b4b649fc3c04e9858f
nspool <25042597+nspool@users.noreply.github.com>
2021-11-09 14:49:13 +1100

    cognito config: remove default credentials

--- web-app/js/aws-cognito/_config.js
+++ web-app/js/aws-cognito/_config.js
@@ -1,8 +1,8 @@
 window._config = {
     region: "ap-southeast-2",
     cognito: {
-      UserPoolId: "ap-southeast-2_HKMh9Piwp",
-      ClientId: "ttspre6jds4fa5sjt979mtjrb", // nsp-cog01
+      UserPoolId: null,
+      ClientId: null
     },
   };
   
